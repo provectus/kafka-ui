ea4b9dc0b43ea2ef0cc91d1fe0236960b98e391c
Oleg Shuralev <workshur@gmail.com>
2020-01-12 01:52:37 +0300

    [API] Topic Deatils mocks

---
+++ api/mocks/topicDetails.js
@@ -0,0 +1,10 @@
+module.exports = {
+  partitionCount: 25,
+  replicationFactor: 1,
+  replicas: 25,
+  inSyncReplicas: 25,
+  bytesInPerSec: 0,
+  segmentSize: 0,
+  segmentCount: 25,
+  underReplicatedPartitions: 0
+};
--- api/services/topics.js
+++ api/services/topics.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const topics = require('../mocks/topics');
+const topicDetails = require('../mocks/topicDetails');
 
 module.exports = function (fastify, opts, next) {
-  fastify.get('/clusters/:clusterId/topics', function (request, reply) {
-    reply.send(topics[request.params.clusterId]);
-  });
+  fastify
+    .get('/clusters/:clusterId/topics', function (request, reply) {
+      reply.send(topics[request.params.clusterId]);
+    })
+    .get('/clusters/:clusterId/topics/:topicId', function (request, reply) {
+      reply.send(topicDetails);
+    });
 
   next();
 }
