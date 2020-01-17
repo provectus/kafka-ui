ee92f4482ff486ac113be47a153813f70ad7072d
Oleg Shuralev <workshur@gmail.com>
2020-01-17 23:06:48 +0300

    [API] POST Topic

--- api/services/topics.js
+++ api/services/topics.js
@@ -14,6 +14,23 @@ module.exports = function (fastify, opts, next) {
     })
     .get('/clusters/:clusterId/topics/:topicId/config', function (request, reply) {
       reply.send(topicConfig);
+    })
+    .post('/clusters/:clusterId/topics', function (request, reply) {
+      /* Payload
+        {
+          "name":"AlphaNumeric-String_with,and.",
+          "partitions":"1",
+          "replicationFactor":"1",
+          "configs": {
+            "retention.ms": "604800000",
+            "retention.bytes": "-1",
+            "max.message.bytes":"1000012",
+            "min.insync.replicas":"1"
+          }
+        }
+      */
+
+      reply.code(201).send();
     });
 
   next();
