9b0aa072c0fcfb760f14aaa797cde835190e9841
Azat Gataullin <gataniel@gmail.com>
2020-04-16 19:08:20 +0300

    enhancement/improve-time-to-retain-usability

--- kafka-ui-react-app/src/components/Topics/New/TimeToRetain.tsx
+++ kafka-ui-react-app/src/components/Topics/New/TimeToRetain.tsx
@@ -4,50 +4,52 @@ import { useFormContext, ErrorMessage } from 'react-hook-form';
 import { MILLISECONDS_IN_WEEK } from 'lib/constants';
 
 const MILLISECONDS_IN_SECOND = 1000;
+const MILLISECONDS_IN_DAY = 86_400_000;
 
 interface Props {
   isSubmitting: boolean;
 }
 
-const TimeToRetain: React.FC<Props> = ({
-  isSubmitting,
-}) => {
+const TimeToRetain: React.FC<Props> = ({ isSubmitting }) => {
   const { register, errors, watch } = useFormContext();
   const defaultValue = MILLISECONDS_IN_WEEK;
-  const name: string = 'retentionMs';
-  const watchedValue: any = watch(name, defaultValue.toString());
+  const name = 'retentionMs';
+  const watchedValue = watch(name, defaultValue.toString());
 
   const valueHint = React.useMemo(() => {
     const value = parseInt(watchedValue, 10);
     return value >= MILLISECONDS_IN_SECOND ? prettyMilliseconds(value) : false;
-  }, [watchedValue])
+  }, [watchedValue]);
 
   return (
     <>
-      <label className="label">
-        Time to retain data (in ms)
-      </label>
+      <label className="label">Time to retain data (in ms)</label>
       <input
+        list="list"
         className="input"
+        id="timeToRetain"
         type="number"
         defaultValue={defaultValue}
         name={name}
-        ref={register(
-          { min: { value: -1, message: 'must be greater than or equal to -1' }}
-        )}
+        ref={register({
+          min: { value: -1, message: 'must be greater than or equal to -1' },
+        })}
+        autoComplete="off"
         disabled={isSubmitting}
       />
+      <datalist id="list">
+        <option value={MILLISECONDS_IN_DAY / 2}>12 hours</option>
+        <option value={MILLISECONDS_IN_DAY}>1 day</option>
+        <option value={MILLISECONDS_IN_DAY * 2}>2 days</option>
+        <option value={MILLISECONDS_IN_DAY * 7}>1 week</option>
+        <option value={MILLISECONDS_IN_DAY * 7 * 4}>4 weeks</option>
+      </datalist>
       <p className="help is-danger">
-        <ErrorMessage errors={errors} name={name}/>
+        <ErrorMessage errors={errors} name={name} />
       </p>
-      {
-        valueHint &&
-        <p className="help is-info">
-          {valueHint}
-        </p>
-      }
+      {valueHint && <p className="help is-info">{valueHint}</p>}
     </>
   );
-}
+};
 
 export default TimeToRetain;
