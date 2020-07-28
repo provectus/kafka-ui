e3e62d7f46f4e8c3026b07a619b10efab09d8b20
Bulat Yusupov <byusupov@provectus.com>
2020-07-28 10:36:38 +0300

    fixed release_json.sh script

--- release_json.sh
+++ release_json.sh
@@ -8,6 +8,6 @@ else
     last_tag="$last_tag..HEAD"
 fi
 
-commits_list=$(git --no-pager log $last_tag --oneline --pretty=format:"- %s\\n" | tr -d '\n' | sed 's/\"/\\"/g')
+commits_list=$(git --no-pager log $last_tag --oneline --pretty=format:"- %s\\n" | uniq | tr -d '\n' | sed 's/\"/\\"/g')
 
 echo '{"tag_name":"'$1'","name":"'$1'","body":"'$commits_list'"}' > /tmp/release.json
