ae6ca2bf5ca316f71adba5f2d3d4b5d8d617e481
Oleg Shuralev <workshur@gmail.com>
2021-02-15 12:08:13 +0300

    [CHORE] Configure Test env. Add enzyme-to-json snapshot serializer

--- kafka-ui-react-app/src/setupTests.ts
+++ kafka-ui-react-app/src/setupTests.ts
@@ -1,6 +1,5 @@
-/* eslint-disable */
-import * as Enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom/extend-expect';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
