d3c0d7612a5bc8bd64a3f4deb05048402a6ea8ad
GneyHabub <mr.afigitelniychuvak@gmail.com>
2021-03-05 22:04:19 +0300

    Fix MetricsWrapper tests

--- kafka-ui-react-app/src/components/common/Dashboard/__tests__/MetricsWrapper.spec.tsx
+++ kafka-ui-react-app/src/components/common/Dashboard/__tests__/MetricsWrapper.spec.tsx
@@ -3,21 +3,22 @@ import React from 'react';
 import MetricsWrapper from '../MetricsWrapper';
 
 describe('MetricsWrapper', () => {
-  it('correctly renders the title', () => {
-    const title = 'Title';
-    const component = shallow(<MetricsWrapper title={title} />);
-    expect(
-      component.find('[data-testid="metrics-wrapper-title"]').props().children
-    ).toEqual(title);
+  it('correctly adds classes', () => {
+    const className = 'className';
+    const component = shallow(
+      <MetricsWrapper wrapperClassName={className} multiline />
+    );
+    expect(component.find(`.${className}`).exists()).toBeTruthy();
+    expect(component.find('.level-multiline').exists()).toBeTruthy();
   });
 
   it('correctly renders children', () => {
-    const child = <div>Child</div>;
-    const component = mount(<MetricsWrapper>{child}</MetricsWrapper>);
-    expect(
-      component
-        .find('[data-testid="metrics-wrapper-children"]')
-        .containsMatchingElement(child)
-    ).toBeTruthy();
+    let component = shallow(<MetricsWrapper />);
+    expect(component.find('.subtitle').exists()).toBeFalsy();
+
+    const title = 'title';
+    component = shallow(<MetricsWrapper title={title} />);
+    expect(component.find('.subtitle').exists()).toBeTruthy();
+    expect(component.text()).toEqual(title);
   });
 });
