bd760223723d43a8aa763c1956888838abe85f3d
mbovtryuk <mbovtryuk@provectus.com>
2021-04-22 14:24:07 +0300

    [issues-211] - Clearing up messages from a topic

--- kafka-ui-react-app/src/redux/reducers/topics/__test__/reducer.spec.ts
+++ kafka-ui-react-app/src/redux/reducers/topics/__test__/reducer.spec.ts
@@ -1,29 +1,33 @@
-import { deleteTopicAction } from 'redux/actions';
+import { deleteTopicAction, clearMessagesTopicAction } from 'redux/actions';
 import reducer from '../reducer';
+
+const topic = {
+  name: 'topic',
+  id: 'id',
+};
+
+const state = {
+  byName: {
+    [topic.name]: topic,
+  },
+  allNames: [topic.name],
+  messages: [],
+  totalPages: 1,
+};
 
 describe('topics reducer', () => {
   it('deletes the topic from the list on DELETE_TOPIC__SUCCESS', () => {
-    const topic = {
-      name: 'topic',
-      id: 'id',
-    };
-    expect(
-      reducer(
-        {
-          byName: {
-            [topic.name]: topic,
-          },
-          allNames: [topic.name],
-          messages: [],
-          totalPages: 1,
-        },
-        deleteTopicAction.success(topic.name)
-      )
-    ).toEqual({
+    expect(reducer(state, deleteTopicAction.success(topic.name))).toEqual({
       byName: {},
       allNames: [],
       messages: [],
       totalPages: 1,
     });
+  });
+
+  it('delete topic messages on CLEAR_TOPIC_MESSAGES__SUCCESS', () => {
+    expect(
+      reducer(state, clearMessagesTopicAction.success(topic.name))
+    ).toEqual(state);
   });
 });
