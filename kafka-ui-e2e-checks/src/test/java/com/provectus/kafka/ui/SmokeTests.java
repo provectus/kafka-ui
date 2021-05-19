59a464ed5f0b376e0bcf996fca8b1426298cde28
Bogdan Volodarskiy <56226746+bvolodarskiy@users.noreply.github.com>
2021-05-19 09:28:43 +0300

    Checks/446 run on ci (#469)
    * try to add selenoid to flow
    
    * trying to copy .env file
    
    * fixed cp
    
    * changed backed yml for tests ci
    
    * added allure to ci
    
    * fixed allure gh pages
    
    * fixed after rewiev
    
    Co-authored-by: Bogdan Volodarskiy <bvolodarskiy@provectus.com>

--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/SmokeTests.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/SmokeTests.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SmokeTests extends BaseTest {
-
-    @Disabled("till we get tests in ci run")
+    @Test
     @SneakyThrows
     @DisplayName("main page should load")
     @Issue("380")
