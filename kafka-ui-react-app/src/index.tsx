ef923bf1cc21419d44ae4c8c686e3a8576b06bdc
Alexander <mr.afigitelniychuvak@gmail.com>
2021-06-01 11:27:12 +0300

    Add basename prop

--- kafka-ui-react-app/src/index.tsx
+++ kafka-ui-react-app/src/index.tsx
@@ -11,7 +11,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL ?? './'}>
       <AppContainer />
     </BrowserRouter>
   </Provider>,
