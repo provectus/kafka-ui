402de0e23978528ab0519d1ecd00f4bb844a0d47
mbovtryuk <mbovtryuk@provectus.com>
2021-06-18 09:11:44 +0300

    Cover Brokers page with tests #213

--- kafka-ui-react-app/src/components/Brokers/__test__/Brokers.spec.tsx
+++ kafka-ui-react-app/src/components/Brokers/__test__/Brokers.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Brokers from 'components/Brokers/Brokers';
 import { ClusterName } from 'redux/interfaces';
 import { StaticRouter } from 'react-router';
@@ -11,35 +11,78 @@ interface Props extends ClusterStats {
   fetchBrokers: (clusterName: ClusterName) => void;
 }
 
-describe('Brokers', () => {
+describe('Brokers Component', () => {
   const pathname = `ui/clusters/local/brokers`;
 
-  const setupEmptyComponent = (props: Partial<Props> = {}) => (
-    <StaticRouter location={{ pathname }} context={{}}>
-      <Brokers
-        brokerCount={0}
-        activeControllers={0}
-        zooKeeperStatus={0}
-        onlinePartitionCount={0}
-        offlinePartitionCount={0}
-        inSyncReplicasCount={0}
-        outOfSyncReplicasCount={0}
-        underReplicatedPartitionCount={0}
-        fetchClusterStats={jest.fn()}
-        fetchBrokers={jest.fn()}
-        diskUsage={undefined}
-        isFetched={false}
-        {...props}
-      />
-    </StaticRouter>
-  );
+  describe('Brokers Empty', () => {
+    const setupEmptyComponent = (props: Partial<Props> = {}) => (
+      <StaticRouter location={{ pathname }} context={{}}>
+        <Brokers
+          brokerCount={0}
+          activeControllers={0}
+          zooKeeperStatus={0}
+          onlinePartitionCount={0}
+          offlinePartitionCount={0}
+          inSyncReplicasCount={0}
+          outOfSyncReplicasCount={0}
+          underReplicatedPartitionCount={0}
+          fetchClusterStats={jest.fn()}
+          fetchBrokers={jest.fn()}
+          diskUsage={undefined}
+          isFetched={false}
+          {...props}
+        />
+      </StaticRouter>
+    );
+    it('renders section', () => {
+      const component = mount(setupEmptyComponent());
+      expect(component.exists('.section')).toBeTruthy();
+    });
 
-  it('matches snapshot', () => {
-    expect(mount(setupEmptyComponent())).toMatchSnapshot();
+    it('renders section with is-danger selector', () => {
+      const component = mount(setupEmptyComponent());
+      expect(component.exists('.is-danger')).toBeTruthy();
+    });
+
+    it('matches Brokers Empty snapshot', () => {
+      expect(mount(setupEmptyComponent())).toMatchSnapshot();
+    });
   });
 
-  it('renders Brokers', () => {
-    const component = shallow(setupEmptyComponent());
-    expect(component.find('.section')).toBeTruthy();
+  describe('Brokers', () => {
+    const setupComponent = (props: Partial<Props> = {}) => (
+      <StaticRouter location={{ pathname }} context={{}}>
+        <Brokers
+          brokerCount={1}
+          activeControllers={1}
+          zooKeeperStatus={1}
+          onlinePartitionCount={64}
+          offlinePartitionCount={0}
+          inSyncReplicasCount={64}
+          outOfSyncReplicasCount={0}
+          underReplicatedPartitionCount={0}
+          fetchClusterStats={jest.fn()}
+          fetchBrokers={jest.fn()}
+          diskUsage={[
+            {
+              brokerId: 1,
+              segmentCount: 64,
+              segmentSize: 60718,
+            },
+          ]}
+          isFetched
+          {...props}
+        />
+      </StaticRouter>
+    );
+
+    it('renders section with is-success selector', () => {
+      const component = mount(setupComponent());
+      expect(component.exists('.is-success')).toBeTruthy();
+    });
+
+    it('matches snapshot', () => {
+      expect(mount(setupComponent())).toMatchSnapshot();
+    });
   });
 });
