bec5b5be4e0cfd4805838198a04b3110186f436e
German Osin <german.osin@gmail.com>
2021-06-24 08:07:26 +0300

    Fixed sonarcube issue

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/serde/ProtobufFileRecordSerDe.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/serde/ProtobufFileRecordSerDe.java
@@ -13,6 +13,7 @@ import java.nio.file.Path;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.utils.Bytes;
@@ -26,8 +27,11 @@ public class ProtobufFileRecordSerDe implements RecordSerDe {
   public ProtobufFileRecordSerDe(Path protobufSchemaPath, String messageName,
                                  ObjectMapper objectMapper) throws IOException {
     this.objectMapper = objectMapper;
-    final String schemaString = Files.lines(protobufSchemaPath).collect(Collectors.joining());
-    this.protobufSchema = new ProtobufSchema(schemaString).copy(messageName);
+    try (final Stream<String> lines = Files.lines(protobufSchemaPath)) {
+      this.protobufSchema = new ProtobufSchema(
+          lines.collect(Collectors.joining())
+      ).copy(messageName);
+    }
   }
 
   @Override
