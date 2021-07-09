d34b18bf05e2d2c9f2fd19ebeb74d9e6891c67e5
Ilya Kuramshin <ikuramshin@provectus.com>
2021-07-09 21:10:35 +0300

    unused mapper removed

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/serde/schemaregistry/JsonSchemaMessageFormatter.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/serde/schemaregistry/JsonSchemaMessageFormatter.java
@@ -1,14 +1,10 @@
 package com.provectus.kafka.ui.serde.schemaregistry;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.kafka.serializers.json.KafkaJsonSchemaDeserializer;
-import lombok.SneakyThrows;
 
 public class JsonSchemaMessageFormatter implements MessageFormatter {
-
-  private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
 
   private final KafkaJsonSchemaDeserializer<JsonNode> jsonSchemaDeserializer;
 
@@ -16,7 +12,6 @@ public class JsonSchemaMessageFormatter implements MessageFormatter {
     this.jsonSchemaDeserializer = new KafkaJsonSchemaDeserializer<>(client);
   }
 
-  @SneakyThrows
   @Override
   public String format(String topic, byte[] value) {
     JsonNode json = jsonSchemaDeserializer.deserialize(topic, value);
