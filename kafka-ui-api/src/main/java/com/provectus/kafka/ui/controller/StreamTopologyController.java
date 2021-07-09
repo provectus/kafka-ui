19b981ff67d7e74729a148bf3c31ac903a4d24a2
fliur <flyur.karimov@gmail.com>
2021-07-09 15:26:40 +0300

    represented processor topology as a graph, fixed demo endpoint (#205)

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/controller/StreamTopologyController.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/controller/StreamTopologyController.java
@@ -13,6 +13,7 @@ import com.provectus.kafka.ui.model.TopologyNode;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -44,9 +45,9 @@ public class StreamTopologyController implements StreamTopologyApi {
     sourceConnector.setName("source connector");
     sourceConnector.setType(TopologyNode.TypeEnum.SOURCE_CONNECTOR);
 
-    final TopologyNode sourceTopic = new TopicNode();
-    sourceTopic.setName("conversation-meta");
-    sourceTopic.setType(TopologyNode.TypeEnum.TOPIC);
+    final TopologyNode inputTopic = new TopicNode();
+    inputTopic.setName("inputTopic");
+    inputTopic.setType(TopologyNode.TypeEnum.TOPIC);
 
     final ProcessorTopologyNode topologyNode = new ProcessorTopologyNode();
     topologyNode.setName("topology 1");
@@ -60,21 +61,27 @@ public class StreamTopologyController implements StreamTopologyApi {
     sinkConnector.setName("sink connector");
     sinkConnector.setType(TopologyNode.TypeEnum.SINK_CONNECTOR);
 
-    final Map<String, List<String>> adjacency = new LinkedHashMap<>();
-    adjacency.put(sourceConnector.getName(), List.of(sourceTopic.getName()));
-    adjacency.put(sourceTopic.getName(), List.of(topologyNode.getName()));
-    adjacency.put(topologyNode.getName(), List.of(sinkTopic.getName()));
-    adjacency.put(sinkTopic.getName(), List.of(sinkConnector.getName()));
-    adjacency.put(sinkConnector.getName(), List.of());
+    final Map<String, List<String>> globalAdjacency = new LinkedHashMap<>();
+    globalAdjacency.put(sourceConnector.getName(), List.of(inputTopic.getName()));
+    globalAdjacency.put(inputTopic.getName(), List.of(topologyNode.getName()));
+    globalAdjacency.put(topologyNode.getName(), List.of(sinkTopic.getName()));
+    globalAdjacency.put(sinkTopic.getName(), List.of(sinkConnector.getName()));
+    globalAdjacency.put(sinkConnector.getName(), List.of());
 
-    topology.setAdjacency(adjacency);
-    topology
-        .setNodes(List.of(sourceConnector, sourceTopic, topologyNode, sinkTopic, sinkConnector));
+    final Map<String, TopologyNode> globalNodes = new LinkedHashMap<>();
+    globalNodes.put(sourceConnector.getName(), sourceConnector);
+    globalNodes.put(inputTopic.getName(), inputTopic);
+    globalNodes.put(topologyNode.getName(), topologyNode);
+    globalNodes.put(sinkTopic.getName(), sinkTopic);
+    globalNodes.put(sinkConnector.getName(), sinkConnector);
+
+    topology.setAdjacency(globalAdjacency);
+    topology.setNodes(globalNodes);
 
     final SubTopologyNode subTopologyNode =
         getSubTopologyNode("sub-topology 0", "KSTREAM-SOURCE-0000000000",
-            "KSTREAM-TRANSFORM-0000000001", "KSTREAM-SINK-000000002", "conversation-meta",
-            "count-resolved-repartition");
+            "KSTREAM-TRANSFORM-0000000001", "KSTREAM-SINK-000000002", inputTopic.getName(),
+            "count-resolved-repartition", "store 1");
 
     final TopologyNode interTopic = new TopicNode();
     interTopic.setName("count-resolved-repartition");
@@ -83,16 +90,31 @@ public class StreamTopologyController implements StreamTopologyApi {
     final SubTopologyNode subTopologyNode1 =
         getSubTopologyNode("sub-topology 1", "KSTREAM-SOURCE-000000003",
             "KSTREAM-TRANSFORM-000000004", "KSTREAM-SINK-000000005", "count-resolved-repartition",
-            "streams-count-resolved");
+            "streams-count-resolved", "store 2");
 
-    topologyNode.setTopology(List.of(subTopologyNode, interTopic, subTopologyNode1));
+    final TopologyGraph processorTopology = new TopologyGraph();
+    Map<String, List<String>> processorAdjacency = new LinkedHashMap<>();
+    processorAdjacency.put(subTopologyNode.getName(), List.of(interTopic.getName()));
+    processorAdjacency.put(interTopic.getName(), List.of(subTopologyNode1.getName()));
+    processorAdjacency.put(subTopologyNode1.getName(), List.of());
+
+    Map<String, TopologyNode> processorNodes = new LinkedHashMap<>();
+    processorNodes.put(subTopologyNode.getName(), subTopologyNode);
+    processorNodes.put(interTopic.getName(), interTopic);
+    processorNodes.put(subTopologyNode1.getName(), subTopologyNode1);
+
+    processorTopology.setAdjacency(processorAdjacency);
+    processorTopology.setNodes(processorNodes);
+
+    topologyNode.setTopology(processorTopology);
 
     return globalTopology;
   }
 
   private static SubTopologyNode getSubTopologyNode(String subTopologyName, String sourceProcessor,
                                                     String transformProcessor, String sinkProcessor,
-                                                    String sourceTopic, String sinkTopic) {
+                                                    String sourceTopic, String sinkTopic,
+                                                    String store) {
     final SubTopologyNode subTopologyNode = new SubTopologyNode();
     subTopologyNode.setName(subTopologyName);
     subTopologyNode.setType(TopologyNode.TypeEnum.SUB_TOPOLOGY);
@@ -102,12 +124,12 @@ public class StreamTopologyController implements StreamTopologyApi {
     final SourceProcessorNode topology0Processor0 = new SourceProcessorNode();
     topology0Processor0.setName(sourceProcessor);
     topology0Processor0.setType(TopologyNode.TypeEnum.SOURCE_PROCESSOR);
-    topology0Processor0.addTopicsItem(sourceTopic);
+    Optional.ofNullable(sourceTopic).ifPresent(topology0Processor0::addTopicsItem);
 
     final ProcessorNode topology0Processor1 = new ProcessorNode();
     topology0Processor1.setName(transformProcessor);
     topology0Processor1.setType(TopologyNode.TypeEnum.PROCESSOR);
-    topology0Processor1.addStoresItem("store");
+    Optional.ofNullable(store).ifPresent(topology0Processor1::addStoresItem);
 
     final SinkProcessorNode topology0Processor2 = new SinkProcessorNode();
     topology0Processor2.setName(sinkProcessor);
@@ -120,7 +142,13 @@ public class StreamTopologyController implements StreamTopologyApi {
     adjacency.put(topology0Processor2.getName(), List.of());
 
     subTopology.setAdjacency(adjacency);
-    subTopology.setNodes(List.of(topology0Processor0, topology0Processor1, topology0Processor2));
+
+    Map<String, TopologyNode> nodes = new LinkedHashMap<>();
+    nodes.put(topology0Processor0.getName(), topology0Processor0);
+    nodes.put(topology0Processor1.getName(), topology0Processor1);
+    nodes.put(topology0Processor2.getName(), topology0Processor2);
+
+    subTopology.setNodes(nodes);
     subTopologyNode.setSubTopology(subTopology);
     return subTopologyNode;
   }
