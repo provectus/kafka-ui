c035f58f5a9f4d262ff7b5a611a7605a9e0f2d8f
MBovtriukProvectus <72087783+MBovtriukProvectus@users.noreply.github.com>
2021-07-19 12:11:53 +0300

    Review all tables and unify the way of data displaying. #602 (#687)

--- kafka-ui-react-app/src/components/ConsumerGroups/List/List.tsx
+++ kafka-ui-react-app/src/components/ConsumerGroups/List/List.tsx
@@ -22,51 +22,52 @@ const List: React.FC<Props> = ({ consumerGroups }) => {
       <Breadcrumb>All Consumer Groups</Breadcrumb>
 
       <div className="box">
-        {consumerGroups.length > 0 ? (
-          <div>
-            <div className="columns">
-              <div className="column is-half is-offset-half">
-                <input
-                  id="searchText"
-                  type="text"
-                  name="searchText"
-                  className="input"
-                  placeholder="Search"
-                  value={searchText}
-                  onChange={handleInputChange}
-                />
-              </div>
+        <div>
+          <div className="columns">
+            <div className="column is-half is-offset-half">
+              <input
+                id="searchText"
+                type="text"
+                name="searchText"
+                className="input"
+                placeholder="Search"
+                value={searchText}
+                onChange={handleInputChange}
+              />
             </div>
-            <table className="table is-striped is-fullwidth is-hoverable">
-              <thead>
-                <tr>
-                  <th>Consumer group ID</th>
-                  <th>Num of members</th>
-                  <th>Num of topics</th>
-                  <th>Messages behind</th>
-                  <th>Coordinator</th>
-                  <th>State</th>
-                </tr>
-              </thead>
-              <tbody>
-                {consumerGroups
-                  .filter(
-                    (consumerGroup) =>
-                      !searchText ||
-                      consumerGroup?.groupId?.indexOf(searchText) >= 0
-                  )
-                  .map((consumerGroup) => (
-                    <ListItem
-                      key={consumerGroup.groupId}
-                      consumerGroup={consumerGroup}
-                    />
-                  ))}
-              </tbody>
-            </table>
           </div>
-        ) : (
-          'No active consumer groups'
-        )}
+          <table className="table is-striped is-fullwidth is-hoverable">
+            <thead>
+              <tr>
+                <th>Consumer group ID</th>
+                <th>Num of members</th>
+                <th>Num of topics</th>
+                <th>Messages behind</th>
+                <th>Coordinator</th>
+                <th>State</th>
+              </tr>
+            </thead>
+            <tbody>
+              {consumerGroups
+                .filter(
+                  (consumerGroup) =>
+                    !searchText ||
+                    consumerGroup?.groupId?.indexOf(searchText) >= 0
+                )
+                .map((consumerGroup) => (
+                  <ListItem
+                    key={consumerGroup.groupId}
+                    consumerGroup={consumerGroup}
+                  />
+                ))}
+              {consumerGroups.length === 0 && (
+                <tr>
+                  <td colSpan={10}>No active consumer groups</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
