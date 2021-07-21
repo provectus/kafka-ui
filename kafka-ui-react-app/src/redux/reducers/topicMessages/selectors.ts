c491ae95afc682fca89d91e1384cea75ddde2c62
Oleg Shuralev <workshur@gmail.com>
2021-07-22 02:30:10 +0300

    Specs

--- kafka-ui-react-app/src/redux/reducers/topicMessages/selectors.ts
+++ kafka-ui-react-app/src/redux/reducers/topicMessages/selectors.ts
@@ -18,23 +18,3 @@ export const getTopicMessgesMeta = createSelector(
   topicMessagesState,
   ({ meta }) => meta
 );
-
-export const getTopicMessgesBytesConsumed = createSelector(
-  getTopicMessgesMeta,
-  ({ bytesConsumed }) => bytesConsumed
-);
-
-export const getTopicMessgesElapsedMs = createSelector(
-  getTopicMessgesMeta,
-  ({ elapsedMs }) => elapsedMs
-);
-
-export const getTopicMessgesMessagesConsumed = createSelector(
-  getTopicMessgesMeta,
-  ({ messagesConsumed }) => messagesConsumed
-);
-
-export const getTopicMessgesIsCancelled = createSelector(
-  getTopicMessgesMeta,
-  ({ isCancelled }) => isCancelled
-);
