dd09ae3b069a9494d56a51d700132e367b93f8d0
Azat Belgibayev <belg.azat@gmail.com>
2021-08-11 21:43:27 +0300

    Add KsqlDb List component tests

---
+++ kafka-ui-react-app/src/components/KsqlDb/List/__test__/List.spec.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import List from 'components/KsqlDb/List/List';
+import { mount, shallow } from 'enzyme';
+import { StaticRouter } from 'react-router';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { RootState } from 'redux/interfaces';
+
+const emptyPlaceholder = 'No tables or streams found';
+
+const mockStore = configureStore();
+
+describe('KsqlDb List', () => {
+  const pathname = `ui/clusters/local/ksql-db`;
+
+  it('Renders placeholder on empty data', () => {
+    const initialState: Partial<RootState> = {
+      ksqlDb: {
+        tables: [],
+        streams: [],
+      },
+      loader: {
+        GET_KSQL_DB_TABLES_AND_STREAMS: 'fetched',
+      },
+    };
+    const store = mockStore(initialState);
+
+    const component = mount(
+      <StaticRouter location={{ pathname }} context={{}}>
+        <Provider store={store}>
+          <List />
+        </Provider>
+      </StaticRouter>
+    );
+
+    expect(
+      component.find({ children: emptyPlaceholder }).exists()
+    ).toBeTruthy();
+  });
+});
