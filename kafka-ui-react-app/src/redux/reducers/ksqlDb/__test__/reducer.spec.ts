d26864619a9420772b979dbce425212edbc3564d
Azat Belgibayev <belg.azat@gmail.com>
2021-08-16 15:35:28 +0300

    Add ksqldb reducer tests

--- kafka-ui-react-app/src/redux/reducers/ksqlDb/__test__/reducer.spec.ts
+++ kafka-ui-react-app/src/redux/reducers/ksqlDb/__test__/reducer.spec.ts
@@ -1,4 +1,4 @@
-import { fetchKsqlDbTablesAction } from 'redux/actions';
+import { fetchKsqlDbTablesAction, resetExecutionResult } from 'redux/actions';
 import reducer, { initialState } from 'redux/reducers/ksqlDb/reducer';
 
 import { fetchKsqlDbTablesPayload } from './fixtures';
@@ -9,13 +9,27 @@ describe('KsqlDb reducer', () => {
       initialState
     );
   });
-  it('Adds new message', () => {
+  it('Fetches tables and streams', () => {
     const state = reducer(
       undefined,
       fetchKsqlDbTablesAction.success(fetchKsqlDbTablesPayload)
     );
     expect(state.tables.length).toEqual(2);
     expect(state.streams.length).toEqual(2);
+    expect(state).toMatchSnapshot();
+  });
+  it('Resets execution result', () => {
+    const state = reducer(
+      {
+        tables: [],
+        streams: [],
+        executionResult: {
+          message: 'No available data',
+        },
+      },
+      resetExecutionResult()
+    );
+    expect(state.executionResult).toEqual(null);
     expect(state).toMatchSnapshot();
   });
 });
