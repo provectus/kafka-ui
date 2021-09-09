aaa2ed2f972f9973875509b5f1a76ad6da74ce9e
Anna Antipova <aantipova@provectus.com>
2021-09-09 16:28:17 +0300

    #627 add separate config file to connector deletion test

--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/topics/ConnectorsTests.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/topics/ConnectorsTests.java
@@ -11,7 +11,8 @@ public class ConnectorsTests extends BaseTest {
     public static final String LOCAL = "local";
     public static final String SINK_CONNECTOR = "sink_postgres_activities_e2e_checks";
     public static final String TOPIC_FOR_CONNECTOR = "topic_for_connector";
-    public static final String TOPIC_FOR_UPDATED_CONNECTOR = "topic_for_update_connector";
+    public static final String TOPIC_FOR_UPDATE_CONNECTOR = "topic_for_update_connector";
+    public static final String TOPIC_FOR_DELETE_CONNECTOR = "topic_for_delete_connector";
     public static final String FIRST = "first";
     public static final String CONNECTOR_FOR_DELETE = "sink_postgres_activities_e2e_checks_for_delete";
     public static final String CONNECTOR_FOR_UPDATE = "sink_postgres_activities_e2e_checks_for_update";
@@ -79,14 +80,18 @@ public class ConnectorsTests extends BaseTest {
     @DisplayName("should delete connector")
     @Test
     void deleteConnector() {
+        Helpers.INSTANCE.apiHelper.createTopic(LOCAL, TOPIC_FOR_DELETE_CONNECTOR);
+        Helpers.INSTANCE.apiHelper.sendMessage(LOCAL, TOPIC_FOR_DELETE_CONNECTOR,
+                FileUtils.getResourceAsString("message_content_create_topic.json.json"), " ");
         Helpers.INSTANCE.apiHelper.createConnector(LOCAL, FIRST,
                 CONNECTOR_FOR_DELETE,
-                FileUtils.getResourceAsString("create_connector_api_config.json"));
+                FileUtils.getResourceAsString("delete_connector_config.json"));
         pages.openConnectorsList(LOCAL)
                 .isOnPage()
                 .openConnector(CONNECTOR_FOR_DELETE);
         pages.openConnectorsView(LOCAL, CONNECTOR_FOR_DELETE)
                 .clickDeleteButton();
         pages.openConnectorsList(LOCAL).isNotVisible(CONNECTOR_FOR_DELETE);
+        Helpers.INSTANCE.apiHelper.deleteTopic(LOCAL, TOPIC_FOR_DELETE_CONNECTOR);
     }
 }
