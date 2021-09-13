8c2698ea3a7c9184f6995171b7a6e14633580822
sergei <scheremnov@provectus.com>
2021-09-13 20:26:22 +0300

    remove ternary operator from style

--- kafka-ui-react-app/src/components/Nav/ClusterMenuItem/ClusterMenuItem.styled.ts
+++ kafka-ui-react-app/src/components/Nav/ClusterMenuItem/ClusterMenuItem.styled.ts
@@ -8,29 +8,32 @@ const StyledMenuItem = styled.li<MenuItemProps>`
   font-weight: normal;
   font-size: 14px;
 
-  ${({ to, theme, liType }) =>
-    to
-      ? `
-        & a {
-          padding: 0.5em 0.75em;
-          cursor: pointer;
-          text-decoration: none;
-          margin: 0px 0px;
-          line-height: 20px;
-          background-color: ${theme.liStyles[liType].backgroundColor.normal};
-          color: ${theme.liStyles[liType].color.normal};
+  ${(props) => {
+    if (!props.to) return `padding: 0.5em 0.75em;`;
 
-          &.hover {
-            background-color: ${theme.liStyles[liType].backgroundColor.hover};
-            color: ${theme.liStyles[liType].color.hover};
-          }
-          &.is-active {
-            background-color: ${theme.liStyles[liType].backgroundColor.active};
-            color: ${theme.liStyles[liType].color.active};
-          }
+    const { theme, liType } = props;
+
+    return `
+      & a {
+        padding: 0.5em 0.75em;
+        cursor: pointer;
+        text-decoration: none;
+        margin: 0px 0px;
+        line-height: 20px;
+        background-color: ${theme.liStyles[liType].backgroundColor.normal};
+        color: ${theme.liStyles[liType].color.normal};
+
+        &.hover {
+          background-color: ${theme.liStyles[liType].backgroundColor.hover};
+          color: ${theme.liStyles[liType].color.hover};
         }
-    `
-      : `padding: 0.5em 0.75em;`}
+        &.is-active {
+          background-color: ${theme.liStyles[liType].backgroundColor.active};
+          color: ${theme.liStyles[liType].color.active};
+        }
+      }
+    `;
+  }}
 `;
 
 StyledMenuItem.displayName = 'StyledMenuItem';
