00ff3579f56f6e59ac1b954ea2677fd4318d9be0
Ilya Kuramshin <ikuramshin@provectus.com>
2021-09-15 19:38:39 +0300

    All AdminClient interactions moved to ReactiveAdminClient

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/AdminClientServiceImpl.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/AdminClientServiceImpl.java
@@ -1,7 +1,7 @@
 package com.provectus.kafka.ui.service;
 
-import com.provectus.kafka.ui.model.ExtendedAdminClient;
 import com.provectus.kafka.ui.model.KafkaCluster;
+import java.io.Closeable;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,21 +17,20 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 @Log4j2
-public class AdminClientServiceImpl implements AdminClientService {
-  private final Map<String, ExtendedAdminClient> adminClientCache = new ConcurrentHashMap<>();
+public class AdminClientServiceImpl implements AdminClientService, Closeable {
+  private final Map<String, ReactiveAdminClient> adminClientCache = new ConcurrentHashMap<>();
   @Setter // used in tests
   @Value("${kafka.admin-client-timeout}")
   private int clientTimeout;
 
   @Override
-  public Mono<ExtendedAdminClient> getOrCreateAdminClient(KafkaCluster cluster) {
+  public Mono<ReactiveAdminClient> get(KafkaCluster cluster) {
     return Mono.justOrEmpty(adminClientCache.get(cluster.getName()))
         .switchIfEmpty(createAdminClient(cluster))
         .map(e -> adminClientCache.computeIfAbsent(cluster.getName(), key -> e));
   }
 
-  @Override
-  public Mono<ExtendedAdminClient> createAdminClient(KafkaCluster cluster) {
+  private Mono<ReactiveAdminClient> createAdminClient(KafkaCluster cluster) {
     return Mono.fromSupplier(() -> {
       Properties properties = new Properties();
       properties.putAll(cluster.getProperties());
@@ -39,6 +38,11 @@ public class AdminClientServiceImpl implements AdminClientService {
           .put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, cluster.getBootstrapServers());
       properties.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, clientTimeout);
       return AdminClient.create(properties);
-    }).flatMap(ExtendedAdminClient::extendedAdminClient);
+    }).flatMap(ReactiveAdminClient::create);
+  }
+
+  @Override
+  public void close() {
+    adminClientCache.values().forEach(ReactiveAdminClient::close);
   }
 }
--- kafka-ui-api/src/test/java/com/provectus/kafka/ui/KafkaConsumerTests.java
+++ kafka-ui-api/src/test/java/com/provectus/kafka/ui/KafkaConsumerTests.java
@@ -103,7 +103,7 @@ public class KafkaConsumerTests extends AbstractBaseTest {
         .expectStatus()
         .isOk();
 
-    PartitionsIncreaseResponseDTO response = webTestClient.patch()
+    var r = webTestClient.patch()
         .uri("/api/clusters/{clusterName}/topics/{topicName}/partitions",
             LOCAL,
             topicName)
@@ -114,9 +114,9 @@ public class KafkaConsumerTests extends AbstractBaseTest {
         .expectStatus()
         .isOk()
         .expectBody(PartitionsIncreaseResponseDTO.class)
-        .returnResult()
-        .getResponseBody();
+        .returnResult();
 
+    PartitionsIncreaseResponseDTO response = r.getResponseBody();
     assert response != null;
     Assertions.assertEquals(10, response.getTotalPartitionsCount());
 
