ba43e549730e2831c6b74fba90879ce9b2562465
Alexander <mr.afigitelniychuvak@gmail.com>
2021-09-16 11:25:34 +0300

    Use json-schema-faker and ajv

--- kafka-ui-react-app/src/components/Topics/Topic/SendMessage/validateMessage.ts
+++ kafka-ui-react-app/src/components/Topics/Topic/SendMessage/validateMessage.ts
@@ -1,57 +1,54 @@
 import { TopicMessageSchema } from 'generated-sources';
-import convertToYup from 'json-schema-yup-transformer';
+import Ajv from 'ajv/dist/2020';
 
 const validateMessage = async (
   key: string,
   content: string,
   messageSchema: TopicMessageSchema | undefined,
-  setSchemaErrorString: React.Dispatch<React.SetStateAction<string>>
+  setSchemaErrors: React.Dispatch<React.SetStateAction<string[]>>
 ): Promise<boolean> => {
-  setSchemaErrorString('');
+  setSchemaErrors([]);
+  const ajv = new Ajv();
   try {
     if (messageSchema) {
-      const validateKey = convertToYup(JSON.parse(messageSchema.key.schema));
-      const validateContent = convertToYup(
-        JSON.parse(messageSchema.value.schema)
-      );
       let keyIsValid = false;
       let contentIsValid = false;
 
       try {
-        await validateKey?.validate(JSON.parse(key));
-        keyIsValid = true;
-      } catch (err) {
-        let errorString = '';
-        if (err.errors) {
-          err.errors.forEach((e: string) => {
-            errorString = errorString ? `${errorString}-Key ${e}` : `Key ${e}`;
-          });
-        } else {
-          errorString = errorString
-            ? `${errorString}-Key ${err.message}`
-            : `Key ${err.message}`;
+        const validateKey = ajv.compile(JSON.parse(messageSchema.key.schema));
+        keyIsValid = validateKey(JSON.parse(key));
+        if (!keyIsValid) {
+          const errorString: string[] = [];
+          if (validateKey.errors) {
+            validateKey.errors.forEach((e) => {
+              errorString.push(
+                `${e.schemaPath.replace('#', 'Key')} ${e.message}`
+              );
+            });
+            setSchemaErrors((e) => [...e, ...errorString]);
+          }
         }
-
-        setSchemaErrorString((e) => (e ? `${e}-${errorString}` : errorString));
+      } catch (err) {
+        setSchemaErrors((e) => [...e, `Key ${err.message}`]);
       }
       try {
-        await validateContent?.validate(JSON.parse(content));
-        contentIsValid = true;
-      } catch (err) {
-        let errorString = '';
-        if (err.errors) {
-          err.errors.forEach((e: string) => {
-            errorString = errorString
-              ? `${errorString}-Content ${e}`
-              : `Content ${e}`;
-          });
-        } else {
-          errorString = errorString
-            ? `${errorString}-Content ${err.message}`
-            : `Content ${err.message}`;
+        const validateContent = ajv.compile(
+          JSON.parse(messageSchema.value.schema)
+        );
+        contentIsValid = validateContent(JSON.parse(content));
+        if (!contentIsValid) {
+          const errorString: string[] = [];
+          if (validateContent.errors) {
+            validateContent.errors.forEach((e) => {
+              errorString.push(
+                `${e.schemaPath.replace('#', 'Content')} ${e.message}`
+              );
+            });
+            setSchemaErrors((e) => [...e, ...errorString]);
+          }
         }
-
-        setSchemaErrorString((e) => (e ? `${e}-${errorString}` : errorString));
+      } catch (err) {
+        setSchemaErrors((e) => [...e, `Content ${err.message}`]);
       }
 
       if (keyIsValid && contentIsValid) {
@@ -59,7 +56,7 @@ const validateMessage = async (
       }
     }
   } catch (err) {
-    setSchemaErrorString((e) => (e ? `${e}-${err.message}` : err.message));
+    setSchemaErrors((e) => (e ? `${e}-${err.message}` : err.message));
   }
   return false;
 };
