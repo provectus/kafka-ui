7a45acbb882f7c68c98117eed6f024c3ad5e6040
German Osin <german.osin@gmail.com>
2021-09-17 11:23:06 +0300

    ISSUE-886 Schema registry multiple urls

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/model/InternalSchemaRegistry.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/model/InternalSchemaRegistry.java
@@ -1,5 +1,6 @@
 package com.provectus.kafka.ui.model;
 
+import java.util.List;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,5 +9,10 @@ import lombok.Data;
 public class InternalSchemaRegistry {
   private final String username;
   private final String password;
-  private final String url;
+  private final List<String> url;
+
+  public String getFirstUrl() {
+    return url != null  && !url.isEmpty() ? url.iterator().next() : null;
+  }
+
 }
