2184e671317144b2a89a46ca367375b684f1060f
Alexander Krivonosov <31561808+GneyHabub@users.noreply.github.com>
2021-09-22 10:38:50 +0300

    Custom select (#896)
    * Implement custom select

---
+++ kafka-ui-react-app/src/components/common/Select/Select.styled.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+interface Poprs {
+  selectSize: 'M' | 'L';
+  isLive?: boolean;
+}
+
+const StyledSelect = styled.select<Poprs>`
+  height: ${(props) => (props.selectSize === 'M' ? '32px' : '40px')};
+  border: 1px ${(props) => props.theme.selectStyles.borderColor.normal} solid;
+  border-radius: 4px;
+  font-size: 14px;
+  width: 100%;
+  padding-left: ${(props) => (props.isLive ? '36px' : '12px')};
+  color: ${(props) => props.theme.selectStyles.color.normal};
+
+  background-image: url('data:image/svg+xml,%3Csvg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath d="M1 1L5 5L9 1" stroke="%23454F54"/%3E%3C/svg%3E%0A') !important;
+  background-repeat: no-repeat !important;
+  background-position-x: 97% !important;
+  background-position-y: 50% !important;
+  appearance: none !important;
+
+  &:hover {
+    color: ${(props) => props.theme.selectStyles.color.hover};
+    border-color: ${(props) => props.theme.selectStyles.borderColor.hover};
+  }
+  &:focus {
+    outline: none;
+    color: ${(props) => props.theme.selectStyles.color.active};
+    border-color: ${(props) => props.theme.selectStyles.borderColor.active};
+  }
+  &:disabled {
+    color: ${(props) => props.theme.selectStyles.color.disabled};
+    border-color: ${(props) => props.theme.selectStyles.borderColor.disabled};
+    cursor: not-allowed;
+  }
+`;
+
+export default StyledSelect;
