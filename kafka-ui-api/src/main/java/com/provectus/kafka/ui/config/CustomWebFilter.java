90e629e2a2b8881187494201400ad645307f8301
German Osin <german.osin@gmail.com>
2021-10-01 00:23:43 +0300

    ISSUE-861 Context path fix

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/config/CustomWebFilter.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/config/CustomWebFilter.java
@@ -1,6 +1,5 @@
 package com.provectus.kafka.ui.config;
 
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -8,32 +7,25 @@ import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Component
-
 public class CustomWebFilter implements WebFilter {
-
-  private final ServerProperties serverProperties;
-
-  public CustomWebFilter(ServerProperties serverProperties) {
-    this.serverProperties = serverProperties;
-  }
 
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-    String contextPath = serverProperties.getServlet().getContextPath() != null 
-        ? serverProperties.getServlet().getContextPath() : "";
 
-    final String path = exchange.getRequest().getURI().getPath().replaceAll("/$", "");
-    if (path.equals(contextPath) || path.startsWith(contextPath + "/ui")) {
+    final String basePath = exchange.getRequest().getPath().contextPath().value();
+
+    final String path = exchange.getRequest().getPath().pathWithinApplication().value();
+
+    if (path.startsWith("/ui") || path.equals("/")) {
       return chain.filter(
-          exchange.mutate().request(exchange.getRequest().mutate().path("/index.html").build())
-              .build()
+          exchange.mutate().request(
+              exchange.getRequest().mutate()
+                  .path(basePath + "/index.html")
+                  .contextPath(basePath)
+                  .build()
+          ).build()
       );
-    } else if (path.startsWith(contextPath)) {
-      return chain.filter(
-          exchange.mutate().request(exchange.getRequest().mutate().contextPath(contextPath).build())
-              .build()
-      );
-    }    
+    }
 
     return chain.filter(exchange);
   }
