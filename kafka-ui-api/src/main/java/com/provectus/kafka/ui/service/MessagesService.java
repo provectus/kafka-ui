4efd0afd4a6daa7fd7e0bc31c4bc5d8d22ea64e3
Ilya Kuramshin <ikuramshin@provectus.com>
2021-10-13 15:30:02 +0300

    checkstyle fix

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/MessagesService.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/MessagesService.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.Consumer;
@@ -42,8 +43,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
-
-import javax.annotation.Nullable;
 
 @Service
 @RequiredArgsConstructor
