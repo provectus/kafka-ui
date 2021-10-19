5101aa857f1ce55e62b7d2cc7fa85d1405903825
Anna Antipova <aantipova@provectus.com>
2021-10-19 22:11:29 +0300

    #626 increase the waiting time for the topic update to 5 minutes

--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/extensions/WaitUtils.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/extensions/WaitUtils.java
@@ -25,7 +25,7 @@ public class WaitUtils {
             refresh();
             i++;
             sleep(2000);
-        } while (!selectedValue.equals(element.getSelectedValue()) && i != 120);
+        } while (!selectedValue.equals(element.getSelectedValue()) && i != 300);
         Assertions.assertEquals(selectedValue, element.getSelectedValue()) ;
     }
 }
