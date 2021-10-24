19e68340d0d2e84f955de0add4514851757c21c4
Anna Antipova <aantipova@provectus.com>
2021-10-24 23:36:27 +0300

    #627 reduce code duplication

--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/topics/ConnectorsTests.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/topics/ConnectorsTests.java
@@ -2,6 +2,7 @@ package com.provectus.kafka.ui.topics;
 
 import com.provectus.kafka.ui.base.BaseTest;
 import com.provectus.kafka.ui.extensions.FileUtils;
+import com.provectus.kafka.ui.helpers.ApiHelper;
 import com.provectus.kafka.ui.helpers.Helpers;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.*;
@@ -20,59 +21,61 @@ public class ConnectorsTests extends BaseTest {
     @BeforeAll
     @SneakyThrows
     public static void beforeAll() {
-        Helpers.INSTANCE.apiHelper.createTopic(LOCAL, TOPIC_FOR_CONNECTOR);
-        Helpers.INSTANCE.apiHelper.sendMessage(LOCAL, TOPIC_FOR_CONNECTOR,
-                FileUtils.getResourceAsString("message_content_create_topic.json"), " ");
-        Helpers.INSTANCE.apiHelper.createTopic(LOCAL, TOPIC_FOR_UPDATE_CONNECTOR);
-        Helpers.INSTANCE.apiHelper.sendMessage(LOCAL, TOPIC_FOR_UPDATE_CONNECTOR,
-                FileUtils.getResourceAsString("message_content_create_topic.json"), " ");
-        Helpers.INSTANCE.apiHelper.createConnector(LOCAL, FIRST, CONNECTOR_FOR_UPDATE,
-                FileUtils.getResourceAsString("config_for_create_connector_via_api.json"));
-        Helpers.INSTANCE.apiHelper.createTopic(LOCAL, TOPIC_FOR_DELETE_CONNECTOR);
-        Helpers.INSTANCE.apiHelper.sendMessage(LOCAL, TOPIC_FOR_DELETE_CONNECTOR,
-                FileUtils.getResourceAsString("message_content_create_topic.json"), " ");
-        Helpers.INSTANCE.apiHelper.createConnector(LOCAL, FIRST,
-                CONNECTOR_FOR_DELETE,
-                FileUtils.getResourceAsString("delete_connector_config.json"));
+        ApiHelper apiHelper = Helpers.INSTANCE.apiHelper;
+
+        String message = FileUtils.getResourceAsString("message_content_create_topic.json");
+        String createConfig = FileUtils.getResourceAsString("config_for_create_connector_via_api.json");
+        String deleteConfig = FileUtils.getResourceAsString("delete_connector_config.json");
+
+        apiHelper.createTopic(LOCAL, TOPIC_FOR_CONNECTOR);
+        apiHelper.sendMessage(LOCAL, TOPIC_FOR_CONNECTOR, message, " ");
+        apiHelper.createTopic(LOCAL, TOPIC_FOR_UPDATE_CONNECTOR);
+        apiHelper.sendMessage(LOCAL, TOPIC_FOR_UPDATE_CONNECTOR, message, " ");
+        apiHelper.createConnector(LOCAL, FIRST, CONNECTOR_FOR_UPDATE, createConfig);
+        apiHelper.createTopic(LOCAL, TOPIC_FOR_DELETE_CONNECTOR);
+        apiHelper.sendMessage(LOCAL, TOPIC_FOR_DELETE_CONNECTOR, message, " ");
+        apiHelper.createConnector(LOCAL, FIRST, CONNECTOR_FOR_DELETE, deleteConfig);
     }
 
     @AfterAll
     @SneakyThrows
     public static void afterAll() {
-      Helpers.INSTANCE.apiHelper.deleteConnector(LOCAL, FIRST, SINK_CONNECTOR);
-      Helpers.INSTANCE.apiHelper.deleteConnector(LOCAL, FIRST, CONNECTOR_FOR_UPDATE);
-      Helpers.INSTANCE.apiHelper.deleteTopic(LOCAL, TOPIC_FOR_UPDATE_CONNECTOR);
-      Helpers.INSTANCE.apiHelper.deleteTopic(LOCAL, TOPIC_FOR_DELETE_CONNECTOR);
-      Helpers.INSTANCE.apiHelper.deleteTopic(LOCAL, TOPIC_FOR_CONNECTOR);
+        ApiHelper apiHelper = Helpers.INSTANCE.apiHelper;
+
+        apiHelper.deleteConnector(LOCAL, FIRST, SINK_CONNECTOR);
+        apiHelper.deleteConnector(LOCAL, FIRST, CONNECTOR_FOR_UPDATE);
+        apiHelper.deleteTopic(LOCAL, TOPIC_FOR_UPDATE_CONNECTOR);
+        apiHelper.deleteTopic(LOCAL, TOPIC_FOR_DELETE_CONNECTOR);
+        apiHelper.deleteTopic(LOCAL, TOPIC_FOR_CONNECTOR);
     }
 
     @SneakyThrows
     @DisplayName("should create a connector")
     @Test
     void createConnector() {
-            pages.openConnectorsList(LOCAL)
-                    .isOnPage()
-                    .clickCreateConnectorButton()
-                    .setConnectorConfig(
-                            SINK_CONNECTOR,
-                            FileUtils.getResourceAsString("config_for_create_connector.json")
-                    )
-                    .connectorIsVisible();
-            pages.openConnectorsList(LOCAL).connectorIsVisibleInList(SINK_CONNECTOR, TOPIC_FOR_CONNECTOR);
+        pages.openConnectorsList(LOCAL)
+                .isOnPage()
+                .clickCreateConnectorButton()
+                .setConnectorConfig(
+                        SINK_CONNECTOR,
+                        FileUtils.getResourceAsString("config_for_create_connector.json")
+                )
+                .connectorIsVisible();
+        pages.openConnectorsList(LOCAL).connectorIsVisibleInList(SINK_CONNECTOR, TOPIC_FOR_CONNECTOR);
     }
 
     @SneakyThrows
     @DisplayName("should update a connector")
     @Test
     void updateConnector() {
-            pages.openConnectorsList(LOCAL)
-                    .isOnPage()
-                    .openConnector(CONNECTOR_FOR_UPDATE);
-            pages.openConnectorsView(LOCAL, CONNECTOR_FOR_UPDATE)
-                    .openEditConfig()
-                    .updateConnectorConfig(
-                            FileUtils.getResourceAsString("config_for_update_connector.json"));
-            pages.openConnectorsList(LOCAL).connectorIsVisibleInList(CONNECTOR_FOR_UPDATE, TOPIC_FOR_UPDATE_CONNECTOR);
+        pages.openConnectorsList(LOCAL)
+                .isOnPage()
+                .openConnector(CONNECTOR_FOR_UPDATE);
+        pages.openConnectorsView(LOCAL, CONNECTOR_FOR_UPDATE)
+                .openEditConfig()
+                .updateConnectorConfig(
+                        FileUtils.getResourceAsString("config_for_update_connector.json"));
+        pages.openConnectorsList(LOCAL).connectorIsVisibleInList(CONNECTOR_FOR_UPDATE, TOPIC_FOR_UPDATE_CONNECTOR);
     }
 
     @SneakyThrows
