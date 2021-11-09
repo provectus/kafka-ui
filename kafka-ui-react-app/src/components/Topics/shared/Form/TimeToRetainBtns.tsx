7603199e5323302a804b99be473b55176d76e8a6
Roman Zabaluev <rzabaluev@provectus.com>
2021-11-09 15:09:09 +0300

    Fix 4w time to retain selector value

--- kafka-ui-react-app/src/components/Topics/shared/Form/TimeToRetainBtns.tsx
+++ kafka-ui-react-app/src/components/Topics/shared/Form/TimeToRetainBtns.tsx
@@ -29,7 +29,7 @@ const TimeToRetainBtns: React.FC<Props> = ({ name }) => (
     <TimeToRetainBtn
       text="4w"
       inputName={name}
-      value={MILLISECONDS_IN_DAY * 7 * 24}
+      value={MILLISECONDS_IN_DAY * 7 * 4}
     />
   </div>
 );
