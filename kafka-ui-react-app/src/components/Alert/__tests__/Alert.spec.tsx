7277766e2379f491c66c1c651e028cd0ef377de9
Oleg Shur <workshur@gmail.com>
2021-11-25 11:31:33 +0300

    Refactor Alerts component (#1124)

--- kafka-ui-react-app/src/components/Alert/__tests__/Alert.spec.tsx
+++ kafka-ui-react-app/src/components/Alert/__tests__/Alert.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Alert as AlertProps } from 'redux/interfaces';
 import * as actions from 'redux/actions/actions';
 import Alert from 'components/Alert/Alert';
@@ -13,72 +13,59 @@ const httpStatusText = 'My Status Text';
 const dismiss = jest.fn();
 
 describe('Alert', () => {
-  const setupComponent = (props: Partial<AlertProps> = {}) => (
-    <Alert
-      id={id}
-      type="error"
-      title={title}
-      message={message}
-      createdAt={1234567}
-      {...props}
-    />
-  );
-
+  const setupComponent = (props: Partial<AlertProps> = {}) =>
+    render(
+      <Alert
+        id={id}
+        type="error"
+        title={title}
+        message={message}
+        createdAt={1234567}
+        {...props}
+      />
+    );
   it('renders with initial props', () => {
-    const wrapper = mount(setupComponent());
-    expect(wrapper.exists('.alert-title')).toBeTruthy();
-    expect(wrapper.find('.alert-title').text()).toEqual(title);
-    expect(wrapper.exists('.alert-message')).toBeTruthy();
-    expect(wrapper.find('.alert-message').text()).toEqual(message);
-    expect(wrapper.exists('span')).toBeTruthy();
+    setupComponent();
+    expect(screen.getByRole('heading')).toHaveTextContent(title);
+    expect(screen.getByRole('contentinfo')).toHaveTextContent(message);
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
-
   it('renders alert with server side message', () => {
-    const wrapper = mount(
-      setupComponent({
-        type: 'info',
-        response: {
-          status: statusCode,
-          statusText: 'My Status Text',
-          body: {
-            message: serverSideMessage,
-          },
+    setupComponent({
+      type: 'info',
+      response: {
+        status: statusCode,
+        statusText: 'My Status Text',
+        body: {
+          message: serverSideMessage,
         },
-      })
-    );
-    expect(wrapper.exists('.alert-server-response')).toBeTruthy();
-    expect(wrapper.find('.alert-server-response').text()).toEqual(
-      `${statusCode} ${serverSideMessage}`
-    );
-  });
-
-  it('renders alert with http status text', () => {
-    const wrapper = mount(
-      setupComponent({
-        type: 'info',
-        response: {
-          status: statusCode,
-          statusText: httpStatusText,
-          body: {},
-        },
-      })
-    );
-    expect(wrapper.exists('.alert-server-response')).toBeTruthy();
-    expect(wrapper.find('.alert-server-response').text()).toEqual(
-      `${statusCode} ${httpStatusText}`
-    );
-  });
-
-  it('matches snapshot', () => {
-    expect(mount(setupComponent())).toMatchSnapshot();
-  });
-
-  describe('dismiss', () => {
-    it('handles dismiss callback', () => {
-      jest.spyOn(actions, 'dismissAlert').mockImplementation(dismiss);
-      const wrapper = mount(setupComponent());
-      wrapper.find('span').simulate('click');
-      expect(dismiss).toHaveBeenCalledWith(id);
+      },
     });
+    expect(screen.getAllByRole('contentinfo').length).toEqual(2);
+    expect(screen.getByText(message)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${statusCode} ${serverSideMessage}`)
+    ).toBeInTheDocument();
+  });
+  it('renders alert with http status text', () => {
+    setupComponent({
+      type: 'warning',
+      response: {
+        status: statusCode,
+        statusText: httpStatusText,
+        body: {},
+      },
+    });
+    expect(screen.getAllByRole('contentinfo').length).toEqual(2);
+    expect(screen.getByText(message)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${statusCode} ${httpStatusText}`)
+    ).toBeInTheDocument();
+  });
+  it('handles dismiss callback', () => {
+    jest.spyOn(actions, 'dismissAlert').mockImplementation(dismiss);
+    setupComponent();
+    fireEvent.click(screen.getByRole('button'));
+    expect(dismiss).toHaveBeenCalledWith(id);
   });
 });
