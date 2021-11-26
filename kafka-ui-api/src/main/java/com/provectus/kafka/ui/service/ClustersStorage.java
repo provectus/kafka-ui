b4d95d86241c60d958dc4fbc06e73f89693ef70b
iliax <ikuramshin@provectus.com>
2021-11-26 13:38:09 +0300

    add respect to cluster's order in ClustersStorage

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/ClustersStorage.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/ClustersStorage.java
@@ -1,33 +1,25 @@
 package com.provectus.kafka.ui.service;
 
+import com.google.common.collect.ImmutableMap;
 import com.provectus.kafka.ui.config.ClustersProperties;
 import com.provectus.kafka.ui.mapper.ClusterMapper;
 import com.provectus.kafka.ui.model.KafkaCluster;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import javax.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class ClustersStorage {
 
-  private final Map<String, KafkaCluster> kafkaClusters = new ConcurrentHashMap<>();
+  private final ImmutableMap<String, KafkaCluster> kafkaClusters;
 
-  private final ClustersProperties clusterProperties;
-
-  private final ClusterMapper clusterMapper = Mappers.getMapper(ClusterMapper.class);
-
-  @PostConstruct
-  public void init() {
-    for (ClustersProperties.Cluster clusterProperties : clusterProperties.getClusters()) {
-      KafkaCluster cluster = clusterMapper.toKafkaCluster(clusterProperties);
-      kafkaClusters.put(clusterProperties.getName(), cluster);
-    }
+  public ClustersStorage(ClustersProperties properties) {
+    var mapper = Mappers.getMapper(ClusterMapper.class);
+    var builder = ImmutableMap.<String, KafkaCluster>builder();
+    properties.getClusters().forEach(c -> builder.put(c.getName(), mapper.toKafkaCluster(c)));
+    this.kafkaClusters = builder.build();
   }
 
   public Collection<KafkaCluster> getKafkaClusters() {
