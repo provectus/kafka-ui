d1623b22fe0f8aee074654ad9cc55fc2bc9fdb80
NelyDavtyan <96067981+NelyDavtyan@users.noreply.github.com>
2021-12-29 12:51:25 +0400

    Updated topic URP status color.

--- kafka-ui-react-app/src/components/Topics/Topic/SendMessage/SendMessage.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/SendMessage/SendMessage.tsx
@@ -106,8 +106,8 @@ const SendMessage: React.FC = () => {
           clusterName,
           topicName,
           createTopicMessage: {
-            key: !key ? null : key,
-            content: !content ? null : content,
+            key: !key ? undefined : key,
+            content: !content ? undefined : content,
             headers,
             partition,
           },
