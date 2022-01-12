9301aaf5fb6f4006d205b92e2e8b4d5c3dbb1fee
NelyDavtyan <96067981+NelyDavtyan@users.noreply.github.com>
2022-01-12 15:22:46 +0400

    Added test for topic detail overview.

--- kafka-ui-react-app/src/components/Topics/Topic/Details/Overview/__test__/Overview.spec.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Details/Overview/__test__/Overview.spec.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { screen } from '@testing-library/react';
+import { render } from 'lib/testHelpers';
 import Overview from 'components/Topics/Topic/Details/Overview/Overview';
+import { Colors } from 'theme/theme';
 
 describe('Overview', () => {
   const mockInternal = false;
   const mockClusterName = 'local';
   const mockTopicName = 'topic';
+  const mockUnderReplicatedPartitions = 1;
+  const mockInSyncReplicas = 1;
+  const mockReplicas = 0;
   const mockClearTopicMessages = jest.fn();
   const mockPartitions = [
     {
@@ -22,6 +28,20 @@ describe('Overview', () => {
       offsetMin: 0,
     },
   ];
+  const renderComponent = () =>
+    render(
+      <Overview
+        name={mockTopicName}
+        partitions={mockPartitions}
+        internal={mockInternal}
+        clusterName={mockClusterName}
+        topicName={mockTopicName}
+        clearTopicMessages={mockClearTopicMessages}
+        underReplicatedPartitions={mockUnderReplicatedPartitions}
+        inSyncReplicas={mockInSyncReplicas}
+        replicas={mockReplicas}
+      />
+    );
 
   describe('when it has internal flag', () => {
     it('does not render the Action button a Topic', () => {
@@ -52,6 +72,23 @@ describe('Overview', () => {
       );
 
       expect(componentEmpty.find('td').text()).toEqual('No Partitions found');
+    });
+  });
+
+  describe('should render circular alert', () => {
+    it('should be in document', () => {
+      renderComponent();
+      const circles = screen.getAllByRole('circle');
+      expect(circles[0]).toBeInTheDocument();
+      expect(circles[1]).toBeInTheDocument();
+      expect(circles.length).toEqual(2);
+    });
+
+    it('should be the appropriate color', () => {
+      renderComponent();
+      const circles = screen.getAllByRole('circle');
+      expect(circles[0]).toHaveStyle(`fill: ${Colors.green[40]}`);
+      expect(circles[1]).toHaveStyle(`fill: ${Colors.red[50]}`);
     });
   });
 });
