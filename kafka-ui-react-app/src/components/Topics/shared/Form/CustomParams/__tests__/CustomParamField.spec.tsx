fece48baf9ac19972d9c81d49383707e5cb9f5f2
Damir Abdulganiev <dabdulganiev@provectus.com>
2022-01-20 20:02:45 +0300

    Fixes regression

--- kafka-ui-react-app/src/components/Topics/shared/Form/CustomParams/__tests__/CustomParamField.spec.tsx
+++ kafka-ui-react-app/src/components/Topics/shared/Form/CustomParams/__tests__/CustomParamField.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {screen, waitFor, within} from '@testing-library/react';
+import { screen, waitFor, within } from '@testing-library/react';
 import { render } from 'lib/testHelpers';
 import CustomParamsField, {
   Props,
