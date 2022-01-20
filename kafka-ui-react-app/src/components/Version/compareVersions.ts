a9b1559b95a235c13dcec5a2238e5ec9c538bdea
Damir Abdulganiev <dabdulganiev@provectus.com>
2022-01-20 17:10:24 +0300

    Adds safeguard against strange cases

--- kafka-ui-react-app/src/components/Version/compareVersions.ts
+++ kafka-ui-react-app/src/components/Version/compareVersions.ts
@@ -6,18 +6,23 @@ const split = (v: string): string[] => {
 const compareVersions = (v1?: string, v2?: string): number => {
   if (!v1 || !v2) return 0;
 
-  const s1 = split(v1);
-  const s2 = split(v2);
+  // try..catch - is our safeguard for strange git tags (or usecases without network)
+  try {
+    const s1 = split(v1);
+    const s2 = split(v2);
 
-  for (let i = 0; i < Math.max(s1.length, s2.length); i += 1) {
-    const n1 = parseInt(s1[i] || '0', 10);
-    const n2 = parseInt(s2[i] || '0', 10);
+    for (let i = 0; i < Math.max(s1.length, s2.length); i += 1) {
+      const n1 = parseInt(s1[i] || '0', 10);
+      const n2 = parseInt(s2[i] || '0', 10);
 
-    if (n1 > n2) return 1;
-    if (n2 > n1) return -1;
+      if (n1 > n2) return 1;
+      if (n2 > n1) return -1;
+    }
+
+    return 0;
+  } catch (_) {
+    return 0;
   }
-
-  return 0;
 };
 
 export default compareVersions;
