8ec9d111b916c533695b7b17e6ae2b0fbbea8e4e
iliax <ikuramshin@provectus.com>
2022-01-25 15:36:42 +0300

    sse refactoring

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/controller/KsqlController.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/controller/KsqlController.java
@@ -3,13 +3,15 @@ package com.provectus.kafka.ui.controller;
 import com.provectus.kafka.ui.api.KsqlApi;
 import com.provectus.kafka.ui.model.KsqlCommandDTO;
 import com.provectus.kafka.ui.model.KsqlCommandResponseDTO;
+import com.provectus.kafka.ui.model.KsqlCommandV2DTO;
 import com.provectus.kafka.ui.model.KsqlResponseDTO;
 import com.provectus.kafka.ui.model.KsqlTableResponseDTO;
 import com.provectus.kafka.ui.service.KsqlService;
-import com.provectus.kafka.ui.service.ksql.KsqlApiClient;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import com.provectus.kafka.ui.service.ksql.KsqlServiceV2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -24,35 +26,43 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class KsqlController extends AbstractController implements KsqlApi {
   private final KsqlService ksqlService;
+  private final KsqlServiceV2 ksqlServiceV2;
 
   @Override
   public Mono<ResponseEntity<KsqlCommandResponseDTO>> executeKsqlCommand(String clusterName,
-                                                                      Mono<KsqlCommandDTO>
-                                                                          ksqlCommand,
-                                                                      ServerWebExchange exchange) {
+                                                                         Mono<KsqlCommandDTO>
+                                                                             ksqlCommand,
+                                                                         ServerWebExchange exchange) {
     return ksqlService.executeKsqlCommand(getCluster(clusterName), ksqlCommand)
         .map(ResponseEntity::ok);
   }
 
-  @SuppressWarnings("unchecked")
   @Override
-  public Mono<ResponseEntity<Flux<KsqlResponseDTO>>> executeKsql(String clusterName,
-                                                                 Mono<KsqlCommandDTO> ksqlCommand,
-                                                                 ServerWebExchange exchange) {
+  public Mono<ResponseEntity<Void>> executeKsql(String clusterName,
+                                                Mono<KsqlCommandV2DTO> ksqlCommandV2DTO,
+                                                ServerWebExchange exchange) {
+    return ksqlCommandV2DTO
+        .map(dto -> {
+              ksqlServiceV2.execute(
+                  getCluster(clusterName),
+                  dto.getResponsePipeId(),
+                  dto.getKsql(),
+                  Optional.ofNullable(dto.getStreamsProperties()).orElse(Map.of()));
+              return dto;
+            }
+        )
+        .map(dto -> ResponseEntity.ok().build());
+  }
+
+  @Override
+  public Mono<ResponseEntity<Flux<KsqlResponseDTO>>> openKsqlResponsePipe(String clusterName, String pipeId, ServerWebExchange exchange) {
     return Mono.just(
-        ResponseEntity.ok(
-            ksqlCommand
-                .flux()
-                .flatMap(command ->
-                    new KsqlApiClient(getCluster(clusterName))
-                        .execute(
-                            command.getKsql(),
-                            Optional.ofNullable(command.getStreamsProperties()).orElse(Map.of())))
-                .map(table -> new KsqlResponseDTO()
-                    .table(
-                        new KsqlTableResponseDTO()
-                            .header(table.getHeader())
-                            .columnNames(table.getColumnNames())
-                            .values((List<List<Object>>) ((List<?>) (table.getValues())))))));
+        ResponseEntity.ok(ksqlServiceV2.registerPipe(pipeId)
+            .map(table -> new KsqlResponseDTO()
+                .table(
+                    new KsqlTableResponseDTO()
+                        .header(table.getHeader())
+                        .columnNames(table.getColumnNames())
+                        .values((List<List<Object>>) ((List<?>) (table.getValues())))))));
   }
 }
