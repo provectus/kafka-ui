9e2be159bfb27b94bd506a97ed25c1acaa6c51f1
iliax <ikuramshin@provectus.com>
2022-01-25 18:03:28 +0300

    refactor

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/ksql/KsqlServiceV2.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/ksql/KsqlServiceV2.java
@@ -6,95 +6,62 @@ import com.provectus.kafka.ui.service.ksql.KsqlApiClient.KsqlResponseTable;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.TimeUnit;
-import lombok.SneakyThrows;
+import org.reactivestreams.Subscription;
 import org.springframework.stereotype.Service;
-import reactor.core.Disposable;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.SignalType;
 
 @Service
 public class KsqlServiceV2 {
 
-  private static final Object CLOSE_PIPE_MARKER = new Object();
-  private final Map<String, Pipe> registeredPipes = new ConcurrentHashMap<>();
-
   private static class Pipe {
-    SynchronousQueue<Object> queue = new SynchronousQueue<>();
-    volatile Disposable sourceSubscription;
-
-    @SneakyThrows
-    void err(Throwable th) {
-      queue.put(th);
-    }
-
-    @SneakyThrows
-    void next(KsqlResponseTable t) {
-      queue.put(t);
-    }
-
-    @SneakyThrows
-    void close() {
-      queue.put(CLOSE_PIPE_MARKER);
-    }
+    volatile FluxSink<KsqlResponseTable> sink;
   }
 
+  private final Map<String, Pipe> registeredPipes = new ConcurrentHashMap<>();
 
   public Flux<KsqlResponseTable> registerPipe(String pipeId) {
-    var p = new Pipe();
-    if (registeredPipes.put(pipeId, p) != null) {
+    var pipe = new Pipe();
+    if (registeredPipes.put(pipeId, pipe) != null) {
       throw new ValidationException(String.format("Pipe with id %s already registered", pipeId));
     }
-    Runnable cleaning = () -> {
-      System.out.println("removing pipe " + pipeId);
-      registeredPipes.remove(pipeId);
-      if (p.sourceSubscription != null) {
-        p.sourceSubscription.dispose();
-      }
-    };
-    return Flux.create(sink -> {
-      while (true) {
-        try {
-          var r = p.queue.poll(1, TimeUnit.SECONDS);
-          if (sink.isCancelled()) {
-            cleaning.run();
-            break;
-          }
-          if (r == null) {
-            continue;
-          }
-          if (r == CLOSE_PIPE_MARKER) {
-            sink.complete();
-            cleaning.run();
-            break;
-          }
-          if (r instanceof Throwable) {
-            sink.error((Throwable) r);
-            cleaning.run();
-            break;
-          }
-          if (r instanceof KsqlResponseTable) {
-            sink.next((KsqlResponseTable) r);
-          }
-        } catch (InterruptedException e) {
-          sink.error(e); //todo maybe complete ?
-          cleaning.run();
-          break;
-        }
-      }
-    });
+    return Flux.<KsqlResponseTable>create(sink -> pipe.sink = sink)
+        .doFinally(s -> registeredPipes.remove(pipeId));
   }
 
   public void execute(KafkaCluster cluster,
                       String pipeId,
                       String ksql,
                       Map<String, String> streamProperties) {
-    System.out.println("pipes: " + registeredPipes.keySet());
     var pipe = Objects.requireNonNull(registeredPipes.get(pipeId), "Pipe not found");
-    var dispose = new KsqlApiClient(cluster)
+    new KsqlApiClient(cluster)
         .execute(ksql, streamProperties)
-        .subscribe(pipe::next, pipe::err, pipe::close);
-    pipe.sourceSubscription = dispose;
+        .subscribeWith(new BaseSubscriber<KsqlResponseTable>() {
+
+          @Override
+          protected void hookOnSubscribe(Subscription subscription) {
+            super.hookOnSubscribe(subscription);
+            // if pipe is cancelled we need to cancel subscription too
+            pipe.sink.onCancel(this);
+          }
+
+          @Override
+          protected void hookOnError(Throwable throwable) {
+            pipe.sink.error(throwable);
+          }
+
+          @Override
+          protected void hookOnNext(KsqlResponseTable value) {
+            pipe.sink.next(value);
+          }
+
+          @Override
+          protected void hookFinally(SignalType type) {
+            pipe.sink.complete();
+          }
+        });
   }
 
 }
