8610bdf681e4e56094c28a365a67843e37a8e307
Dekshut <denismaloffev@gmail.com>
2022-01-26 12:26:43 +0200

    got rid of enzyme in DynamicTextButton tests

--- kafka-ui-react-app/src/components/common/DynamicTextButton/__tests__/DynamicTextButton.spec.tsx
+++ kafka-ui-react-app/src/components/common/DynamicTextButton/__tests__/DynamicTextButton.spec.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
 import DynamicTextButton from 'components/common/DynamicTextButton/DynamicTextButton';
+import { render } from 'lib/testHelpers';
+import { fireEvent } from '@testing-library/dom';
 
 describe('DynamicButton', () => {
   const mockCallback = jest.fn();
   it('exectutes callback', () => {
-    const component = shallow(
+    const component = render(
       <DynamicTextButton
         onClick={mockCallback}
         title="title"
         render={() => 'text'}
       />
     );
-    component.simulate('click');
+
+    fireEvent.click(
+      component.baseElement.querySelector('button') as HTMLElement
+    );
     expect(mockCallback).toBeCalled();
   });
 
   it('changes the text', () => {
-    const component = mount(
+    const component = render(
       <DynamicTextButton
         onClick={mockCallback}
         title="title"
         render={(clicked) => (clicked ? 'active' : 'default')}
       />
     );
-    expect(component.text()).toEqual('default');
-    component.simulate('click');
-    expect(component.text()).toEqual('active');
+    expect(component.baseElement).toHaveTextContent('default');
+    fireEvent.click(
+      component.baseElement.querySelector('button') as HTMLElement
+    );
+    expect(component.baseElement).toHaveTextContent('active');
   });
 });
