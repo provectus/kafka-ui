3e5bff9618879de70373e79a9414a101e44611f5
Dekshut <denismaloffev@gmail.com>
2022-01-26 12:23:33 +0200

    got rid of enzyme in Dropdown tests

--- kafka-ui-react-app/src/components/common/Dropdown/__tests__/DropdownItem.spec.tsx
+++ kafka-ui-react-app/src/components/common/Dropdown/__tests__/DropdownItem.spec.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { mount } from 'enzyme';
 import DropdownItem from 'components/common/Dropdown/DropdownItem';
+import { render } from 'lib/testHelpers';
+import { fireEvent } from '@testing-library/dom';
 
 const onClick = jest.fn();
 
 describe('DropdownItem', () => {
   it('matches snapshot', () => {
-    const wrapper = mount(
+    const wrapper = render(
       <DropdownItem onClick={jest.fn()}>Item 1</DropdownItem>
     );
     expect(onClick).not.toHaveBeenCalled();
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.baseElement).toMatchSnapshot();
   });
 
   it('handles Click', () => {
-    const wrapper = mount(
+    const wrapper = render(
       <DropdownItem onClick={onClick}>Item 1</DropdownItem>
     );
-    wrapper.simulate('click');
+
+    const dropDown = wrapper.getByText('Item 1');
+
+    fireEvent.click(dropDown);
     expect(onClick).toHaveBeenCalled();
   });
 });
