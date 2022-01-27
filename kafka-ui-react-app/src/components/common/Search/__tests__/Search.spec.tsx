23a6afc15c8f05e8bcab1f2662eabbf390e0de8b
Dekshut <denismaloffev@gmail.com>
2022-01-27 13:39:52 +0200

    got rid of ThemeProvider in render function

--- kafka-ui-react-app/src/components/common/Search/__tests__/Search.spec.tsx
+++ kafka-ui-react-app/src/components/common/Search/__tests__/Search.spec.tsx
@@ -2,8 +2,6 @@ import Search from 'components/common/Search/Search';
 import React from 'react';
 import { render } from 'lib/testHelpers';
 import { fireEvent } from '@testing-library/dom';
-import { ThemeProvider } from 'styled-components';
-import theme from 'theme/theme';
 
 jest.mock('use-debounce', () => ({
   useDebouncedCallback: (fn: (e: Event) => void) => fn,
@@ -13,13 +11,11 @@ describe('Search', () => {
   const handleSearch = jest.fn();
   it('calls handleSearch on input', () => {
     const component = render(
-      <ThemeProvider theme={theme}>
-        <Search
-          handleSearch={handleSearch}
-          value=""
-          placeholder="Search bt the Topic name"
-        />
-      </ThemeProvider>
+      <Search
+        handleSearch={handleSearch}
+        value=""
+        placeholder="Search bt the Topic name"
+      />
     );
     const input = component.baseElement.querySelector('input') as HTMLElement;
     fireEvent.change(input, { target: { value: 'test' } });
