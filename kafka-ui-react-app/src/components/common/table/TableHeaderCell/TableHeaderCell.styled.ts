a629f64723fb6c8f3b3ac79e3a517eace4f88ea1
Zori4 <workzor@bk.ru>
2022-01-31 14:22:01 +0300

    refactoring TableHeader styles

--- kafka-ui-react-app/src/components/common/table/TableHeaderCell/TableHeaderCell.styled.ts
+++ kafka-ui-react-app/src/components/common/table/TableHeaderCell/TableHeaderCell.styled.ts
@@ -5,79 +5,74 @@ interface TitleProps {
   isOrdered?: boolean;
 }
 
-const getOrderedColor = (active: string, normal: string, isOrdered?: boolean) =>
-  isOrdered ? active : normal;
+export const Title = styled.span<TitleProps>(
+  ({ isOrderable, isOrdered, theme: { table } }) => css`
+    font-family: Inter, sans-serif;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 16px;
+    letter-spacing: 0em;
+    text-align: left;
+    justify-content: start;
+    display: flex;
+    align-items: center;
+    background: ${table.th.backgroundColor.normal};
+    cursor: default;
+    color: ${table.th.color.normal};
 
-const isOrderableStyles = css<TitleProps>`
-  cursor: pointer;
+    ${isOrderable &&
+    css`
+      cursor: pointer;
 
-  padding-right: 18px;
-  position: relative;
+      padding-right: 18px;
+      position: relative;
 
-  &::before,
-  &::after {
-    border: 4px solid transparent;
-    content: '';
-    display: block;
-    height: 0;
-    right: 5px;
-    top: 50%;
-    position: absolute;
-  }
+      &::before,
+      &::after {
+        border: 4px solid transparent;
+        content: '';
+        display: block;
+        height: 0;
+        right: 5px;
+        top: 50%;
+        position: absolute;
+      }
 
-  &::before {
-    border-bottom-color: ${({ isOrdered, theme }) =>
-      getOrderedColor(
-        theme.table.th.color.active,
-        theme.table.th.color.normal,
-        isOrdered
-      )};
-    margin-top: -9px;
-  }
+      &::before {
+        border-bottom-color: ${table.th.color.normal};
+        margin-top: -9px;
+      }
 
-  &::after {
-    border-top-color: ${({ isOrdered, theme }) =>
-      getOrderedColor(
-        theme.table.th.color.active,
-        theme.table.th.color.normal,
-        isOrdered
-      )};
-    margin-top: 1px;
-  }
+      &::after {
+        border-top-color: ${table.th.color.normal};
+        margin-top: 1px;
+      }
 
-  &:hover {
-    color: ${(props) => props.theme.table.th.color.hover};
-    &::before {
-      border-bottom-color: ${(props) => props.theme.table.th.color.hover};
-    }
-    &::after {
-      border-top-color: ${(props) => props.theme.table.th.color.hover};
-    }
-  }
-`;
+      &:hover {
+        color: ${table.th.color.hover};
+        &::before {
+          border-bottom-color: ${table.th.color.hover};
+        }
+        &::after {
+          border-top-color: ${table.th.color.hover};
+        }
+      }
+    `}
 
-export const Title = styled.span<TitleProps>`
-  font-family: Inter, sans-serif;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 16px;
-  letter-spacing: 0em;
-  text-align: left;
-  justify-content: start;
-  display: flex;
-  align-items: center;
-  background: ${(props) => props.theme.table.th.backgroundColor.normal};
-  color: ${({ isOrdered, theme }) =>
-    getOrderedColor(
-      theme.table.th.color.active,
-      theme.table.th.color.normal,
-      isOrdered
-    )};
-  cursor: default;
-
-  ${(props) => props.isOrderable && isOrderableStyles};
-`;
+    ${isOrderable &&
+    isOrdered &&
+    css`
+      color: ${table.th.color.active};
+      &::before {
+        border-bottom-color: ${table.th.color.active};
+      }
+      &::after {
+        border-top-color: ${table.th.color.active};
+      }
+    `}
+  `
+);
 
 export const Preview = styled.span`
   margin-left: 8px;
