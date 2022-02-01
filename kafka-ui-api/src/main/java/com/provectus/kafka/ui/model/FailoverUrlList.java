bf7256705655273e3540d28ff9e162b5e7be066e
Jonas Geiregat (31198) <jonas.geiregat@tvh.com>
2022-02-01 21:55:47 +0100

    Made code thread safe

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/model/FailoverUrlList.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/model/FailoverUrlList.java
@@ -6,16 +6,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FailoverUrlList {
 
   public static final int DEFAULT_RETRY_GRACE_PERIOD_IN_MS = 5000;
 
-  private final Map<Integer, Instant> failures = new HashMap<>();
+  private final Map<Integer, Instant> failures = new ConcurrentHashMap<>();
+  private final AtomicInteger index = new AtomicInteger(0);
   private final List<String> urls;
   private final int retryGracePeriodInMs;
-
-  private Integer index;
 
   public FailoverUrlList(List<String> urls) {
     this(urls, DEFAULT_RETRY_GRACE_PERIOD_IN_MS);
@@ -24,7 +25,6 @@ public class FailoverUrlList {
   public FailoverUrlList(List<String> urls, int retryGracePeriodInMs) {
     if (urls != null && !urls.isEmpty()) {
       this.urls = new ArrayList<>(urls);
-      this.index = new Random().nextInt(urls.size());
     } else {
       throw new IllegalArgumentException("Expected at least one URL to be passed in constructor");
     }
@@ -32,29 +32,27 @@ public class FailoverUrlList {
   }
 
   public String current() {
-    return this.urls.get(this.index);
+    return this.urls.get(this.index.get());
   }
 
   public synchronized void fail(String url) {
-    int currentIndex = this.index;
+    int currentIndex = this.index.get();
     if ((this.urls.get(currentIndex)).equals(url)) {
       this.failures.put(currentIndex, Instant.now());
-      this.index = (currentIndex + 1) % this.urls.size();
+      this.index.compareAndSet(currentIndex, (currentIndex + 1) % this.urls.size());
     }
   }
 
-  public int size() {
-    return this.urls.size();
-  }
-
   public boolean isFailoverAvailable() {
+    var now = Instant.now();
     return this.urls.size() > this.failures.size()
             || this.failures
                     .values()
                     .stream()
-                    .anyMatch(e -> Instant.now().isAfter(e.plusMillis(retryGracePeriodInMs)));
+                    .anyMatch(e -> now.isAfter(e.plusMillis(retryGracePeriodInMs)));
   }
 
+  @Override
   public String toString() {
     return this.urls.toString();
   }
