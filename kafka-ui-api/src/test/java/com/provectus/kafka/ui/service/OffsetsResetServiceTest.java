251457f2af41b2b8b02a5a59808241d62262debc
Roman Zabaluev <rzabaluev@provectus.com>
2022-02-07 15:32:13 +0300

    Implement passing kafka truststore
    Signed-off-by: Roman Zabaluev <rzabaluev@provectus.com>

--- kafka-ui-api/src/test/java/com/provectus/kafka/ui/service/OffsetsResetServiceTest.java
+++ kafka-ui-api/src/test/java/com/provectus/kafka/ui/service/OffsetsResetServiceTest.java
@@ -31,10 +31,15 @@ import org.apache.kafka.common.utils.Bytes;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration(initializers = {AbstractBaseTest.Initializer.class})
 public class OffsetsResetServiceTest extends AbstractBaseTest {
+
+  @Autowired
+  private Environment environment;
 
   private static final int PARTITIONS = 5;
 
@@ -52,7 +57,7 @@ public class OffsetsResetServiceTest extends AbstractBaseTest {
 
   @BeforeEach
   void init() {
-    AdminClientServiceImpl adminClientService = new AdminClientServiceImpl();
+    AdminClientServiceImpl adminClientService = new AdminClientServiceImpl(environment);
     adminClientService.setClientTimeout(5_000);
     offsetsResetService = new OffsetsResetService(adminClientService);
 
