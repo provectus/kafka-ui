772b878d90cff874934596d9cc5523ed306134c3
Ilya Kuramshin <ilia-2k@rambler.ru>
2022-02-09 14:23:35 +0300

    ISSUE-943: Topic messages tailing implementation (#1515)
    * Topic messages tailing implementation

---
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/emitter/ConsumingStats.java
@@ -0,0 +1,41 @@
+package com.provectus.kafka.ui.emitter;
+
+import com.provectus.kafka.ui.model.TopicMessageConsumingDTO;
+import com.provectus.kafka.ui.model.TopicMessageEventDTO;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.utils.Bytes;
+import reactor.core.publisher.FluxSink;
+
+class ConsumingStats {
+
+  private long bytes = 0;
+  private int records = 0;
+  private long elapsed = 0;
+
+  void sendConsumingEvt(FluxSink<TopicMessageEventDTO> sink,
+                               ConsumerRecords<Bytes, Bytes> polledRecords,
+                               long elapsed) {
+    for (ConsumerRecord<Bytes, Bytes> record : polledRecords) {
+      for (Header header : record.headers()) {
+        bytes +=
+            (header.key() != null ? header.key().getBytes().length : 0L)
+                + (header.value() != null ? header.value().length : 0L);
+      }
+      bytes += record.serializedKeySize() + record.serializedValueSize();
+    }
+    this.records += polledRecords.count();
+    this.elapsed += elapsed;
+    final TopicMessageConsumingDTO consuming = new TopicMessageConsumingDTO()
+        .bytesConsumed(this.bytes)
+        .elapsedMs(this.elapsed)
+        .isCancelled(sink.isCancelled())
+        .messagesConsumed(this.records);
+    sink.next(
+        new TopicMessageEventDTO()
+            .type(TopicMessageEventDTO.TypeEnum.CONSUMING)
+            .consuming(consuming)
+    );
+  }
+}
