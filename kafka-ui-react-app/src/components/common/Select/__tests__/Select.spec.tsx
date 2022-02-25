8847d92153fa36d9e8f7d18b0011dbfbda232b5b
NelyDavtyan <ndavtyan@provectus.com>
2022-02-25 13:54:49 +0400

    adding test cases for select

--- kafka-ui-react-app/src/components/common/Select/__tests__/Select.spec.tsx
+++ kafka-ui-react-app/src/components/common/Select/__tests__/Select.spec.tsx
@@ -1,7 +1,11 @@
-import Select, { SelectProps } from 'components/common/Select/Select';
+import Select, {
+  SelectProps,
+  SelectOption,
+} from 'components/common/Select/Select';
 import React from 'react';
 import { render } from 'lib/testHelpers';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('react-hook-form', () => ({
   useFormContext: () => ({
@@ -9,10 +13,36 @@ jest.mock('react-hook-form', () => ({
   }),
 }));
 
+const options = [
+  { label: 'test-label1', value: 'test-value1' },
+  { label: 'test-label2', value: 'test-value2' },
+];
+
 const renderComponent = (props?: Partial<SelectProps>) =>
   render(<Select name="test" {...props} />);
 
 describe('Custom Select', () => {
+  it('renders component', () => {
+    renderComponent();
+    expect(screen.getByRole('listbox')).toBeInTheDocument();
+  });
+  it('show select options when select is being clicked', () => {
+    renderComponent({
+      options,
+    });
+    expect(screen.getByRole('option')).toBeInTheDocument();
+    userEvent.click(screen.getByRole('listbox'));
+    expect(screen.getAllByRole('option')).toHaveLength(3);
+  });
+  it('checking select option change', () => {
+    renderComponent({
+      options,
+    });
+    userEvent.click(screen.getByRole('listbox'));
+    userEvent.selectOptions(screen.getByRole('listbox'), ['test-label1']);
+    expect(screen.getByRole('option')).toHaveTextContent('test-label1');
+  });
+
   describe('when non-live', () => {
     it('there is not live icon', () => {
       renderComponent({ isLive: false });
