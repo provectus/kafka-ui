31f74d78cdc1902093f2c8e76fbbce0b83789ec0
NelyDavtyan <ndavtyan@provectus.com>
2022-03-07 17:12:26 +0400

    fixing code smell issue

--- kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/MessagesTable.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/MessagesTable.tsx
@@ -68,38 +68,6 @@ const MessagesTable: React.FC = () => {
     }
   }, [searchParams, history, messages]);
 
-  const handlePreviousClick = React.useCallback(() => {
-    const seekTo = searchParams.get('seekTo');
-    if (seekTo) {
-      const selectedPartitions = seekTo.split(',').map((item) => {
-        const [partition] = item.split('::');
-        return { offset: 0, partition: parseInt(partition, 10) };
-      });
-      const messageUniqs = map(groupBy(messages, 'partition'), (v) =>
-        searchParams.get('seekDirection') === SeekDirection.BACKWARD
-          ? minBy(v, 'offset')
-          : maxBy(v, 'offset')
-      ).map((message) => ({
-        offset: message?.offset || 0,
-        partition: message?.partition || 0,
-      }));
-      const previousSeekTo = compact(
-        map(
-          groupBy(concat(selectedPartitions, messageUniqs), 'partition'),
-          (v) => maxBy(v, 'offset')
-        )
-      )
-        .map(({ offset, partition }) => `${partition}::${offset}`)
-        .join(',');
-      // const previousSeekTo = '0::501';
-
-      searchParams.set('seekTo', previousSeekTo);
-      history.push({
-        search: `?${searchParams.toString()}`,
-      });
-    }
-  }, [searchParams, history, messages]);
-
   return (
     <>
       <Table isFullwidth>
@@ -141,9 +109,6 @@ const MessagesTable: React.FC = () => {
         </tbody>
       </Table>
       <MessagesPaginationWrapperStyled>
-        <S.PaginationButton onClick={handlePreviousClick}>
-          Previous
-        </S.PaginationButton>
         <S.PaginationButton onClick={handleNextClick}>Next</S.PaginationButton>
       </MessagesPaginationWrapperStyled>
     </>
