617557ec01689f1a21458adbc6c2e945c3a5a028
NelyDavtyan <ndavtyan@provectus.com>
2022-03-07 14:31:05 +0400

    removing unneccessary code

--- kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/Filters/Filters.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/Filters/Filters.tsx
@@ -254,14 +254,6 @@ const Filters: React.FC<FiltersProps> = ({
     handleFiltersSubmit();
   }, [handleFiltersSubmit, seekDirection]);
 
-  // const [num, setNum] = React.useState<number>(0);
-  let num = 0;
-  const plus = () => {
-    while (num < 100) {
-      console.log(num);
-      num += 1;
-    }
-  };
   return (
     <S.FiltersWrapper>
       <div>
@@ -338,8 +330,7 @@ const Filters: React.FC<FiltersProps> = ({
               buttonType="secondary"
               buttonSize="M"
               disabled={isSubmitDisabled}
-              // onClick={handleFiltersSubmit}
-              onClick={plus}
+              onClick={handleFiltersSubmit}
               style={{ fontWeight: 500 }}
             >
               Submit
@@ -391,7 +382,6 @@ const Filters: React.FC<FiltersProps> = ({
           </S.MetricsIcon>
           <span>{messagesConsumed} messages</span>
         </S.Metric>
-        <p>COUNT: {num}</p>
       </S.FiltersMetrics>
     </S.FiltersWrapper>
   );
--- kafka-ui-react-app/src/components/common/Icons/MessageToggleIcon.styled.ts
+++ kafka-ui-react-app/src/components/common/Icons/MessageToggleIcon.styled.ts
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 
 export const Svg = styled.svg`
