23a896c88e10d891adab7c3c354b51e0c815e0c1
iliax <ikuramshin@provectus.com>
2022-03-08 22:48:35 +0300

    Integration tests structure unified, unnecessary imports removed

--- kafka-ui-api/src/test/java/com/provectus/kafka/ui/KafkaConsumerTests.java
+++ kafka-ui-api/src/test/java/com/provectus/kafka/ui/KafkaConsumerTests.java
@@ -19,16 +19,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@ContextConfiguration(initializers = {AbstractBaseTest.Initializer.class})
 @Slf4j
-@AutoConfigureWebTestClient(timeout = "60000")
-public class KafkaConsumerTests extends AbstractBaseTest {
+public class KafkaConsumerTests extends AbstractIntegrationTest {
 
   @Autowired
   private WebTestClient webTestClient;
