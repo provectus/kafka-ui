cb6295961ec3da189f7c3fbc1b10081a01e8066e
Mgrdich <mgotm13@gmail.com>
2022-03-10 15:42:59 +0400

    minor code modifications ListItem Testing

--- kafka-ui-react-app/src/components/ConsumerGroups/Details/__tests__/ListItem.spec.tsx
+++ kafka-ui-react-app/src/components/ConsumerGroups/Details/__tests__/ListItem.spec.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { clusterConsumerGroupDetailsPath } from 'lib/paths';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ListItem from 'components/ConsumerGroups/Details/ListItem';
 import { consumerGroupPayload } from 'redux/reducers/consumerGroups/__test__/fixtures';
 import { render } from 'lib/testHelpers';
 import { Route } from 'react-router';
-import fetchMock from 'fetch-mock';
 import { ConsumerGroupTopicPartition } from 'generated-sources';
 
 const clusterName = 'cluster1';
@@ -35,25 +34,17 @@ const renderComponent = (consumers: ConsumerGroupTopicPartition[] = []) =>
   );
 
 describe('ListItem', () => {
-  afterEach(() => {
-    fetchMock.reset();
-  });
-
   it('renders list item with topic content closed', () => {
     renderComponent(consumerGroupPayload.partitions);
-    const listItem = screen.queryByTestId('consumer-group-list-item');
-    expect(listItem).toBeInTheDocument();
+    expect(screen.getByTestId('consumer-group-list-item')).toBeInTheDocument();
   });
 
   it('renders list item with topic content open', async () => {
     renderComponent(consumerGroupPayload.partitions);
-    const listItem = screen.queryByTestId('consumer-group-list-item');
-    expect(listItem).toBeInTheDocument();
+    expect(screen.getByTestId('consumer-group-list-item')).toBeInTheDocument();
 
-    await waitFor(() => {
-      userEvent.click(screen.getByTestId('consumer-group-list-item-toggle'));
-    });
+    userEvent.click(screen.getByTestId('consumer-group-list-item-toggle'));
 
-    expect(screen.queryByText('Consumer ID')).toBeInTheDocument();
+    expect(screen.getByText('Consumer ID')).toBeInTheDocument();
   });
 });
