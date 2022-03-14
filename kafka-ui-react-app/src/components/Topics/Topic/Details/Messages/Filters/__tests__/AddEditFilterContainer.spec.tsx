65b82912f779705a05a22a0506ce299f85d4c774
Mgrdich <mgotm13@gmail.com>
2022-03-14 22:29:26 +0400

    adding tests for AddEditFilterContainer component

--- kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/Filters/__tests__/AddEditFilterContainer.spec.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/Filters/__tests__/AddEditFilterContainer.spec.tsx
@@ -5,11 +5,17 @@ import AddEditFilterContainer, {
 import { render } from 'lib/testHelpers';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { MessageFilters } from 'components/Topics/Topic/Details/Messages/Filters/Filters';
 
 describe('AddEditFilterContainer component', () => {
   const defaultTitle = 'Test Title';
   const defaultSubmitBtn = 'Submit Button';
   const defaultNewFilter = 'Create New Filters';
+
+  const mockData: MessageFilters = {
+    name: 'mockName',
+    code: 'mockCode',
+  };
 
   const setupComponent = (props: Partial<AddEditFilterContainerProps> = {}) => {
     const { title, submitBtnText, createNewFilterText } = props;
@@ -78,6 +84,59 @@ describe('AddEditFilterContainer component', () => {
         const requiredFieldTextElements =
           screen.getAllByText(/required field/i);
         expect(requiredFieldTextElements).toHaveLength(2);
+      });
+    });
+  });
+
+  describe('Custom setup for the component', () => {
+    it('should render the input with default data if they are passed', () => {
+      setupComponent({
+        inputDisplayNameDefaultValue: mockData.name,
+        inputCodeDefaultValue: mockData.code,
+      });
+
+      const inputs = screen.getAllByRole('textbox');
+      const inputNameElement = inputs[0];
+      const textAreaElement = inputs[1];
+
+      expect(inputNameElement).toHaveValue(mockData.name);
+      expect(textAreaElement).toHaveValue(mockData.code);
+    });
+
+    it('should test whether the cancel callback is being called', async () => {
+      const cancelCallback = jest.fn();
+      setupComponent({
+        cancelBtnHandler: cancelCallback,
+      });
+      const cancelBtnElement = screen.getByText(/cancel/i);
+      userEvent.click(cancelBtnElement);
+      expect(cancelCallback).toBeCalled();
+    });
+
+    it('should test whether the submit Callback is being called', async () => {
+      const submitCallback = jest.fn() as (v: MessageFilters) => void;
+      setupComponent({
+        submitCallback,
+      });
+
+      const inputs = screen.getAllByRole('textbox');
+
+      const inputNameElement = inputs[0];
+      userEvent.type(inputNameElement, 'Hello World!');
+
+      const textAreaElement = inputs[1];
+      userEvent.type(textAreaElement, 'Hello World With TextArea');
+
+      const submitBtnElement = screen.getByText(defaultSubmitBtn);
+
+      await waitFor(() => {
+        expect(submitBtnElement).toBeEnabled();
+      });
+
+      userEvent.click(submitBtnElement);
+
+      await waitFor(() => {
+        expect(submitCallback).toBeCalled();
       });
     });
   });
