d791bf4e6e07a7f6aa9d56770a2b2b5482466804
Mgrdich <mgotm13@gmail.com>
2022-03-14 15:42:08 +0400

    adding AddEditFilterContainer Component moving the form validation and controlled components feature into the component

--- kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/Filters/AddEditFilterContainer.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/Filters/AddEditFilterContainer.tsx
@@ -3,51 +3,64 @@ import * as S from 'components/Topics/Topic/Details/Messages/Filters/Filters.sty
 import { InputLabel } from 'components/common/Input/InputLabel.styled';
 import Input from 'components/common/Input/Input';
 import { Textarea } from 'components/common/Textbox/Textarea.styled';
-import {
-  FormProvider,
-  Controller,
-  DeepMap,
-  FieldError,
-  Control,
-  FieldValues,
-  UseFormReturn,
-} from 'react-hook-form';
+import { FormProvider, Controller, useForm } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 import { Button } from 'components/common/Button/Button';
 import { MessageFilters } from 'components/Topics/Topic/Details/Messages/Filters/Filters';
+import { yupResolver } from '@hookform/resolvers/yup';
+import yup from 'lib/yupExtended';
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required(),
+  code: yup.string().required(),
+});
 
 export interface AddEditFilterContainerProps {
   title: string;
-  methods: UseFormReturn<MessageFilters>;
-  onSubmit: () => void;
   cancelBtnHandler: () => void;
-  submitBtnDisabled: boolean;
   submitBtnText: string;
-  handleSubmit: () => void;
-  errors: DeepMap<MessageFilters, FieldError> | undefined;
-  control: Control<FieldValues> | undefined;
   toggleSaveFilter?: boolean;
   inputDisplayNameDefaultValue?: string;
   setToggleSaveFilter?: () => void;
   inputCodeDefaultValue?: string;
   createNewFilterText?: string;
+  submitCallback?: (values: MessageFilters) => void;
+  submitCallbackWithReset?: boolean;
 }
 
 const AddEditFilterContainer: React.FC<AddEditFilterContainerProps> = ({
   title,
-  methods,
+  cancelBtnHandler,
+  submitBtnText,
   toggleSaveFilter,
   setToggleSaveFilter,
-  cancelBtnHandler,
-  submitBtnDisabled,
-  submitBtnText,
-  handleSubmit,
-  errors,
-  control,
   createNewFilterText,
   inputDisplayNameDefaultValue = '',
   inputCodeDefaultValue = '',
+  submitCallback,
+  submitCallbackWithReset,
 }) => {
+  const methods = useForm<MessageFilters>({
+    mode: 'onChange',
+    resolver: yupResolver(validationSchema),
+  });
+  const {
+    handleSubmit,
+    control,
+    formState: { isDirty, isSubmitting, isValid, errors },
+    reset,
+  } = methods;
+
+  const onSubmit = React.useCallback(
+    (values: MessageFilters) => {
+      submitCallback?.(values);
+      if (submitCallbackWithReset) {
+        reset({ name: '', code: '' });
+      }
+    },
+    [reset, submitCallback, submitCallbackWithReset]
+  );
+
   return (
     <>
       <S.FilterTitle>{title}</S.FilterTitle>
@@ -55,7 +68,10 @@ const AddEditFilterContainer: React.FC<AddEditFilterContainerProps> = ({
         {createNewFilterText && (
           <S.CreatedFilter>{createNewFilterText}</S.CreatedFilter>
         )}
-        <form onSubmit={handleSubmit} aria-label="Filters submit Form">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          aria-label="Filters submit Form"
+        >
           <div>
             <InputLabel>Display name</InputLabel>
             <Input
@@ -106,7 +122,7 @@ const AddEditFilterContainer: React.FC<AddEditFilterContainerProps> = ({
               buttonSize="M"
               buttonType="primary"
               type="submit"
-              disabled={submitBtnDisabled}
+              disabled={!isValid || isSubmitting || !isDirty}
             >
               {submitBtnText}
             </Button>
