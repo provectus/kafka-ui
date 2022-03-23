aad5742d9eeedcd48da65c3d8118da7a9d7a3130
Mgrdich <mgotm13@gmail.com>
2022-03-23 11:17:38 +0400

    adding Tests in the Consumer Groups List

--- kafka-ui-react-app/src/components/ConsumerGroups/List/__test__/List.spec.tsx
+++ kafka-ui-react-app/src/components/ConsumerGroups/List/__test__/List.spec.tsx
@@ -4,17 +4,24 @@ import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { render } from 'lib/testHelpers';
 import { consumerGroups as consumerGroupMock } from 'redux/reducers/consumerGroups/__test__/fixtures';
-import { SortOrder } from 'generated-sources';
+import { ConsumerGroupOrdering, SortOrder } from 'generated-sources';
+import theme from 'theme/theme';
 
 describe('List', () => {
   const setUpComponent = (props: Partial<Props> = {}) => {
-    const { consumerGroups, orderBy, sortOrder, totalPages } = props;
+    const {
+      consumerGroups,
+      orderBy,
+      sortOrder,
+      totalPages,
+      setConsumerGroupsSortOrderBy,
+    } = props;
     return render(
       <List
         consumerGroups={consumerGroups || []}
-        orderBy={orderBy || null}
+        orderBy={orderBy || ConsumerGroupOrdering.NAME}
         sortOrder={sortOrder || SortOrder.ASC}
-        setConsumerGroupsSortOrderBy={jest.fn()}
+        setConsumerGroupsSortOrderBy={setConsumerGroupsSortOrderBy || jest.fn()}
         totalPages={totalPages || 1}
       />
     );
@@ -46,6 +53,39 @@ describe('List', () => {
         expect(screen.getByText('groupId1')).toBeInTheDocument();
         expect(screen.getByText('groupId2')).toBeInTheDocument();
       });
+    });
+
+    describe('Testing the Ordering', () => {
+      it('should test the sort order functionality', async () => {
+        const thElement = screen.getByText(/consumer group id/i);
+        expect(thElement).toBeInTheDocument();
+        expect(thElement).toHaveStyle(`color:${theme.table.th.color.active}`);
+      });
+    });
+  });
+
+  describe('consumerGroups are fetched with custom parameters', () => {
+    it('should test the order by functionality of another element', async () => {
+      const sortOrder = jest.fn();
+      setUpComponent({
+        consumerGroups: consumerGroupMock,
+        setConsumerGroupsSortOrderBy: sortOrder,
+      });
+      const thElement = screen.getByText(/num of members/i);
+      expect(thElement).toBeInTheDocument();
+
+      userEvent.click(thElement);
+      expect(sortOrder).toBeCalled();
+    });
+
+    it('should view the ordered list with the right prop', () => {
+      setUpComponent({
+        consumerGroups: consumerGroupMock,
+        orderBy: ConsumerGroupOrdering.MEMBERS,
+      });
+      expect(screen.getByText(/num of members/i)).toHaveStyle(
+        `color:${theme.table.th.color.active}`
+      );
     });
   });
 });
