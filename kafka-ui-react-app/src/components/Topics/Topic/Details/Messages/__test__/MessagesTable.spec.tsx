c7f19bbaa50127716f2538925128f000c44d3477
Mgrdich <mgotm13@gmail.com>
2022-03-29 12:26:16 +0400

    improve the MessagesTable test File

--- kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/__test__/MessagesTable.spec.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Details/Messages/__test__/MessagesTable.spec.tsx
@@ -5,22 +5,40 @@ import MessagesTable from 'components/Topics/Topic/Details/Messages/MessagesTabl
 import { Router } from 'react-router';
 import { createMemoryHistory } from 'history';
 import { SeekDirection, SeekType } from 'generated-sources';
+import userEvent from '@testing-library/user-event';
+import TopicMessagesContext, {
+  ContextProps,
+} from 'components/contexts/TopicMessagesContext';
 
 describe('MessagesTable', () => {
-  const searchParams = `?filterQueryType=STRING_CONTAINS&attempt=0&limit=100&seekDirection=${SeekDirection.FORWARD}&seekType=${SeekType.OFFSET}&seekTo=0::9`;
-  const setUpComponent = (params = searchParams) => {
+  const searchParams = new URLSearchParams(
+    `?filterQueryType=STRING_CONTAINS&attempt=0&limit=100&seekDirection=${SeekDirection.FORWARD}&seekType=${SeekType.OFFSET}&seekTo=0::9`
+  );
+  const contextValue: ContextProps = {
+    isLive: false,
+    seekDirection: SeekDirection.FORWARD,
+    searchParams,
+    toggleSeekDirection: jest.fn(),
+  };
+
+  const setUpComponent = (
+    params: URLSearchParams = searchParams,
+    ctx: ContextProps = contextValue
+  ) => {
     const history = createMemoryHistory();
     history.push({
-      search: new URLSearchParams(params).toString(),
+      search: params.toString(),
     });
     return render(
       <Router history={history}>
-        <MessagesTable />
+        <TopicMessagesContext.Provider value={ctx}>
+          <MessagesTable />
+        </TopicMessagesContext.Provider>
       </Router>
     );
   };
 
-  describe('Default Search Params', () => {
+  describe('Default Setup', () => {
     beforeEach(() => {
       setUpComponent();
     });
@@ -32,7 +50,19 @@ describe('MessagesTable', () => {
     it('should check the if no elements is rendered in the table', () => {
       expect(screen.getByText(/No messages found/i)).toBeInTheDocument();
     });
+
+    it('should check if next button exist and check the click after next click', () => {
+      const nextBtnElement = screen.getByText(/next/i);
+      expect(nextBtnElement).toBeInTheDocument();
+      userEvent.click(nextBtnElement);
+      expect(screen.getByText(/No messages found/i)).toBeInTheDocument();
+    });
   });
 
-  describe('Custom Search Params', () => {});
+  describe('Custom Setup', () => {
+    it('should check if next click is gone during isLive Param', () => {
+      setUpComponent(searchParams, { ...contextValue, isLive: true });
+      expect(screen.queryByText(/next/i)).not.toBeInTheDocument();
+    });
+  });
 });
