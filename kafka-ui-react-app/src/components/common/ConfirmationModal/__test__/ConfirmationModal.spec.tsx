c097733f992f516ab1a796f49f847a6fb1dc445e
Oleg Shuralev <workshur@gmail.com>
2022-05-11 01:27:40 +0300

    linting

--- kafka-ui-react-app/src/components/common/ConfirmationModal/__test__/ConfirmationModal.spec.tsx
+++ kafka-ui-react-app/src/components/common/ConfirmationModal/__test__/ConfirmationModal.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ConfirmationModal, {
   ConfirmationModalProps,
 } from 'components/common/ConfirmationModal/ConfirmationModal';
-import { screen, prettyDOM } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { render } from 'lib/testHelpers';
 
