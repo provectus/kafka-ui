37245a9441420caeb812516f77a7d6fcf106a8fc
lazzy-panda <grifx.design@gmail.com>
2022-05-18 11:15:42 +0300

    Added replicas data to topic overview page

--- kafka-ui-react-app/src/components/common/table/TableHeaderCell/TableHeaderCell.styled.ts
+++ kafka-ui-react-app/src/components/common/table/TableHeaderCell/TableHeaderCell.styled.ts
@@ -111,6 +111,10 @@ export const Preview = styled.span`
   cursor: pointer;
 `;
 
+export const ReplicaCell = styled.span<{ leader: boolean | undefined }>`
+  color: ${(props) => (props.leader ? 'orange' : null)};
+`;
+
 export const TableHeaderCell = styled.th`
   padding: 4px 0 4px 24px;
   border-bottom-width: 1px;
