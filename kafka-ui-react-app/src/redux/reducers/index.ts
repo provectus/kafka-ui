6a0d406ab181284601aa43f6216504d05c925356
rAzizbekyan <razizbekyan@provectus.com>
2022-05-23 16:35:09 +0400

    adding tests for selectors

--- kafka-ui-react-app/src/redux/reducers/index.ts
+++ kafka-ui-react-app/src/redux/reducers/index.ts
@@ -5,11 +5,10 @@ import brokers from 'redux/reducers/brokers/brokersSlice';
 import alerts from 'redux/reducers/alerts/alertsSlice';
 import schemas from 'redux/reducers/schemas/schemasSlice';
 import connect from 'redux/reducers/connect/connectSlice';
-
-import topicMessages from './topicMessages/topicMessagesSlice';
-import topics from './topics/topicsSlice';
-import consumerGroups from './consumerGroups/consumerGroupsSlice';
-import ksqlDb from './ksqlDb/ksqlDbSlice';
+import topicMessages from 'redux/reducers/topicMessages/topicMessagesSlice';
+import topics from 'redux/reducers/topics/topicsSlice';
+import consumerGroups from 'redux/reducers/consumerGroups/consumerGroupsSlice';
+import ksqlDb from 'redux/reducers/ksqlDb/ksqlDbSlice';
 
 export default combineReducers({
   loader,
