4de796f2bfa48bbaef85ccf53565e86a56f1f5de
Mgrdich <mgotm13@gmail.com>
2022-05-27 22:31:15 +0400

    BreadCrumbs Routes fixes

--- kafka-ui-react-app/src/components/common/Breadcrumb/Breadcrumb.route.tsx
+++ kafka-ui-react-app/src/components/common/Breadcrumb/Breadcrumb.route.tsx
@@ -1,17 +1,34 @@
 import React, { useContext, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Params, Location, useLocation, useParams } from 'react-router-dom';
 
 import { BreadcrumbContext } from './Breadcrumb.context';
 
+const getRoutePath = (location: Location, params: Params): string => {
+  const { pathname } = location;
+
+  if (!Object.keys(params).length) {
+    return pathname; // we don't need to replace anything
+  }
+
+  let path = pathname;
+  Object.entries(params).forEach(([paramName, paramValue]) => {
+    if (paramValue) {
+      path = path.replace(paramValue, `:${paramName}`);
+    }
+  });
+  return path;
+};
+
 const BreadcrumbRouteInternal: React.FC = () => {
   const location = useLocation();
+  const params = useParams();
+  const path = getRoutePath(location, params);
   const context = useContext(BreadcrumbContext);
 
   useEffect(() => {
-    // TODO check this
     context.handleRouteChange({
       url: location.pathname,
-      path: location.pathname,
+      path,
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.pathname]);
