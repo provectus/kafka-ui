e5f2df86dd4f395f649943971dcf420e25aa3875
sbritprovectus <85669187+sbritprovectus@users.noreply.github.com>
2022-06-06 21:05:43 +0300

    Fix e2e tests (#1543)
    * Fix tests. Change folders structure
    
    * Activate tests
    
    * Update selenoid.yaml, broesers.json. Add pull of selenoid browser image.
    
    * Change method of pulling chrome image
    
    * Fix browsers.json and return the pulling of browser image
    
    * Update pom.
    
    * Update pom
    
    * Fix pom
    
    * Fix java version in pom
    
    * Migrate to selenium containers
    
    * Return selenoid container. Fix using of BASE_URL
    
    * Delete video recording for Selenoid container
    
    * expose kafka-ui port
    
    Signed-off-by: Roman Zabaluev <rzabaluev@provectus.com>
    
    * Fix tests run
    
    * Delete comment from workflow file
    
    * Stop using containers and migrate to WDM.
    
    * Delete comments. Update dependencies.
    
    * Comment using of  TestConfiguration.BASE_URL.replace("localhost", "host.docker.internal");
    
    * Retern changes
    
    * Clean code
    
    * Update e2e-checks.yaml
    
    * Update e2e-checks.yaml
    
    * Fix skip test parameter in tests project
    
    * Delete host.docker.internal in base tests
    
    * Return previous usage of testcontainers
    
    * Delete base url configuration
    
    * Revert versions in pom.xml
    
    * Return some values
    
    * Small fixes for stabilize tests
    
    * Trying to fix tests running on github
    
    * Update base test
    
    * Fix selenide url
    
    Signed-off-by: Roman Zabaluev <rzabaluev@provectus.com>
    
    * Fix selenide url
    
    Signed-off-by: Roman Zabaluev <rzabaluev@provectus.com>
    
    * test stuff
    
    Signed-off-by: Roman Zabaluev <rzabaluev@provectus.com>
    
    * added extra-hosts host.docker.internal:host-gateway
    
    * added extra-hosts host.docker.internal:host-gateway
    
    * added extra-hosts host.docker.internal:host-gateway
    
    * added command --add-host
    
    * separate driver container
    
    * small refactoring, change selenoid to BrowserWebDriverContainer
    
    * changed in BaseTest port from 8678 to 8080
    
    * changed tests with xpath and added additional actions (click javascrip, click with action)
    
    * added issue to allure reporting
    
    * provided fix for connectors tests
    
    * deleted afterAll annotation
    
    * fixed tests with ConnectorsTests
    
    * fixed tests with Topics
    
    * removed commented action with click
    
    * Revert unnecessary changes
    
    * Fix workflow
    
    * added @Disabled for test- update topic dui to issue https://github.com/provectus/kafka-ui/issues/1500
    
    * removed dependency, testconteiners version
    
    Co-authored-by: Roman Zabaluev <rzabaluev@provectus.com>
    Co-authored-by: kshpilchyna <kshpilchyna@provectus.com>

--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/base/BaseTest.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/base/BaseTest.java
@@ -1,118 +1,118 @@
 package com.provectus.kafka.ui.base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.provectus.kafka.ui.helpers.Helpers;
 import com.provectus.kafka.ui.pages.Pages;
 import com.provectus.kafka.ui.screenshots.Screenshooter;
 import com.provectus.kafka.ui.steps.Steps;
 import io.github.cdimascio.dotenv.Dotenv;
+import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayNameGeneration;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
 
 @Slf4j
 @DisplayNameGeneration(CamelCaseToSpacedDisplayNameGenerator.class)
 public class BaseTest {
 
-  protected Steps steps = Steps.INSTANCE;
-  protected Pages pages = Pages.INSTANCE;
-  protected Helpers helpers = Helpers.INSTANCE;
+    protected Steps steps = Steps.INSTANCE;
+    protected Pages pages = Pages.INSTANCE;
+    protected Helpers helpers = Helpers.INSTANCE;
 
-  private Screenshooter screenshooter = new Screenshooter();
+    private Screenshooter screenshooter = new Screenshooter();
 
-  public void compareScreenshots(String name) {
-    screenshooter.compareScreenshots(name);
-  }
+    public static BrowserWebDriverContainer<?> webDriverContainer =
+            new BrowserWebDriverContainer<>()
+                    .withCapabilities(new ChromeOptions()
+                            .addArguments("--no-sandbox")
+                            .addArguments("--disable-dev-shm-usage"))
+                    .waitingFor(Wait.defaultWaitStrategy().withStartupTimeout(Duration.ofSeconds(90)));
 
-  public void compareScreenshots(String name, Boolean shouldUpdateScreenshots) {
-    screenshooter.compareScreenshots(name, shouldUpdateScreenshots);
-  }
-
-  public static GenericContainer selenoid =
-      new GenericContainer(DockerImageName.parse("aerokube/selenoid:latest-release"))
-          .withExposedPorts(4444)
-          .withFileSystemBind("selenoid/config/", "/etc/selenoid", BindMode.READ_WRITE)
-          .withFileSystemBind("/var/run/docker.sock", "/var/run/docker.sock", BindMode.READ_WRITE)
-          .withFileSystemBind("selenoid/video", "/opt/selenoid/video", BindMode.READ_WRITE)
-          .withFileSystemBind("selenoid/logs", "/opt/selenoid/logs", BindMode.READ_WRITE)
-          .withEnv("OVERRIDE_VIDEO_OUTPUT_DIR", "/opt/selenoid/video")
-          .withCommand(
-              "-conf", "/etc/selenoid/browsers.json", "-log-output-dir", "/opt/selenoid/logs");
-
-  static {
-    if (!new File("./.env").exists()) {
-      try {
-        FileUtils.copyFile(new File(".env.example"), new File(".env"));
-      } catch (IOException e) {
-        log.error("couldn't copy .env.example to .env. Please add .env");
-        e.printStackTrace();
-      }
-    }
-    Dotenv.load().entries().forEach(env -> System.setProperty(env.getKey(), env.getValue()));
-    if (TestConfiguration.CLEAR_REPORTS_DIR) {
-      clearReports();
-    }
-    setupSelenoid();
-  }
-
-  @AfterAll
-  public static void afterAll() {
-//    closeWebDriver();
-//    selenoid.close();
-  }
-
-  @SneakyThrows
-  private static void setupSelenoid() {
-    String remote = TestConfiguration.SELENOID_URL;
-    if (TestConfiguration.SHOULD_START_SELENOID) {
-      selenoid.start();
-      remote =
-          "http://%s:%s/wd/hub"
-              .formatted(selenoid.getContainerIpAddress(), selenoid.getMappedPort(4444));
+    public void compareScreenshots(String name) {
+        screenshooter.compareScreenshots(name);
     }
 
-    Configuration.reportsFolder = TestConfiguration.REPORTS_FOLDER;
-    if (!TestConfiguration.USE_LOCAL_BROWSER) {
-      Configuration.remote = remote;
-      TestConfiguration.BASE_URL =
-          TestConfiguration.BASE_URL.replace("localhost", "host.docker.internal");
+    public void compareScreenshots(String name, Boolean shouldUpdateScreenshots) {
+        screenshooter.compareScreenshots(name, shouldUpdateScreenshots);
     }
-    Configuration.screenshots = TestConfiguration.SCREENSHOTS;
-    Configuration.savePageSource = TestConfiguration.SAVE_PAGE_SOURCE;
-    Configuration.reopenBrowserOnFail = TestConfiguration.REOPEN_BROWSER_ON_FAIL;
-    Configuration.browser = TestConfiguration.BROWSER;
-    Configuration.baseUrl = TestConfiguration.BASE_URL;
-    Configuration.browserSize = TestConfiguration.BROWSER_SIZE;
-    var capabilities = new DesiredCapabilities();
-//    DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-    capabilities.setCapability("enableVNC", TestConfiguration.ENABLE_VNC);
-    Configuration.browserCapabilities = capabilities;
 
-    SelenideLogger.addListener("allure", new AllureSelenide().savePageSource(false));
-  }
-
-  public static void clearReports() {
-    log.info("Clearing reports dir [%s]...".formatted(TestConfiguration.REPORTS_FOLDER));
-    File allureResults = new File(TestConfiguration.REPORTS_FOLDER);
-    if (allureResults.isDirectory()) {
-      File[] list = allureResults.listFiles();
-      if (list != null)
-        Arrays.stream(list)
-            .sequential()
-            .filter(e -> !e.getName().equals("categories.json"))
-            .forEach(File::delete);
+    @BeforeAll
+    public static void start() {
+        Testcontainers.exposeHostPorts(8080);
+        webDriverContainer.start();
+        webDriverContainer.isRunning();
+        RemoteWebDriver remoteWebDriver = webDriverContainer.getWebDriver();
+        WebDriverRunner.setWebDriver(remoteWebDriver);
     }
-  }
+
+    static {
+        if (!new File("./.env").exists()) {
+            try {
+                FileUtils.copyFile(new File(".env.example"), new File(".env"));
+            } catch (IOException e) {
+                log.error("couldn't copy .env.example to .env. Please add .env");
+                e.printStackTrace();
+            }
+        }
+        Dotenv.load().entries().forEach(env -> System.setProperty(env.getKey(), env.getValue()));
+        if (TestConfiguration.CLEAR_REPORTS_DIR) {
+            clearReports();
+        }
+        setup();
+    }
+
+
+    @AfterEach
+    public void afterMethod() {
+        webDriverContainer.getWebDriver().manage().deleteAllCookies();
+        Allure.addAttachment("Screenshot",
+                new ByteArrayInputStream(((TakesScreenshot) webDriverContainer.getWebDriver()).getScreenshotAs(OutputType.BYTES)));
+    }
+
+    @SneakyThrows
+    private static void setup() {
+
+        Configuration.reportsFolder = TestConfiguration.REPORTS_FOLDER;
+        Configuration.screenshots = TestConfiguration.SCREENSHOTS;
+        Configuration.savePageSource = TestConfiguration.SAVE_PAGE_SOURCE;
+        Configuration.reopenBrowserOnFail = TestConfiguration.REOPEN_BROWSER_ON_FAIL;
+        Configuration.browser = TestConfiguration.BROWSER;
+        Configuration.baseUrl = TestConfiguration.BASE_WEB_URL;
+        Configuration.timeout = 10000;
+        Configuration.browserSize = TestConfiguration.BROWSER_SIZE;
+        SelenideLogger.addListener("allure", new AllureSelenide().savePageSource(false));
+    }
+
+    public static void clearReports() {
+        log.info("Clearing reports dir [%s]...".formatted(TestConfiguration.REPORTS_FOLDER));
+        File allureResults = new File(TestConfiguration.REPORTS_FOLDER);
+        if (allureResults.isDirectory()) {
+            File[] list = allureResults.listFiles();
+            if (list != null) {
+                Arrays.stream(list)
+                        .sequential()
+                        .filter(e -> !e.getName().equals("categories.json"))
+                        .forEach(File::delete);
+            }
+        }
+    }
 }
--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/tests/ConnectorsTests.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/tests/ConnectorsTests.java
@@ -5,17 +5,19 @@ import com.provectus.kafka.ui.extensions.FileUtils;
 import com.provectus.kafka.ui.helpers.ApiHelper;
 import com.provectus.kafka.ui.helpers.Helpers;
 import lombok.SneakyThrows;
-import org.junit.Ignore;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-@Disabled // TODO #1480
 public class ConnectorsTests extends BaseTest {
 
-    public static final String LOCAL = "local";
+    public static final String LOCAL_CLUSTER = "local";
     public static final String SINK_CONNECTOR = "sink_postgres_activities_e2e_checks";
     public static final String TOPIC_FOR_CONNECTOR = "topic_for_connector";
+    public static final String TOPIC_FOR_DELETE_CONNECTOR = "topic_for_delete_connector";
     public static final String TOPIC_FOR_UPDATE_CONNECTOR = "topic_for_update_connector";
-    public static final String FIRST = "first";
+    public static final String FIRST_CONNECTOR = "first";
     public static final String CONNECTOR_FOR_DELETE = "sink_postgres_activities_e2e_checks_for_delete";
     public static final String CONNECTOR_FOR_UPDATE = "sink_postgres_activities_e2e_checks_for_update";
 
@@ -24,60 +26,75 @@ public class ConnectorsTests extends BaseTest {
     public static void beforeAll() {
         ApiHelper apiHelper = Helpers.INSTANCE.apiHelper;
 
+        String connectorToDelete = FileUtils.getResourceAsString("delete_connector_config.json");
+        String connectorToUpdate = FileUtils.getResourceAsString("config_for_create_connector_via_api.json");
         String message = FileUtils.getResourceAsString("message_content_create_topic.json");
-        apiHelper.createTopic(LOCAL, TOPIC_FOR_CONNECTOR);
-        apiHelper.sendMessage(LOCAL, TOPIC_FOR_CONNECTOR, message, " ");
+
+        apiHelper.deleteTopic(LOCAL_CLUSTER, CONNECTOR_FOR_DELETE);
+
+        apiHelper.createTopic(LOCAL_CLUSTER, TOPIC_FOR_CONNECTOR);
+        apiHelper.sendMessage(LOCAL_CLUSTER, TOPIC_FOR_CONNECTOR, message, " ");
+
+        apiHelper.createTopic(LOCAL_CLUSTER, TOPIC_FOR_DELETE_CONNECTOR);
+        apiHelper.sendMessage(LOCAL_CLUSTER, TOPIC_FOR_DELETE_CONNECTOR, message, " ");
+
+        apiHelper.createTopic(LOCAL_CLUSTER, TOPIC_FOR_UPDATE_CONNECTOR);
+        apiHelper.sendMessage(LOCAL_CLUSTER, TOPIC_FOR_UPDATE_CONNECTOR, message, " ");
+
+        apiHelper.createConnector(LOCAL_CLUSTER, FIRST_CONNECTOR, CONNECTOR_FOR_DELETE, connectorToDelete);
+        apiHelper.createConnector(LOCAL_CLUSTER, FIRST_CONNECTOR, CONNECTOR_FOR_UPDATE, connectorToUpdate);
     }
 
     @AfterAll
     @SneakyThrows
     public static void afterAll() {
         ApiHelper apiHelper = Helpers.INSTANCE.apiHelper;
-        apiHelper.deleteConnector(LOCAL, FIRST, SINK_CONNECTOR);
-        apiHelper.deleteTopic(LOCAL, TOPIC_FOR_CONNECTOR);
+        apiHelper.deleteConnector(LOCAL_CLUSTER, FIRST_CONNECTOR, SINK_CONNECTOR);
+        apiHelper.deleteConnector(LOCAL_CLUSTER, FIRST_CONNECTOR, CONNECTOR_FOR_UPDATE);
+        apiHelper.deleteTopic(LOCAL_CLUSTER, TOPIC_FOR_CONNECTOR);
+        apiHelper.deleteTopic(LOCAL_CLUSTER, TOPIC_FOR_DELETE_CONNECTOR);
+        apiHelper.deleteTopic(LOCAL_CLUSTER, TOPIC_FOR_UPDATE_CONNECTOR);
     }
 
     @SneakyThrows
     @DisplayName("should create a connector")
     @Test
-    void createConnector() {
-        pages.openConnectorsList(LOCAL)
+    public void createConnector() {
+        pages.openConnectorsList(LOCAL_CLUSTER)
                 .isOnPage()
                 .clickCreateConnectorButton()
                 .isOnConnectorCreatePage()
                 .setConnectorConfig(
                         SINK_CONNECTOR,
-                        FileUtils.getResourceAsString("config_for_create_connector.json")
-                );
-        pages.openConnectorsList(LOCAL).connectorIsVisibleInList(SINK_CONNECTOR, TOPIC_FOR_CONNECTOR);
+                        FileUtils.getResourceAsString("config_for_create_connector.json"));
+        pages.openConnectorsList(LOCAL_CLUSTER)
+                .isOnPage()
+                .connectorIsVisibleInList(SINK_CONNECTOR, TOPIC_FOR_CONNECTOR);
     }
 
     //disable test due 500 error during create connector via api
     @SneakyThrows
     @DisplayName("should update a connector")
     @Test
-    @Disabled
-    void updateConnector() {
-        pages.openConnectorsList(LOCAL)
+    public void updateConnector() {
+        pages.openConnectorsList(LOCAL_CLUSTER)
                 .isOnPage()
                 .openConnector(CONNECTOR_FOR_UPDATE);
-        pages.openConnectorsView(LOCAL, CONNECTOR_FOR_UPDATE)
-                .openEditConfig()
-                .updateConnectorConfig(
-                        FileUtils.getResourceAsString("config_for_update_connector.json"));
-        pages.openConnectorsList(LOCAL).connectorIsVisibleInList(CONNECTOR_FOR_UPDATE, TOPIC_FOR_UPDATE_CONNECTOR);
+                pages.connectorsView.connectorIsVisibleOnOverview();
+        pages.connectorsView.openEditConfig()
+                        .updConnectorConfig(FileUtils.getResourceAsString("config_for_update_connector.json"));
+        pages.openConnectorsList(LOCAL_CLUSTER).connectorIsVisibleInList(CONNECTOR_FOR_UPDATE, TOPIC_FOR_UPDATE_CONNECTOR);
     }
 
     @SneakyThrows
     @DisplayName("should delete connector")
     @Test
-    @Disabled
-    void deleteConnector() {
-        pages.openConnectorsList(LOCAL)
+    public void deleteConnector() {
+        pages.openConnectorsList(LOCAL_CLUSTER)
                 .isOnPage()
                 .openConnector(CONNECTOR_FOR_DELETE);
-        pages.openConnectorsView(LOCAL, CONNECTOR_FOR_DELETE)
-                .clickDeleteButton();
-        pages.openConnectorsList(LOCAL).isNotVisible(CONNECTOR_FOR_DELETE);
+                pages.connectorsView.clickDeleteButton();
+        pages.openConnectorsList(LOCAL_CLUSTER)
+                .isNotVisible(CONNECTOR_FOR_DELETE);
     }
 }
