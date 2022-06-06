d032fb2c6dd9c2ac91ea9351b279fcef24bd3af3
iliax <ikuramshin@provectus.com>
2022-06-06 12:56:28 +0400

    wip

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/ReactiveAdminClient.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/ReactiveAdminClient.java
@@ -304,7 +304,7 @@ public class ReactiveAdminClient implements Closeable {
         .map(lst -> lst.stream().map(ConsumerGroupListing::groupId).collect(toList()));
   }
 
-  public Mono<Map<String, ConsumerGroupDescription>> describeConsumerGroups(List<String> groupIds) {
+  public Mono<Map<String, ConsumerGroupDescription>> describeConsumerGroups(Collection<String> groupIds) {
     return toMono(client.describeConsumerGroups(groupIds).all());
   }
 
@@ -342,6 +342,7 @@ public class ReactiveAdminClient implements Closeable {
 
   public Mono<Map<TopicPartition, Long>> listOffsets(Collection<TopicPartition> partitions,
                                                      OffsetSpec offsetSpec) {
+    //TODO: need to split this into multiple calls if number of target partitions is big
     return toMono(
         client.listOffsets(partitions.stream().collect(toMap(tp -> tp, tp -> offsetSpec))).all())
         .map(offsets -> offsets.entrySet()
