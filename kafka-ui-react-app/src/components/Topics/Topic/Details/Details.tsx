442f7e13945beb62add57a52f41ec181bb25c805
Oleg Shuralev <workshur@gmail.com>
2022-06-06 13:19:19 +0300

    Remove unused bulma styles

--- kafka-ui-react-app/src/components/Topics/Topic/Details/Details.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Details/Details.tsx
@@ -189,7 +189,8 @@ const Details: React.FC<Props> = ({
       <Navbar role="navigation">
         <NavLink
           to="."
-          className={({ isActive }) => (isActive ? 'is-active is-primary' : '')}
+          className={({ isActive }) => (isActive ? 'is-active' : '')}
+          end
         >
           Overview
         </NavLink>
