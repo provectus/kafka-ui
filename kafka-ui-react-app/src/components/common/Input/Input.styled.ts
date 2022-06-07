3cd0c2897a30b77854f5b95c06967df8b70efebf
Oleg Shur <workshur@gmail.com>
2022-06-07 11:15:30 +0300

    get rid of unused bulma styles. small UI refactoring (#2130)
    * Get rid of bulma
    
    * Refactor SecondaryTabs
    
    * Refactor Messages View
    
    * Refactor Inputs
    
    * get rid of unused components
    
    * refactoring
    
    * Remove unused bulma styles

--- kafka-ui-react-app/src/components/common/Input/Input.styled.ts
+++ kafka-ui-react-app/src/components/common/Input/Input.styled.ts
@@ -5,41 +5,45 @@ export interface InputProps {
   hasLeftIcon: boolean;
 }
 
+export const Wrapper = styled.div`
+  position: relative;
+`;
+
 export const Input = styled.input<InputProps>(
-  ({ theme, ...props }) => css`
-    border: 1px ${theme.input.borderColor.normal} solid;
+  ({ theme: { input }, inputSize, hasLeftIcon }) => css`
+    border: 1px ${input.borderColor.normal} solid;
     border-radius: 4px;
-    height: ${props.inputSize === 'M' ? '32px' : '40px'};
+    height: ${inputSize === 'M' ? '32px' : '40px'};
     width: 100%;
-    padding-left: ${props.hasLeftIcon ? '36px' : '12px'};
+    padding-left: ${hasLeftIcon ? '36px' : '12px'};
     font-size: 14px;
 
     &::placeholder {
-      color: ${theme.input.color.placeholder.normal};
+      color: ${input.color.placeholder.normal};
       font-size: 14px;
     }
     &:hover {
-      border-color: ${theme.input.borderColor.hover};
+      border-color: ${input.borderColor.hover};
     }
     &:focus {
       outline: none;
-      border-color: ${theme.input.borderColor.focus};
+      border-color: ${input.borderColor.focus};
       &::placeholder {
         color: transparent;
       }
     }
     &:disabled {
-      color: ${theme.input.color.disabled};
-      border-color: ${theme.input.borderColor.disabled};
+      color: ${input.color.disabled};
+      border-color: ${input.borderColor.disabled};
       cursor: not-allowed;
     }
     &:read-only {
-      color: ${theme.input.color.readOnly};
+      color: ${input.color.readOnly};
       border: none;
-      background-color: ${theme.input.backgroundColor.readOnly};
+      background-color: ${input.backgroundColor.readOnly};
       &:focus {
         &::placeholder {
-          color: ${theme.input.color.placeholder.readOnly};
+          color: ${input.color.placeholder.readOnly};
         }
       }
       cursor: not-allowed;
@@ -50,4 +54,22 @@ export const Input = styled.input<InputProps>(
 export const FormError = styled.p`
   color: ${({ theme }) => theme.input.error};
   font-size: 12px;
+`;
+
+interface InputIconProps {
+  className: string;
+  position: 'left' | 'right';
+  inputSize: 'M' | 'L';
+}
+
+export const InputIcon = styled.i<InputIconProps>`
+  position: absolute;
+  top: 50%;
+  line-height: 0;
+  z-index: 1;
+  left: ${({ position }) => (position === 'left' ? '12px' : 'unset')};
+  right: ${({ position }) => (position === 'right' ? '15px' : 'unset')};
+  height: 11px;
+  width: 11px;
+  color: ${({ theme }) => theme.input.icon.color};
 `;
