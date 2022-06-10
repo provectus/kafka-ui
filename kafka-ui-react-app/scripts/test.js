78e0e0a2b5ea611a638523197371ba1a77c163e7
Oleg Shuralev <workshur@gmail.com>
2022-06-10 12:46:13 +0300

    Cleanup

--- kafka-ui-react-app/scripts/test.js
+++ kafka-ui-react-app/scripts/test.js
@@ -16,26 +16,7 @@ process.on('unhandledRejection', err => {
 require('../config/env');
 
 const jest = require('jest');
-const execSync = require('child_process').execSync;
 let argv = process.argv.slice(2);
-
-function isInGitRepository() {
-  try {
-    execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-function isInMercurialRepository() {
-  try {
-    execSync('hg --cwd . root', { stdio: 'ignore' });
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
 
 // Watch unless on CI or explicitly running all tests
 if (
@@ -43,9 +24,7 @@ if (
   argv.indexOf('--watchAll') === -1 &&
   argv.indexOf('--watchAll=false') === -1
 ) {
-  // https://github.com/facebook/create-react-app/issues/5210
-  const hasSourceControl = isInGitRepository() || isInMercurialRepository();
-  argv.push(hasSourceControl ? '--watch' : '--watchAll');
+  argv.push('--watch');
 }
 
 
---
+++ kafka-ui-react-app/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, loadEnv, UserConfigExport } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const defaultConfig: UserConfigExport = {
+    plugins: [react(), tsconfigPaths()],
+    define: {
+      'process.env.NODE_ENV': `"${mode}"`,
+    },
+  };
+
+  if (mode === 'development' && process.env.VITE_DEV_PROXY) {
+    return {
+      ...defaultConfig,
+      server: {
+        open: true,
+        proxy: {
+          '/api': {
+            target: process.env.VITE_DEV_PROXY,
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      },
+    };
+  }
+
+  return defaultConfig;
+});
