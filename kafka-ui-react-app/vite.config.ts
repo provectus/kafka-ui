1b71ccb9755041e18ff540d5c57318243b38876d
Oleg Shur <workshur@gmail.com>
2022-06-15 16:59:02 +0300

    Migrate React app to Vite (#2153)
    * Ejected CRA
    
    * Cleanup
    
    * jest
    
    * Cleanup
    
    * refactor cluster widgets
    
    * configure build
    
    * configure build
    
    * configure coverage
    
    * Experiment with lazy
    
    * add ace to vendor
    
    * Cleanup

---
+++ kafka-ui-react-app/vite.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig, loadEnv, UserConfigExport } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const defaultConfig: UserConfigExport = {
+    plugins: [react(), tsconfigPaths()],
+    build: {
+      outDir: 'build',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            venod: [
+              'react',
+              'react-router-dom',
+              'react-dom',
+              'redux',
+              'redux-thunk',
+              'react-redux',
+              'styled-components',
+              'react-ace',
+            ],
+            lodash: ['lodash'],
+          },
+        },
+      },
+    },
+    define: {
+      'process.env.NODE_ENV': `"${mode}"`,
+      'process.env.VITE_TAG': `"${process.env.VITE_TAG}"`,
+      'process.env.GIT_COMMIT': `"${process.env.VITE_COMMIT}"`,
+    },
+  };
+  const proxy = process.env.VITE_DEV_PROXY;
+
+  if (mode === 'development' && proxy) {
+    return {
+      ...defaultConfig,
+      server: {
+        open: true,
+        proxy: {
+          '/api': {
+            target: proxy,
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      },
+    };
+  }
+
+  return defaultConfig;
+});
