24ca33c13c37e4079713e384e218f291d76a6aff
ssiradeghyan <ssiradeghyan@provectus.com>
2022-06-15 16:02:10 +0400

    add test for paths

--- kafka-ui-react-app/src/lib/__test__/paths.spec.ts
+++ kafka-ui-react-app/src/lib/__test__/paths.spec.ts
@@ -1,11 +1,12 @@
 import { GIT_REPO_LINK } from 'lib/constants';
 import * as paths from 'lib/paths';
-import { RouteParams } from 'lib/paths';
+import { clusterBrokerMetricsPath, RouteParams } from 'lib/paths';
 
 const clusterName = 'test-cluster-name';
 const groupId = 'test-group-id';
 const schemaId = 'test-schema-id';
 const topicId = 'test-topic-id';
+const brokerId = 'test-Broker-id';
 const connectName = 'test-connect-name';
 const connectorName = 'test-connector-name';
 
@@ -29,6 +30,23 @@ describe('Paths', () => {
     );
     expect(paths.clusterBrokersPath()).toEqual(
       paths.clusterBrokersPath(RouteParams.clusterName)
+    );
+
+    expect(paths.clusterBrokerPath(clusterName, brokerId)).toEqual(
+      `${paths.clusterPath(clusterName)}/brokers/${brokerId}`
+    );
+    expect(paths.clusterBrokerPath()).toEqual(
+      paths.clusterBrokerPath(RouteParams.clusterName, RouteParams.brokerId)
+    );
+
+    expect(paths.clusterBrokerMetricsPath(clusterName, brokerId)).toEqual(
+      `${paths.clusterPath(clusterName)}/brokers/${brokerId}/metrics`
+    );
+    expect(paths.clusterBrokerMetricsPath()).toEqual(
+      paths.clusterBrokerMetricsPath(
+        RouteParams.clusterName,
+        RouteParams.brokerId
+      )
     );
   });
   it('clusterConsumerGroupsPath', () => {
