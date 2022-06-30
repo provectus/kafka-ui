9f2310472dd63eb8c49ab11fc0258f617a8a6b33
Smbat Siradeghyan <104761619+ssiradeghyan@users.noreply.github.com>
2022-06-30 13:22:52 +0400

    [ BROKER ] Add broker metrics page (#2157)
    * add metrics page
    
    * add test for broker
    
    * add test for paths
    
    * remove unused-vars
    
    * add editor view for broker metrics
    
    * add not available message
    
    * try to fix BrokerLogdir tests
    
    * add tests for translateLogdir and translateLogdirs
    
    * add tests for translateLogdir and translateLogdirs
    
    * add tests for metrics page
    
    * fix review comments
    
    * fix unused var
    
    * remove describe duplicate
    
    Co-authored-by: Oleg Shur <workshur@gmail.com>

--- kafka-ui-react-app/src/components/Brokers/__test__/fixtures.ts
+++ kafka-ui-react-app/src/components/Brokers/__test__/fixtures.ts
@@ -1,43 +1,11 @@
 import { BrokersLogdirs } from 'generated-sources';
 
 export const brokersPayload = [
-  { id: 1, host: 'b-1.test.kafka.amazonaws.com' },
-  { id: 2, host: 'b-2.test.kafka.amazonaws.com' },
+  { id: 1, host: 'b-1.test.kafka.amazonaws.com', port: 9092 },
+  { id: 2, host: 'b-2.test.kafka.amazonaws.com', port: 9092 },
 ];
 
 export const clusterStatsPayload = {
-  brokerCount: 2,
-  activeControllers: 1,
-  onlinePartitionCount: 138,
-  offlinePartitionCount: 0,
-  inSyncReplicasCount: 239,
-  outOfSyncReplicasCount: 0,
-  underReplicatedPartitionCount: 0,
-  diskUsage: [
-    { brokerId: 0, segmentSize: 334567, segmentCount: 245 },
-    { brokerId: 1, segmentSize: 12345678, segmentCount: 121 },
-  ],
-  version: '2.2.1',
-};
-
-export const initialBrokersReducerState = {
-  items: brokersPayload,
-  brokerCount: 2,
-  activeControllers: 1,
-  onlinePartitionCount: 138,
-  offlinePartitionCount: 0,
-  inSyncReplicasCount: 239,
-  outOfSyncReplicasCount: 0,
-  underReplicatedPartitionCount: 0,
-  diskUsage: [
-    { brokerId: 0, segmentSize: 1111, segmentCount: 333 },
-    { brokerId: 1, segmentSize: 2222, segmentCount: 444 },
-  ],
-  version: '2.2.1',
-};
-
-export const updatedBrokersReducerState = {
-  items: brokersPayload,
   brokerCount: 2,
   activeControllers: 1,
   onlinePartitionCount: 138,
