4c2ddb2e8492cdded440efdf1815f595b0e55203
Oleg Shuralev <workshur@gmail.com>
2022-08-08 13:02:38 +0300

    Typo

--- kafka-ui-react-app/src/components/common/NewTable/TimestampCell.tsx
+++ kafka-ui-react-app/src/components/common/NewTable/TimestampCell.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 import * as S from './Table.styled';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const TimestapmCell: React.FC<CellContext<any, any>> = ({ getValue }) => (
+const TimestampCell: React.FC<CellContext<any, any>> = ({ getValue }) => (
   <S.Nowrap>{dayjs(getValue()).format('MM.DD.YY hh:mm:ss a')}</S.Nowrap>
 );
 
-export default TimestapmCell;
+export default TimestampCell;
