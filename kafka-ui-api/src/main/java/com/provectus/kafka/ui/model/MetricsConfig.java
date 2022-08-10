07abb64fb28014b269c520710baf04cfba303429
Daniel Ching <daching@provectus.com>
2022-08-10 11:46:27 -0600

    test

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/model/MetricsConfig.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/model/MetricsConfig.java
@@ -13,9 +13,8 @@ public class MetricsConfig {
   public static final String PROMETHEUS_METRICS_TYPE = "PROMETHEUS";
 
   private final String type = JMX_METRICS_TYPE;
-  private final Integer port; //jmxPort, jmxExporterPort
-  private final boolean ssl; //jmxssl
-  private final String username; //jmxUsername
-  private final String password; //jmxPassword
-
+  private final Integer port;
+  private final boolean ssl;
+  private final String username;
+  private final String password;
 }
