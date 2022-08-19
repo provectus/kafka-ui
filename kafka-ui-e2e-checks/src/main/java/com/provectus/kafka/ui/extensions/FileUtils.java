57242a84619d29cd9f71969e22763e7c5510c81b
anezboretskiy <anezboretskiy@modeln.com>
2022-08-19 12:23:03 +0300

    Update FileUtils #2431

--- kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/extensions/FileUtils.java
+++ kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/extensions/FileUtils.java
@@ -1,6 +1,5 @@
 package com.provectus.kafka.ui.extensions;
 
-import lombok.extern.slf4j.Slf4j;
 import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -8,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 
 import static org.apache.kafka.common.utils.Utils.readFileAsString;
 
-@Slf4j
 public class FileUtils {
 
     public static String getResourceAsString(String resourceFileName) {
