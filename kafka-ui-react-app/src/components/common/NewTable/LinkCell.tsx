8c1ad06dc0c513e5fcbdac123f2d304d2adbf001
Kris-K-Dev <92114648+Kris-K-Dev@users.noreply.github.com>
2022-09-14 09:20:41 -0500

    Fix URL encoding upon retrieving schemas (#2567)
    Co-authored-by: Roman Zabaluev <rzabaluev@provectus.com>

--- kafka-ui-react-app/src/components/common/NewTable/LinkCell.tsx
+++ kafka-ui-react-app/src/components/common/NewTable/LinkCell.tsx
@@ -7,7 +7,7 @@ const LinkCell: React.FC<CellContext<any, unknown>> = ({ getValue }) => {
   const value = `${getValue<string | number>()}`;
   const handleClick: React.MouseEventHandler = (e) => e.stopPropagation();
   return (
-    <NavLink to={value} title={value} onClick={handleClick}>
+    <NavLink to={encodeURIComponent(value)} title={value} onClick={handleClick}>
       {value}
     </NavLink>
   );
