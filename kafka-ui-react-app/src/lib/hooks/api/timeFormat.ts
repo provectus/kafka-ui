4d2c8bbfb6526e5e95547ef1f207b6b07f528805
habrahamyanpro <habrahamyan@provectus.com>
2022-09-19 12:00:59 +0400

    add fetchTimeFormat function

--- kafka-ui-react-app/src/lib/hooks/api/timeFormat.ts
+++ kafka-ui-react-app/src/lib/hooks/api/timeFormat.ts
@@ -1,8 +1,13 @@
-import { timerStampFormat as api } from 'lib/api';
 import { useQuery } from '@tanstack/react-query';
+import { BASE_PARAMS } from 'lib/constants';
+
+const fetchTimeFormat = async () => {
+  const data = await fetch('/api/info/timestampformat', BASE_PARAMS).then(
+    (res) => res.json()
+  );
+  return data;
+};
 
 export function useTimeFormatStats() {
-  return useQuery(['timestampformat'], () => api.getTimeStampFormat(), {
-    enabled: false,
-  });
+  return useQuery(['timestampformat'], fetchTimeFormat);
 }
