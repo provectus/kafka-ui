63719d718dab1403bba02ccfef2eaecd391e6b01
Arthur <79512124+ArthurNiedial@users.noreply.github.com>
2022-09-27 13:02:56 +0300

    Issue_2438 (#2649)
    * [e2e] create Setup class and refactoring of BaseTest #2438
    
    * [e2e] create Setup class and refactoring of BaseTest #2438
    
    Co-authored-by: Vlad Senyuta <66071557+VladSenyuta@users.noreply.github.com>

---
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/base/Setup.java
@@ -0,0 +1,41 @@
+package com.provectus.kafka.ui.base;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.Arrays;
+
+@Slf4j
+public abstract class Setup {
+
+    @SneakyThrows
+    static void setup() {
+        Configuration.reportsFolder = Config.REPORTS_FOLDER;
+        Configuration.screenshots = Config.SCREENSHOTS;
+        Configuration.savePageSource = Config.SAVE_PAGE_SOURCE;
+        Configuration.reopenBrowserOnFail = Config.REOPEN_BROWSER_ON_FAIL;
+        Configuration.browser = Config.BROWSER;
+        Configuration.timeout = 10000;
+        Configuration.pageLoadTimeout = 180000;
+        Configuration.browserSize = Config.BROWSER_SIZE;
+        SelenideLogger.addListener("allure", new AllureSelenide().savePageSource(false));
+    }
+
+    public static void clearReports() {
+        log.info(String.format("Clearing reports dir [%s]...", Config.REPORTS_FOLDER));
+        File allureResults = new File(Config.REPORTS_FOLDER);
+        if (allureResults.isDirectory()) {
+            File[] list = allureResults.listFiles();
+            if (list != null) {
+                Arrays.stream(list)
+                        .sequential()
+                        .filter(e -> !e.getName().equals("categories.json"))
+                        .forEach(File::delete);
+            }
+        }
+    }
+}
