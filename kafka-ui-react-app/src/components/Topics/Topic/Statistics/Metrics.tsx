69113c628e316063d2c62f0144226e7164f4ae37
habrahamyanpro <habrahamyan@provectus.com>
2022-10-04 10:06:13 +0400

    fixed lint problems

--- kafka-ui-react-app/src/components/Topics/Topic/Statistics/Metrics.tsx
+++ kafka-ui-react-app/src/components/Topics/Topic/Statistics/Metrics.tsx
@@ -1,4 +1,4 @@
-import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   useAnalyzeTopic,
   useCancelTopicAnalysis,
