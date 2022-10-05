5bfa01b0e26c73757af1bcf2d2dfff8f040ec474
davitbejanyan <dbejanyan@provectus.com>
2022-10-06 01:11:53 +0400

    Migrate Topic Overview to the new version of table #2684

---
+++ kafka-ui-react-app/src/components/Topics/Topic/Overview/ActionsCell.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Partition } from 'generated-sources';
+import { CellContext } from '@tanstack/react-table';
+import { useAppDispatch } from 'lib/hooks/redux';
+import ClusterContext from 'components/contexts/ClusterContext';
+import { RouteParamsClusterTopic } from 'lib/paths';
+import useAppParams from 'lib/hooks/useAppParams';
+import { clearTopicMessages } from 'redux/reducers/topicMessages/topicMessagesSlice';
+import { Dropdown, DropdownItem } from 'components/common/Dropdown';
+import { useTopicDetails } from 'lib/hooks/api/topics';
+
+const ActionsCell: React.FC<CellContext<Partition, unknown>> = ({ row }) => {
+  const { clusterName, topicName } = useAppParams<RouteParamsClusterTopic>();
+  const { data } = useTopicDetails({ clusterName, topicName });
+  const { isReadOnly } = React.useContext(ClusterContext);
+  const { partition } = row.original;
+  const dispatch = useAppDispatch();
+
+  const clearTopicMessagesHandler = async () => {
+    await dispatch(
+      clearTopicMessages({ clusterName, topicName, partitions: [partition] })
+    ).unwrap();
+  };
+
+  return (
+    <div>
+      {!data?.internal && !isReadOnly && data?.cleanUpPolicy === 'DELETE' ? (
+        <Dropdown>
+          <DropdownItem onClick={clearTopicMessagesHandler} danger>
+            Clear Messages
+          </DropdownItem>
+        </Dropdown>
+      ) : null}
+    </div>
+  );
+};
+
+export default ActionsCell;
