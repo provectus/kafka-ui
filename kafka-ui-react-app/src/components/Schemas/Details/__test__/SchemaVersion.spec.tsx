06177cec097d1e3eddff9617ceabeb7a4758b0e3
habrahamyanpro <habrahamyan@provectus.com>
2022-10-07 10:34:51 +0400

    change get row ogigin data from fixtures

--- kafka-ui-react-app/src/components/Schemas/Details/__test__/SchemaVersion.spec.tsx
+++ kafka-ui-react-app/src/components/Schemas/Details/__test__/SchemaVersion.spec.tsx
@@ -4,21 +4,15 @@ import { render } from 'lib/testHelpers';
 import { SchemaSubject } from 'generated-sources';
 import { Row } from '@tanstack/react-table';
 
+import { jsonSchema } from './fixtures';
+
 export interface Props {
   row: Row<SchemaSubject>;
 }
 
 const renderComponent = () => {
   const row = {
-    original: {
-      subject: 'Avrotest',
-      version: '5',
-      id: 106,
-      schema:
-        '{"type":"record","name":"Student","namespace":"DataFlair","fields":[{"name":"Name","type":"string"},{"name":"Age","type":"int"},{"name":"Aged","type":"int"},{"name":"Ages","type":"int"},{"name":"Ageas","type":"int"},{"name":"Ageasa","type":"int"}]}',
-      compatibilityLevel: 'BACKWARD',
-      schemaType: 'AVRO',
-    },
+    original: jsonSchema,
   };
 
   return render(<SchemaVersion row={row as Row<SchemaSubject>} />);
