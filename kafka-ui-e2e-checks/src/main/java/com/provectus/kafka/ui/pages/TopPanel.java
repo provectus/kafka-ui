07f7161c3e9f09d0d7f30ebba6dedb66c0948adf
anezboretskiy <anezboretskiy@provectus.com>
2022-10-31 17:49:19 +0200

    Resolve conversation

--- kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/pages/TopPanel.java
+++ kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/pages/TopPanel.java
@@ -12,10 +12,10 @@ public class TopPanel {
     protected SelenideElement kafkaVersion = $x("//a[@title='Current commit']");
     protected SelenideElement logOutBtn = $x("//button[contains(text(),'Log out')]");
     protected SelenideElement gitBtn = $x("//a[@href='https://github.com/provectus/kafka-ui']");
-    protected SelenideElement discordBtn = $x("//a[@href='https://discord.com/invite/4DWzD7pGE5']");
+    protected SelenideElement discordBtn = $x("//button[contains(text(),'Log out')]/../parent::div/a[last()]");
 
     public List<SelenideElement> getAllVisibleElements() {
-        return Arrays.asList(kafkaLogo, kafkaVersion, logOutBtn);
+        return Arrays.asList(kafkaLogo, kafkaVersion, logOutBtn, gitBtn, discordBtn);
     }
 
     public List<SelenideElement> getAllEnabledElements() {
