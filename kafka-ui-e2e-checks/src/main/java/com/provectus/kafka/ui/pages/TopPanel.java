73ac5e0b869392cd6e6ea5756b050db231f984e7
anezboretskiy <anezboretskiy@provectus.com>
2022-11-01 12:58:22 +0200

    Reformat discordBtn locator

--- kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/pages/TopPanel.java
+++ kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/pages/TopPanel.java
@@ -12,7 +12,7 @@ public class TopPanel {
     protected SelenideElement kafkaVersion = $x("//a[@title='Current commit']");
     protected SelenideElement logOutBtn = $x("//button[contains(text(),'Log out')]");
     protected SelenideElement gitBtn = $x("//a[@href='https://github.com/provectus/kafka-ui']");
-    protected SelenideElement discordBtn = $x("//button[contains(text(),'Log out')]/../parent::div/a[last()]");
+    protected SelenideElement discordBtn = $x("//a[contains(@href,'https://discord.com/invite')]");
 
     public List<SelenideElement> getAllVisibleElements() {
         return Arrays.asList(kafkaLogo, kafkaVersion, logOutBtn, gitBtn, discordBtn);
