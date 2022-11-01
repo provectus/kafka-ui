2669dc188240ea3ab3349248143f0f5405383beb
habrahamyanpro <habrahamyan@provectus.com>
2022-11-01 13:49:07 +0400

    fixed test changes

--- kafka-ui-react-app/src/components/Topics/New/__test__/New.spec.tsx
+++ kafka-ui-react-app/src/components/Topics/New/__test__/New.spec.tsx
@@ -21,11 +21,9 @@ jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockNavigate,
 }));
-
 jest.mock('lib/hooks/api/topics', () => ({
   useCreateTopic: jest.fn(),
 }));
-
 const renderComponent = (path: string) => {
   render(
     <Routes>
@@ -37,7 +35,6 @@ const renderComponent = (path: string) => {
   );
 };
 const createTopicMock = jest.fn();
-
 describe('New', () => {
   beforeEach(() => {
     (useCreateTopic as jest.Mock).mockImplementation(() => ({
@@ -47,90 +44,117 @@ describe('New', () => {
   afterEach(() => {
     mockNavigate.mockClear();
   });
-
   it('checks header for create new', async () => {
     await act(() => renderComponent(clusterTopicNewPath(clusterName)));
     expect(screen.getByRole('heading', { name: 'Create' })).toBeInTheDocument();
   });
-
   it('checks header for copy', async () => {
     await act(() =>
       renderComponent(`${clusterTopicCopyPath(clusterName)}?name=test`)
     );
     expect(screen.getByRole('heading', { name: 'Copy' })).toBeInTheDocument();
   });
-
   it('validates form', async () => {
-    await act(() => renderComponent(clusterTopicNewPath(clusterName)));
-    await waitFor(async () => {
-      await userEvent.type(
-        screen.getByPlaceholderText('Topic Name'),
-        topicName
-      );
-    });
-    await waitFor(async () => {
-      await userEvent.clear(screen.getByPlaceholderText('Topic Name'));
-    });
-    await waitFor(() => {
-      expect(screen.getByText('name is a required field')).toBeInTheDocument();
-    });
+    await renderComponent(clusterTopicNewPath(clusterName));
+    await userEvent.type(screen.getByPlaceholderText('Topic Name'), topicName);
+    await userEvent.clear(screen.getByPlaceholderText('Topic Name'));
+    await userEvent.tab();
+    await expect(
+      screen.getByText('name is a required field')
+    ).toBeInTheDocument();
+
+    await userEvent.type(
+      screen.getByLabelText('Number of partitions *'),
+      minValue
+    );
+    await userEvent.clear(screen.getByLabelText('Number of partitions *'));
+    await userEvent.tab();
+    await expect(
+      screen.getByText('Number of partitions is required and must be a number')
+    ).toBeInTheDocument();
+
+    await userEvent.type(
+      screen.getByLabelText('Min In Sync Replicas *'),
+      minValue
+    );
+    await userEvent.clear(screen.getByLabelText('Min In Sync Replicas *'));
+    await userEvent.tab();
+    await expect(
+      screen.getByText('Min in sync replicas is required and must be a number')
+    ).toBeInTheDocument();
+
+    await userEvent.type(
+      screen.getByLabelText('Replication Factor *'),
+      minValue
+    );
+    await userEvent.clear(screen.getByLabelText('Replication Factor *'));
+    await userEvent.tab();
+    await expect(
+      screen.getByText('Replication factor is required and must be a number')
+    ).toBeInTheDocument();
+
+    await userEvent.type(
+      screen.getByLabelText('Time to retain data (in ms)'),
+      retainTime
+    );
+    await userEvent.clear(screen.getByLabelText('Time to retain data (in ms)'));
+    await userEvent.tab();
+    await expect(
+      screen.getByText('Time to retain data is required and must be a number')
+    ).toBeInTheDocument();
+
+    await userEvent.type(
+      screen.getByLabelText('Maximum message size in bytes *'),
+      minValue
+    );
+    await userEvent.clear(
+      screen.getByLabelText('Maximum message size in bytes *')
+    );
+    await userEvent.tab();
+    await expect(
+      screen.getByText('Maximum message size is required and must be a number')
+    ).toBeInTheDocument();
+
     expect(createTopicMock).not.toHaveBeenCalled();
     expect(mockNavigate).not.toHaveBeenCalled();
   });
 
   it('validates form invalid name', async () => {
-    await act(() => renderComponent(clusterTopicNewPath(clusterName)));
-    await waitFor(() => {
-      userEvent.type(screen.getByPlaceholderText('Topic Name'), 'Invalid,Name');
-    });
-    await waitFor(() => {
-      expect(
-        screen.getByText('Only alphanumeric, _, -, and . allowed')
-      ).toBeInTheDocument();
-    });
+    await renderComponent(clusterTopicNewPath(clusterName));
+    await userEvent.type(
+      screen.getByPlaceholderText('Topic Name'),
+      'Invalid,Name'
+    );
+    await expect(
+      screen.getByText('Only alphanumeric, _, -, and . allowed')
+    ).toBeInTheDocument();
   });
 
   it('submits valid form', async () => {
-    await act(() => renderComponent(clusterTopicNewPath(clusterName)));
-    await act(async () => {
-      await userEvent.type(
-        screen.getByPlaceholderText('Topic Name'),
-        topicName
-      );
-    });
-    await act(() => {
-      userEvent.type(
-        screen.getByPlaceholderText('Number of partitions'),
-        minValue
-      );
-    });
-    await act(() => {
-      userEvent.type(
-        screen.getByPlaceholderText('Min In Sync Replicas'),
-        minValue
-      );
-    });
-    await act(() => {
-      userEvent.type(
-        screen.getByPlaceholderText('Replication Factor'),
-        minValue
-      );
-    });
-    await act(() => {
-      userEvent.type(
-        screen.getByPlaceholderText('Time to retain data (in ms)'),
-        retainTime
-      );
-    });
-    await act(() => {
-      userEvent.type(
-        screen.getByPlaceholderText('Maximum message size'),
-        minValue
-      );
-    });
-    await act(() => {
-      userEvent.click(screen.getByText('Create topic'));
-    });
+    await renderComponent(clusterTopicNewPath(clusterName));
+    await userEvent.type(screen.getByLabelText('Topic Name *'), topicName);
+    await userEvent.type(
+      screen.getByLabelText('Number of partitions *'),
+      minValue
+    );
+    await userEvent.type(
+      screen.getByLabelText('Min In Sync Replicas *'),
+      minValue
+    );
+    await userEvent.type(
+      screen.getByLabelText('Replication Factor *'),
+      minValue
+    );
+    await userEvent.type(
+      screen.getByLabelText('Time to retain data (in ms)'),
+      retainTime
+    );
+    await userEvent.type(
+      screen.getByLabelText('Maximum message size in bytes *'),
+      minValue
+    );
+    expect(screen.getByText('Create topic')).toBeEnabled();
+    await userEvent.click(screen.getByText('Create topic'));
     await waitFor(() => expect(createTopicMock).toHaveBeenCalledTimes(1));
     await waitFor(() =>
       expect(mockNavigate).toHaveBeenCalledWith(`../${topicName}`)
