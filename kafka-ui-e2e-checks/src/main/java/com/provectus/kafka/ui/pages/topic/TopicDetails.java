c5b207b7a17736aa2a9759c8ace502c8302bea16
Mgrdich <46796009+Mgrdich@users.noreply.github.com>
2022-11-07 17:01:57 +0400

    [FE] Add a space to formatted sizes (#2848)
    * Add space to formatted sizes + modification in the test components
    
    * Bytes-format component to be treated as a single word
    
    * fix TopicTests()
    
    * Merge branch 'master' into issues/2676
    
    Co-authored-by: Vlad Senyuta <66071557+VladSenyuta@users.noreply.github.com>
    Co-authored-by: VladSenyuta <vlad.senyuta@gmail.com>

--- kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/pages/topic/TopicDetails.java
+++ kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/pages/topic/TopicDetails.java
@@ -21,7 +21,7 @@ public class TopicDetails {
     protected SelenideElement clearMessagesBtn = $x(("//div[contains(text(), 'Clear messages')]"));
     protected SelenideElement overviewTab = $x("//a[contains(text(),'Overview')]");
     protected SelenideElement messagesTab = $x("//a[contains(text(),'Messages')]");
-    protected SelenideElement editSettingsTab = $x("//li[@role][contains(text(),'Edit settings')]");
+    protected SelenideElement editSettingsMenu = $x("//li[@role][contains(text(),'Edit settings')]");
     protected SelenideElement removeTopicBtn = $x("//ul[@role='menu']//div[contains(text(),'Remove Topic')]");
     protected SelenideElement confirmBtn = $x("//div[@role='dialog']//button[contains(text(),'Confirm')]");
     protected SelenideElement produceMessageBtn = $x("//div//button[text()='Produce Message']");
@@ -39,7 +39,7 @@ public class TopicDetails {
     }
 
     @Step
-    public TopicDetails openTopicMenu(TopicMenu menu) {
+    public TopicDetails openDetailsTab(TopicMenu menu) {
         $(By.linkText(menu.getValue())).shouldBe(Condition.visible).click();
         return this;
     }
@@ -52,7 +52,7 @@ public class TopicDetails {
 
     @Step
     public TopicDetails clickEditSettingsMenu() {
-        editSettingsTab.shouldBe(Condition.visible).click();
+        editSettingsMenu.shouldBe(Condition.visible).click();
         return this;
     }
 
