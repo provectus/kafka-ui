deb718758c256becdca9b6fe088cfa7aba7c0ff9
Mgrdich <mgotm13@gmail.com>
2022-11-10 00:47:53 +0400

    minor typescript code modifications for the cluster required parameter in the rolesHelper

--- kafka-ui-react-app/src/lib/rolesHelper.ts
+++ kafka-ui-react-app/src/lib/rolesHelper.ts
@@ -5,7 +5,7 @@ export type RolesType = UserPermission[];
 export function modifyRolesData(data?: RolesType): Map<string, RolesType> {
   const map = new Map<string, RolesType>();
   data?.forEach((item) => {
-    item.clusters?.forEach((name) => {
+    item.clusters.forEach((name) => {
       const res = map.get(name);
       if (res) {
         map.set(name, res.concat(item));
