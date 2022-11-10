8f7c47a856e45928858ddfd2f340832c69247dfb
Mgrdich <mgotm13@gmail.com>
2022-11-11 02:59:23 +0400

    Add ActionButton Component to handle the Button with tooltip

--- kafka-ui-react-app/src/components/common/ActionButton/ActionButton.styled.ts
+++ kafka-ui-react-app/src/components/common/ActionButton/ActionButton.styled.ts
@@ -6,3 +6,14 @@ export const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
+export const MessageTooltip = styled.div`
+  max-width: 100%;
+  max-height: 100%;
+  background-color: ${({ theme }) => theme.tooltip.bg};
+  color: ${({ theme }) => theme.tooltip.text};
+  border-radius: 6px;
+  padding: 5px;
+  z-index: 1;
+  white-space: pre-wrap;
+`;
--- kafka-ui-react-app/src/components/common/ActionButton/ActionButton.tsx
+++ kafka-ui-react-app/src/components/common/ActionButton/ActionButton.tsx
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Props as ButtonProps } from 'components/common/Button/Button';
+import {
+  Placement,
+  useFloating,
+  useHover,
+  useInteractions,
+} from '@floating-ui/react-dom-interactions';
 
 import * as S from './ActionButton.styled';
 
 interface Props extends ButtonProps {
   canDoAction: boolean;
+  message?: string;
+  placement?: Placement;
 }
 
-const ActionButton: React.FC<Props> = ({ canDoAction, disabled, ...props }) => {
+const ActionButton: React.FC<Props> = ({
+  canDoAction,
+  placement = 'bottom-end',
+  message = "You don't have the permission to do this action",
+  disabled,
+  ...props
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const isDisabled = !canDoAction;
+
+  const setTooltipOpen = (state: boolean) => {
+    if (!isDisabled) return;
+    setOpen(state);
+  };
+
+  const { x, y, reference, floating, strategy, context } = useFloating({
+    open,
+    onOpenChange: setTooltipOpen,
+    placement,
+  });
+
+  useInteractions([useHover(context)]);
+
   return (
-    <S.Wrapper>
-      <Button {...props} disabled={disabled || !canDoAction} />
+    <S.Wrapper ref={reference}>
+      <Button {...props} disabled={disabled || isDisabled} />
+      {open && (
+        <S.MessageTooltip
+          ref={floating}
+          style={{
+            position: strategy,
+            top: y ?? 0,
+            left: x ?? 0,
+            width: 'max-content',
+          }}
+        >
+          {message}
+        </S.MessageTooltip>
+      )}
     </S.Wrapper>
   );
 };
