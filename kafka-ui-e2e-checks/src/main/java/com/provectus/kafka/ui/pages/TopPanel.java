04e554d017f8e610e88bf13300763e2f5227826f
Alexandr Nezboretskiy <88882353+anezboretskiy@users.noreply.github.com>
2022-11-11 15:19:11 +0200

    Issue/copy checking topics section within kafka UI application (#2925)
    * Commit
    
    * Moved and renamed classes from Tests
    
    * Added new methods and refactored getRandomMessage()
    
    * Moved SmokeTests from tests to com.provectus.kafka.ui
    
    * Moved SmokeTests from com.provectus.kafka.ui to Suite
    
    * resolve conversation
    
    * resolve conversation

--- kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/pages/TopPanel.java
+++ kafka-ui-e2e-checks/src/main/java/com/provectus/kafka/ui/pages/TopPanel.java
@@ -1,13 +1,12 @@
 package com.provectus.kafka.ui.pages;
 
-import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$x;
 
+import com.codeborne.selenide.SelenideElement;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.codeborne.selenide.Selenide.$x;
-
-public class TopPanel {
+public class TopPanel extends BasePage{
     protected SelenideElement kafkaLogo = $x("//a[contains(text(),'UI for Apache Kafka')]");
     protected SelenideElement kafkaVersion = $x("//a[@title='Current commit']");
     protected SelenideElement logOutBtn = $x("//button[contains(text(),'Log out')]");
