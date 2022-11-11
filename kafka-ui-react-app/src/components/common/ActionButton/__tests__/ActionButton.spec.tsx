54fd5b2756a61a6df4067c0a4a19193cc898cd5d
Mgrdich <mgotm13@gmail.com>
2022-11-11 12:52:18 +0400

    ActionButton add test suites

--- kafka-ui-react-app/src/components/common/ActionButton/__tests__/ActionButton.spec.tsx
+++ kafka-ui-react-app/src/components/common/ActionButton/__tests__/ActionButton.spec.tsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import { render } from 'lib/testHelpers';
 import ActionButton from 'components/common/ActionButton/ActionButton';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('ActionButton', () => {
-  it('should make the button disable and view the tooltip', () => {
+  const tooltipText = "You don't have the permission to do this action";
+
+  it('should render the button with the correct text, for the permission tooltip not to show', async () => {
     render(
-      <ActionButton buttonType="primary" buttonSize="M" canDoAction={false} />
+      <ActionButton buttonType="primary" buttonSize="M" canDoAction>
+        test
+      </ActionButton>
     );
+    const button = screen.getByRole('button', { name: 'test' });
+    expect(button).toBeInTheDocument();
+    expect(button).toBeEnabled();
+    await userEvent.hover(button);
+    expect(screen.queryByText(tooltipText)).not.toBeInTheDocument();
   });
 
-  it('should make the button enable and act as a normal button', () => {
+  it('should make the button disable and view the tooltip with the default text', async () => {
     render(
-      <ActionButton buttonType="primary" buttonSize="M" canDoAction={false} />
+      <ActionButton buttonType="primary" buttonSize="M" canDoAction={false}>
+        test
+      </ActionButton>
     );
+    const button = screen.getByRole('button');
+    expect(button).toBeDisabled();
+    expect(screen.queryByText(tooltipText)).not.toBeInTheDocument();
+    await userEvent.hover(button);
+    expect(screen.getByText(tooltipText)).toBeInTheDocument();
+  });
+
+  it('should make the button disable and view the tooltip with the given text', async () => {
+    const customTooltipText = 'something here';
+
+    render(
+      <ActionButton
+        buttonType="primary"
+        buttonSize="M"
+        canDoAction={false}
+        message={customTooltipText}
+      />
+    );
+    const button = screen.getByRole('button');
+    expect(button).toBeDisabled();
+    expect(screen.queryByText(customTooltipText)).not.toBeInTheDocument();
+    await userEvent.hover(button);
+    expect(screen.getByText(customTooltipText)).toBeInTheDocument();
+    await userEvent.unhover(button);
+    expect(screen.queryByText(customTooltipText)).not.toBeInTheDocument();
   });
 });
