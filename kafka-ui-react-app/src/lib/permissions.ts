5c439716676439c1029fb425ef34c2230b28c56a
Mgrdich <mgotm13@gmail.com>
2022-11-14 09:21:41 +0400

    add documentation for the headless Part of the permission + add modification of the data version 2 for efficient algorithmic lookup

--- kafka-ui-react-app/src/lib/permissions.ts
+++ kafka-ui-react-app/src/lib/permissions.ts
@@ -25,8 +25,39 @@ export function modifyRolesData(data?: RolesType): Map<string, RolesType> {
   return map;
 }
 
+export function modifyRolesDataV2(
+  data?: RolesType
+): Map<string, Map<UserPermissionResourceEnum, RolesType>> {
+  const map = new Map<string, Map<UserPermissionResourceEnum, RolesType>>();
+
+  data?.forEach((item) => {
+    item.clusters.forEach((name) => {
+      const cluster = map.get(name);
+      if (cluster) {
+        const { resource } = item;
+
+        const resourceItem = cluster.get(resource);
+        if (resourceItem) {
+          cluster.set(resource, resourceItem.concat(item));
+          return;
+        }
+        cluster.set(resource, [item]);
+        return;
+      }
+
+      map.set(name, new Map().set(item.resource, [item]));
+    });
+  });
+  return map;
+}
+
 /**
- * @description
+ * @description it the logic behind depending on the roles whether a certain action
+ * is permitted or not the philosophy is inspired from Headless UI libraries where
+ * you separate the logic from the renderer
+ *
+ * @example you can use this in the hook format where it used in , or if you want to calculate it dynamically
+ * you can call this dynamically in your component but the render is on you from that point on
  * */
 export function isPermitted({
   roles,
