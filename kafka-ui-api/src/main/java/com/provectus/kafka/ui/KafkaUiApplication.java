e5468a96cf2e064635b7d869416b9676e51d37ea
iliax <ikuramshin@provectus.com>
2022-11-15 15:33:38 +0400

    wip

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/KafkaUiApplication.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/KafkaUiApplication.java
@@ -1,6 +1,5 @@
 package com.provectus.kafka.ui;
 
-import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration;
@@ -12,16 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 public class KafkaUiApplication {
 
-  @SneakyThrows
   public static void main(String[] args) {
-    var snClass = Class.forName("org.xerial.snappy.Snappy");
-    var f = snClass.getDeclaredField("impl");
-    f.setAccessible(true);
-    System.out.println("\n\n Snappy implementation (with gcompat): "
-        + f.get(null)
-        + "\n\n "
-        + "\n\n ");
-
     SpringApplication.run(KafkaUiApplication.class, args);
   }
 }
