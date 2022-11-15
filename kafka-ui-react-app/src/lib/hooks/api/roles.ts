bbfa6925e10787020b494656fda0604780345336
Mgrdich <mgotm13@gmail.com>
2022-11-15 18:37:59 +0400

    Roles minor code removal

--- kafka-ui-react-app/src/lib/hooks/api/roles.ts
+++ kafka-ui-react-app/src/lib/hooks/api/roles.ts
@@ -1,11 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { AccessApiClient } from 'lib/api';
 import { QUERY_REFETCH_OFF_OPTIONS } from 'lib/constants';
-import {
-  Action,
-  UserPermission,
-  UserPermissionResourceEnum,
-} from 'generated-sources';
 
 export function useRoleBasedAccess() {
   return useQuery(
@@ -13,65 +8,6 @@ export function useRoleBasedAccess() {
     () => AccessApiClient.getPermissions(),
     QUERY_REFETCH_OFF_OPTIONS
   );
-}
-
-export function useRoleBasedAccessMock() {
-  return useQuery(['roles'], (): Promise<Array<UserPermission>> => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        return resolve([
-          {
-            clusters: ['local'],
-            resource: UserPermissionResourceEnum.TOPIC,
-            value: 'sss',
-            actions: [Action.DELETE, Action.CREATE],
-          },
-          {
-            clusters: ['local'],
-            resource: UserPermissionResourceEnum.KSQL,
-            actions: [Action.EXECUTE],
-          },
-          {
-            clusters: ['local', 'dev'],
-            resource: UserPermissionResourceEnum.SCHEMA,
-            actions: [Action.CREATE],
-          },
-          {
-            clusters: ['local'],
-            resource: UserPermissionResourceEnum.CONNECT,
-            actions: [Action.CREATE],
-          },
-          {
-            clusters: ['local'],
-            resource: UserPermissionResourceEnum.TOPIC,
-            actions: [
-              Action.EDIT,
-              Action.MESSAGES_DELETE,
-              Action.DELETE,
-              Action.VIEW,
-            ],
-            value: '123.*',
-          },
-          {
-            clusters: ['local'],
-            resource: UserPermissionResourceEnum.TOPIC,
-            actions: [Action.CREATE],
-          },
-          {
-            clusters: ['local'],
-            resource: UserPermissionResourceEnum.SCHEMA,
-            actions: [Action.EDIT, Action.DELETE],
-            value: '111.*',
-          },
-          {
-            clusters: ['local'],
-            resource: UserPermissionResourceEnum.CLUSTERCONFIG,
-            actions: [Action.EDIT],
-          },
-        ]);
-      });
-    });
-  });
 }
 
 export function useUserInfo() {
