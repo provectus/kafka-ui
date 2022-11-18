95ce10d6ce675c41c8852dbeea2943c94a597b96
Mgrdich <mgotm13@gmail.com>
2022-11-18 14:47:22 +0400

    change the Query key for the user info

--- kafka-ui-react-app/src/lib/hooks/api/roles.ts
+++ kafka-ui-react-app/src/lib/hooks/api/roles.ts
@@ -4,7 +4,7 @@ import { QUERY_REFETCH_OFF_OPTIONS } from 'lib/constants';
 
 export function useGetUserInfo() {
   return useQuery(
-    ['roles'],
+    ['userInfo'],
     () => AccessApiClient.getUserAuthInfo(),
     QUERY_REFETCH_OFF_OPTIONS
   );
