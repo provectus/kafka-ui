c0598a99ed259082c888d59dd4c39eb16b7c7f94
Arthur <79512124+ArthurNiedial@users.noreply.github.com>
2022-12-20 18:19:31 +0200

    [e2e] Update case "Checking the information of used Broker in a cluster" (#3094)
    * [e2e] Update case "Checking the information of used Broker in a cluster"
    
    * [e2e] Update case "Checking the information of used Broker in a cluster"
    
    * [e2e] Update case "Checking the information of used Broker in a cluster"
    
    * [e2e]First Fix Update case "Checking the information of used Broker in a cluster"
    
    * [e2e]Second Fix Update case "Checking the information of used Broker in a cluster"
    
    * [e2e]Third Fix Update case "Checking the information of used Broker in a cluster"
    
    * [e2e]Third Fix Update case "Checking the information of used Broker in a cluster"
    
    Co-authored-by: Vlad Senyuta <66071557+VladSenyuta@users.noreply.github.com>
    (cherry picked from commit ca3916aae01494a890ba021535400ff148df0fcb)

--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/base/Facade.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/base/Facade.java
@@ -3,6 +3,7 @@ package com.provectus.kafka.ui.base;
 import com.provectus.kafka.ui.helpers.ApiHelper;
 import com.provectus.kafka.ui.pages.NaviSideBar;
 import com.provectus.kafka.ui.pages.TopPanel;
+import com.provectus.kafka.ui.pages.brokers.BrokersConfigTabPanel;
 import com.provectus.kafka.ui.pages.brokers.BrokersDetails;
 import com.provectus.kafka.ui.pages.brokers.BrokersList;
 import com.provectus.kafka.ui.pages.connector.ConnectorCreateForm;
@@ -36,4 +37,5 @@ public abstract class Facade {
     protected TopPanel topPanel = new TopPanel();
     protected BrokersList brokersList = new BrokersList();
     protected BrokersDetails brokersDetails = new BrokersDetails();
+    protected BrokersConfigTabPanel brokersConfigTabPanel = new BrokersConfigTabPanel();
 }
