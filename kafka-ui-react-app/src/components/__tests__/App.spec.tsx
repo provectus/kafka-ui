a4f38880c2b02b4999154b87a4ecbd911123d65b
Mgrdich <46796009+Mgrdich@users.noreply.github.com>
2022-12-23 16:49:32 +0400

    [FE] Change message timestamp format (#3119)
    * remove dayjs from the code base
    
    * remove dayjs library and clean up the hook codes
    
    * minor obsolete fixtures deletion
    
    * minor provider code modifications
    
    * minor provider code modifications
    
    * minor linter code modifications
    
    * minor date local and language code modifications
    
    Co-authored-by: Roman Zabaluev <rzabaluev@provectus.com>

--- kafka-ui-react-app/src/components/__tests__/App.spec.tsx
+++ kafka-ui-react-app/src/components/__tests__/App.spec.tsx
@@ -2,20 +2,11 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import App from 'components/App';
 import { render } from 'lib/testHelpers';
-import { useTimeFormat } from 'lib/hooks/api/timeFormat';
-import { defaultGlobalSettingsValue } from 'components/contexts/GlobalSettingsContext';
 import { useGetUserInfo } from 'lib/hooks/api/roles';
 
 jest.mock('components/Nav/Nav', () => () => <div>Navigation</div>);
 
 jest.mock('components/Version/Version', () => () => <div>Version</div>);
-
-jest.mock('components/NavBar/NavBar', () => () => <div>NavBar</div>);
-
-jest.mock('lib/hooks/api/timeFormat', () => ({
-  ...jest.requireActual('lib/hooks/api/timeFormat'),
-  useTimeFormat: jest.fn(),
-}));
 
 jest.mock('lib/hooks/api/roles', () => ({
   useGetUserInfo: jest.fn(),
@@ -23,10 +14,6 @@ jest.mock('lib/hooks/api/roles', () => ({
 
 describe('App', () => {
   beforeEach(() => {
-    (useTimeFormat as jest.Mock).mockImplementation(() => ({
-      data: defaultGlobalSettingsValue,
-    }));
-
     (useGetUserInfo as jest.Mock).mockImplementation(() => ({
       data: {},
     }));
