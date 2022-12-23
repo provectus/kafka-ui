aa2d7fb8f110ef974a6f06717bd1697a95142023
Mgrdich <mgotm13@gmail.com>
2022-12-23 17:27:13 +0400

    removing timestamp api from the generated sources

--- kafka-ui-react-app/src/lib/api.ts
+++ kafka-ui-react-app/src/lib/api.ts
@@ -8,7 +8,6 @@ import {
   Configuration,
   KafkaConnectApi,
   ConsumerGroupsApi,
-  TimeStampFormatApi,
   AuthorizationApi,
 } from 'generated-sources';
 import { BASE_PARAMS } from 'lib/constants';
@@ -19,7 +18,6 @@ export const ksqlDbApiClient = new KsqlApi(apiClientConf);
 export const topicsApiClient = new TopicsApi(apiClientConf);
 export const brokersApiClient = new BrokersApi(apiClientConf);
 export const schemasApiClient = new SchemasApi(apiClientConf);
-export const timerStampFormatApiClient = new TimeStampFormatApi(apiClientConf);
 export const messagesApiClient = new MessagesApi(apiClientConf);
 export const clustersApiClient = new ClustersApi(apiClientConf);
 export const kafkaConnectApiClient = new KafkaConnectApi(apiClientConf);
