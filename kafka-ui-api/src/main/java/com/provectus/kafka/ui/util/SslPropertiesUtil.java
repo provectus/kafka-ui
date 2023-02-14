a7f0e5cb5458fdbe1917373f00aca54557a4b1b4
iliax <ikuramshin@provectus.com>
2023-02-14 13:51:55 +0400

    SSL properties NPE fixes

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/util/SslPropertiesUtil.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/util/SslPropertiesUtil.java
@@ -11,10 +11,18 @@ public class SslPropertiesUtil {
 
   public void addKafkaSslProperties(@Nullable ClustersProperties.Ssl ssl, Properties properties) {
     if (ssl != null) {
-      properties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, ssl.getTruststoreLocation());
-      properties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, ssl.getTruststorePassword());
-      properties.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, ssl.getKeystoreLocation());
-      properties.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, ssl.getKeystorePassword());
+      if (ssl.getTruststoreLocation() != null) {
+        properties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, ssl.getTruststoreLocation());
+      }
+      if (ssl.getTruststorePassword() != null) {
+        properties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, ssl.getTruststorePassword());
+      }
+      if (ssl.getKeystoreLocation() != null) {
+        properties.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, ssl.getKeystoreLocation());
+      }
+      if (ssl.getKeystorePassword() != null) {
+        properties.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, ssl.getKeystorePassword());
+      }
     }
   }
 
