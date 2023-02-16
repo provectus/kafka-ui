6ee0607e98d17806feaaeae1cc29da95a4b03f34
davitbejanyan <dbejanyan@provectus.com>
2023-02-16 20:11:25 +0400

    testing3

--- kafka-ui-react-app/src/components/common/Tooltip/Tooltip.tsx
+++ kafka-ui-react-app/src/components/common/Tooltip/Tooltip.tsx
@@ -10,12 +10,20 @@ import * as S from './Tooltip.styled';
 
 export interface PropsTypes {
   value: string | JSX.Element;
+  content: string;
   placement?: Placement;
 }
 
-const Tooltip: React.FC<PropsTypes> = ({ value, placement }) => {
+const Tooltip: React.FC<PropsTypes> = ({ value, content, placement }) => {
   const [open, setOpen] = useState(false);
-  const { reference, context } = useFloating({
+  const {
+    x,
+    y,
+    reference,
+    floating: current,
+    strategy,
+    context,
+  } = useFloating({
     open,
     onOpenChange: setOpen,
     placement,
@@ -27,18 +35,19 @@ const Tooltip: React.FC<PropsTypes> = ({ value, placement }) => {
       <div ref={reference}>
         <S.Wrapper>{value}</S.Wrapper>
       </div>
-      <S.MessageTooltip>
-        `DYNAMIC_TOPIC_CONFIG = dynamic topic config that is configured for a
-        specific topic DYNAMIC_BROKER_LOGGER_CONFIG = dynamic broker logger
-        config that is configured for a specific broker DYNAMIC_BROKER_CONFIG =
-        dynamic broker config that is configured for a specific broker
-        DYNAMIC_DEFAULT_BROKER_CONFIG = dynamic broker config that is configured
-        as default for all brokers in the cluster STATIC_BROKER_CONFIG = static
-        broker config provided as broker properties at start up (e.g.
-        server.properties file) DEFAULT_CONFIG = built-in default configuration
-        for configs that have a default value UNKNOWN = source unknown e.g. in
-        the ConfigEntry used for alter requests where source is not set`;
-      </S.MessageTooltip>
+      {open && (
+        <S.MessageTooltip
+          ref={current}
+          style={{
+            position: strategy,
+            top: y ?? 0,
+            left: x ?? 0,
+            width: 'max-content',
+          }}
+        >
+          {content}
+        </S.MessageTooltip>
+      )}
     </>
   );
 };
