efa5c28c3dfce034f72153a11e65b55b586e83ac
VladSenyuta <vlad.senyuta@gmail.com>
2023-02-23 14:04:05 +0200

    upd template

--- kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/qaseSuite/Template.java
+++ kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/qaseSuite/Template.java
@@ -1,22 +1,27 @@
 package com.provectus.kafka.ui.qaseSuite;
 
+import com.provectus.kafka.ui.utilities.qaseUtils.annotations.Automation;
+import com.provectus.kafka.ui.utilities.qaseUtils.annotations.Status;
+import com.provectus.kafka.ui.utilities.qaseUtils.annotations.Suite;
+import io.qase.api.annotation.QaseTitle;
 import io.qase.api.annotation.Step;
+
+import static com.provectus.kafka.ui.utilities.qaseUtils.enums.State.NOT_AUTOMATED;
+import static com.provectus.kafka.ui.utilities.qaseUtils.enums.Status.DRAFT;
 
 public class Template extends BaseQase {
 
     /**
      * this class is a kind of placeholder or example, use is as template to create new one
      * copy class into kafka-ui-e2e-checks/src/test/java/com/provectus/kafka/ui/qaseSuite/suite
-     * remove comment signs and set annotations according to kafka-ui-e2e-checks/src/test/README.md
+     * uncomment @Test and set all annotations according to kafka-ui-e2e-checks/src/test/README.md
      */
 
-    /*
-     @Automation(state = NOT_AUTOMATED)
-     @QaseTitle("testCaseA title")
-     @Status(status = DRAFT)
-     @Suite(id = #)
-     @Test
-     */
+    @Automation(state = NOT_AUTOMATED)
+    @QaseTitle("testCaseA title")
+    @Status(status = DRAFT)
+    @Suite(id = 0)
+//    @Test
     public void testCaseA() {
         stepA();
         stepB();
