65c2d55cd6bb2af7efccdc8909d8e1fc4d34b681
olondar <36262598+olondar@users.noreply.github.com>
2023-03-31 11:03:21 +0300

    Update EditorViewer.styled.ts

--- kafka-ui-react-app/src/components/common/EditorViewer/EditorViewer.styled.ts
+++ kafka-ui-react-app/src/components/common/EditorViewer/EditorViewer.styled.ts
@@ -4,8 +4,7 @@ export const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.viewer.wrapper.backgroundColor};
   padding: 8px 16px;
   .ace_active-line {
-    background-color: ${({ theme }) =>
-      theme.viewer.wrapper.backgroundColor} !important;
+    background-color: ${({ theme }) => theme.default.backgroundColor} !important;
   }
   .ace_line {
     color: ${({ theme }) => theme.viewer.wrapper.color} !important;
