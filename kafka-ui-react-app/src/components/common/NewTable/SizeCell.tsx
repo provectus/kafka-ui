3d35875bed098ced8a9303f3932cd2f726f012bc
Nail Badiullin <nbadiullin@provectus.com>
2023-04-06 14:12:39 +0400

    fix linter issue

--- kafka-ui-react-app/src/components/common/NewTable/SizeCell.tsx
+++ kafka-ui-react-app/src/components/common/NewTable/SizeCell.tsx
@@ -3,7 +3,11 @@ import { CellContext } from '@tanstack/react-table';
 import BytesFormatted from 'components/common/BytesFormatted/BytesFormatted';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const SizeCell: React.FC<CellContext<any, unknown> & { renderSegments?: boolean }> = ({ getValue, row, renderSegments = false }) => (
+type AsAny = any;
+
+const SizeCell: React.FC<
+  CellContext<AsAny, unknown> & { renderSegments?: boolean }
+> = ({ getValue, row, renderSegments = false }) => (
   <>
     <BytesFormatted value={getValue<string | number>()} />
     {renderSegments ? `, ${row?.original.count} segment(s)` : null}
