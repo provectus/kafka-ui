943b4075f65d4b60bc73b149bd651508edfa8e8c
Junseok-Hur <j1hur@student.bridgew.edu>
2023-05-08 21:48:22 -0400

    Revert "Track and show information regarding last KC restart"
    This reverts commit d3fb0e4d7b6b560c530d19a52dbb05ff0b51b80f.

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/KafkaConnectService.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/KafkaConnectService.java
@@ -265,12 +265,10 @@ public class KafkaConnectService {
                                   String connectorName, Predicate<TaskDTO> taskFilter) {
     return getConnectorTasks(cluster, connectName, connectorName)
         .filter(taskFilter)
-        .flatMap(t -> restartConnectorTask(cluster, connectName, connectorName, t.getId().getTask())
-            .onErrorResume(e -> Mono.error(new RuntimeException("Failed to restart task", e)))
-        )
+        .flatMap(t ->
+            restartConnectorTask(cluster, connectName, connectorName, t.getId().getTask()))
         .then();
   }
-
 
   public Flux<TaskDTO> getConnectorTasks(KafkaCluster cluster, String connectName, String connectorName) {
     return api(cluster, connectName)
