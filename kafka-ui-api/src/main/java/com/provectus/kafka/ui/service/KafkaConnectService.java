1f958ee3b023fc18ce305e42f5471e7bf2992b82
Junseok-Hur <j1hur@student.bridgew.edu>
2023-05-08 21:51:26 -0400

    When restart failed, error msg pop up

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/KafkaConnectService.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/service/KafkaConnectService.java
@@ -265,8 +265,9 @@ public class KafkaConnectService {
                                   String connectorName, Predicate<TaskDTO> taskFilter) {
     return getConnectorTasks(cluster, connectName, connectorName)
         .filter(taskFilter)
-        .flatMap(t ->
-            restartConnectorTask(cluster, connectName, connectorName, t.getId().getTask()))
+        .flatMap(t -> restartConnectorTask(cluster, connectName, connectorName, t.getId().getTask())
+            .onErrorResume(e -> Mono.error(new RuntimeException("Failed to restart task", e)))
+        )
         .then();
   }
 
