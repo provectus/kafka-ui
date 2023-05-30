e222851182f2613205a04b412b01b5988c2e57fa
Roman Zabaluev <rzabaluev@provectus.com>
2023-05-30 18:37:11 +0800

    Make webclient use system proxy. Fixes #3868

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/util/GithubReleaseInfo.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/util/GithubReleaseInfo.java
@@ -3,8 +3,10 @@ package com.provectus.kafka.ui.util;
 import com.google.common.annotations.VisibleForTesting;
 import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @Slf4j
 public class GithubReleaseInfo {
@@ -31,7 +33,15 @@ public class GithubReleaseInfo {
 
   @VisibleForTesting
   GithubReleaseInfo(String url) {
-    this.refreshMono = WebClient.create()
+    var httpClient = HttpClient.create()
+        .proxyWithSystemProperties();
+
+    var webClient = WebClient
+        .builder()
+        .clientConnector(new ReactorClientHttpConnector(httpClient))
+        .build();
+
+    this.refreshMono = webClient
         .get()
         .uri(url)
         .exchangeToMono(resp -> resp.bodyToMono(GithubReleaseDto.class))
