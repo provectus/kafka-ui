d525d31a0fc9321b96a068985efdb4bf338d10f8
Roman Zabaluev <rzabaluev@provectus.com>
2023-06-21 17:26:23 +0800

    Satisfy linter

--- kafka-ui-react-app/src/components/Brokers/Broker/Broker.tsx
+++ kafka-ui-react-app/src/components/Brokers/Broker/Broker.tsx
@@ -44,7 +44,10 @@ const Broker: React.FC = () => {
       <Metrics.Wrapper>
         <Metrics.Section>
           <Metrics.Indicator label="Segment Size">
-            <BytesFormatted value={brokerDiskUsage?.segmentSize} precision={2} />
+            <BytesFormatted
+              value={brokerDiskUsage?.segmentSize}
+              precision={2}
+            />
           </Metrics.Indicator>
           <Metrics.Indicator label="Segment Count">
             {brokerDiskUsage?.segmentCount}
