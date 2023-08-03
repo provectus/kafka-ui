79d48e2b7b13e7c533bc5f5827bcea1fb7b31531
Mihai <mihaialexandruagache@yahoo.com>
2023-08-03 22:21:34 +0300

    more formatting

--- kafka-ui-react-app/src/lib/hooks/__tests__/dateTimeHelpers.spec.ts
+++ kafka-ui-react-app/src/lib/hooks/__tests__/dateTimeHelpers.spec.ts
@@ -12,11 +12,17 @@ describe('dateTimeHelpers', () => {
 
     it('should output the correct date', () => {
       const date = new Date();
+      const formattedDate = formatTimestamp(date);
+      const formattedTimestamp = formatTimestamp(date.getTime());
+
+      const expected = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}, ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+      expect(formattedDate).toBe(expected);
+      expect(formattedTimestamp).toBe(expected); const date = new Date();
       expect(formatTimestamp(date)).toBe(
-        date.toLocaleString('en-US', { hour12: true })
+        date.toLocaleString([], { hour12: false })
       );
       expect(formatTimestamp(date.getTime())).toBe(
-        date.toLocaleString('en-US', { hour12: true })
+        date.toLocaleString([], { hour12: false })
       );
     });
   });
