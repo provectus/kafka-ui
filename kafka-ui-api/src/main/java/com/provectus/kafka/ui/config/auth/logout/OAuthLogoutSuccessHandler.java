9520021f641d6983e1e9a592a0b3c2d06662d986
Oldřich Jedlička <oldium.pro@gmail.com>
2023-09-19 08:08:48 +0200

    BE: Add logout with query parameters to generic OAuth provider
    The logout URL is taken from auto-detected endpoint from the provider's
    well known configuration address, but can be overridden by custom
    parameter `logout-url`. Query parameters are optional and are added only
    when the corresponding key is present. Default redirect URL is a base URL
    and can be overridden in custom parameters.
    
    Also the final slash from the redirect URL can be removed when
    `redirect-url-strip-slash` is set to true/yes/on/1.
    
    Example with all custom parameters:
    
    ```
    custom-params:
      type: oauth
      logout-url: https://example.com/sso/logout
      client-id-key: clientId
      redirect-url-key: returnTo
      redirect-url: https://example.com/kafka-ui
      redirect-url-strip-slash: true
    ```
    
    Signed-off-by: Oldřich Jedlička <oldium.pro@gmail.com>

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/config/auth/logout/OAuthLogoutSuccessHandler.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/config/auth/logout/OAuthLogoutSuccessHandler.java
@@ -1,46 +1,106 @@
 package com.provectus.kafka.ui.config.auth.logout;
 
 import com.provectus.kafka.ui.config.auth.OAuthProperties;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import com.provectus.kafka.ui.config.auth.condition.OAuthCondition;
+import com.provectus.kafka.ui.model.rbac.provider.Provider;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import lombok.Getter;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.web.server.WebFilterExchange;
-import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
+import org.springframework.util.Assert;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
-@ConditionalOnProperty(value = "auth.type", havingValue = "OAUTH2")
-public class OAuthLogoutSuccessHandler implements ServerLogoutSuccessHandler {
-  private final OAuthProperties properties;
-  private final List<LogoutSuccessHandler> logoutSuccessHandlers;
-  private final ServerLogoutSuccessHandler defaultOidcLogoutHandler;
+@Conditional(OAuthCondition.class)
+public class OAuthLogoutSuccessHandler extends QueryParamsLogoutSuccessHandler {
 
-  public OAuthLogoutSuccessHandler(final OAuthProperties properties,
-                                   final List<LogoutSuccessHandler> logoutSuccessHandlers,
-                                   final @Qualifier("defaultOidcLogoutHandler") ServerLogoutSuccessHandler handler) {
-    this.properties = properties;
-    this.logoutSuccessHandlers = logoutSuccessHandlers;
-    this.defaultOidcLogoutHandler = handler;
+  @Getter
+  private enum Attributes {
+    CLIENT_ID_KEY("client-id-key"),
+    LOGOUT_URL("logout-url"),
+    REDIRECT_URL("redirect-url"),
+    REDIRECT_URL_KEY("redirect-url-key"),
+    REDIRECT_URL_STRIP_SLASH("redirect-url-strip-slash");
+
+    private final String value;
+
+    Attributes(String value) {
+      this.value = value;
+    }
+  }
+
+  private final Set<String> attributes = Set.of(Arrays.stream(Attributes.values()).map(Attributes::getValue)
+      .toArray(String[]::new));
+
+  private final Set<String> trueValues = Set.of("on", "true", "1", "yes");
+
+  @Override
+  public boolean isApplicable(String provider, Map<String, String> customParams) {
+    // oauth type is used for ACL too, so check if there is any of our attributes
+    return (Provider.Name.OAUTH.equalsIgnoreCase(customParams.getOrDefault("type", null))
+                || Provider.Name.OAUTH.equalsIgnoreCase(provider))
+        && attributes.stream().anyMatch(customParams::containsKey);
   }
 
   @Override
-  public Mono<Void> onLogoutSuccess(final WebFilterExchange exchange,
-                                    final Authentication authentication) {
-    final OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-    final String providerId = oauthToken.getAuthorizedClientRegistrationId();
-    final OAuthProperties.OAuth2Provider oAuth2Provider = properties.getClient().get(providerId);
-    return getLogoutHandler(oAuth2Provider.getProvider())
-        .map(handler -> handler.handle(exchange, authentication, oAuth2Provider))
-        .orElseGet(() -> defaultOidcLogoutHandler.onLogoutSuccess(exchange, authentication));
+  protected URI buildRedirect(WebFilterExchange exchange, UriComponents baseUrl,
+                              OAuthProperties.OAuth2Provider provider, ClientRegistration clientRegistration) {
+    URI logoutUrl = null;
+
+    if (provider.getCustomParams().containsKey(Attributes.LOGOUT_URL.getValue())) {
+      logoutUrl = URI.create(provider.getCustomParams().get(Attributes.LOGOUT_URL.getValue()));
+    }
+
+    if (logoutUrl == null && clientRegistration != null) {
+      Object endSessionEndpoint = clientRegistration.getProviderDetails().getConfigurationMetadata()
+          .get("end_session_endpoint");
+      if (endSessionEndpoint != null) {
+        logoutUrl = URI.create(endSessionEndpoint.toString());
+      }
+    }
+
+    Assert.notNull(logoutUrl, "Cannot determine logout URL, custom params should contain 'logout-url'");
+
+    var params = new LinkedMultiValueMap<String, String>();
+
+    if (provider.getCustomParams().containsKey(Attributes.CLIENT_ID_KEY.getValue())) {
+      params.add(provider.getCustomParams().get(Attributes.CLIENT_ID_KEY.getValue()), provider.getClientId());
+    }
+
+
+    if (provider.getCustomParams().containsKey(Attributes.REDIRECT_URL_KEY.getValue())) {
+      String redirectUrl = provider.getCustomParams().getOrDefault(Attributes.REDIRECT_URL.getValue(),
+                                                                   baseUrl.toString());
+
+      if (provider.getCustomParams().containsKey(Attributes.REDIRECT_URL_STRIP_SLASH.getValue())) {
+        var stripSlashValue = String.valueOf(provider.getCustomParams()
+            .get(Attributes.REDIRECT_URL_STRIP_SLASH.getValue())).toLowerCase();
+
+        if (trueValues.contains(stripSlashValue)) {
+          redirectUrl = stripSlash(redirectUrl);
+        }
+      }
+
+      params.add(provider.getCustomParams().get(Attributes.REDIRECT_URL_KEY.getValue()), redirectUrl);
+    }
+
+    return createRedirectUrl(logoutUrl, params);
   }
 
-  private Optional<LogoutSuccessHandler> getLogoutHandler(final String provider) {
-    return logoutSuccessHandlers.stream()
-        .filter(h -> h.isApplicable(provider))
-        .findFirst();
+  private String stripSlash(String url) {
+    URI parsedUrl = URI.create(url);
+    if (parsedUrl.isAbsolute() && parsedUrl.getPath().endsWith("/")) {
+      return UriComponentsBuilder.fromUri(parsedUrl).replacePath(
+          parsedUrl.getPath().substring(0, parsedUrl.getPath().length() - 1)).build().toString();
+    } else {
+      return url;
+    }
   }
 }
