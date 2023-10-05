84486254538013b43ffb7507b6c2c5411e94fe7c
JhonDoe15 <noamyiz@gmail.com>
2023-10-05 20:18:22 +0300

    faster check for empty string

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/config/CustomWebFilter.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/config/CustomWebFilter.java
@@ -16,7 +16,7 @@ public class CustomWebFilter implements WebFilter {
 
     final String path = exchange.getRequest().getPath().pathWithinApplication().value();
 
-    if (path.startsWith("/ui") || path.equals("") || path.equals("/")) {
+    if (path.startsWith("/ui") || path.isEmpty() || path.equals("/")) {
       return chain.filter(
           exchange.mutate().request(
               exchange.getRequest().mutate()
