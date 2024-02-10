9cf310865a2813edc2845e11d4ce9fde9d2a7a87
Benedetto Marco Serinelli <benedettomarco.serinelli@gmail.com>
2024-02-10 22:51:52 +0100

    feat: change favicon

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/controller/PreferencesController.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/controller/PreferencesController.java
@@ -1,6 +1,6 @@
 package com.provectus.kafka.ui.controller;
 
-import com.provectus.kafka.ui.api.PreferenceseApi;
+import com.provectus.kafka.ui.api.PreferencesApi;
 import com.provectus.kafka.ui.model.ApplicationsPreferencesDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,14 +13,31 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-public class PreferencesController implements PreferenceseApi {
+public class PreferencesController implements PreferencesApi {
 
-  @Value("${kafka.ui.preferences.removegitlink}")
-  private String kafkaUiRemoveGitLink;
+  @Value("${kafka.ui.preferences.removeGitlink}")
+  private boolean kafkaUiRemoveGitLink;
+  @Value("${kafka.ui.preferences.removeDiscordLink}")
+  private boolean kafkaUiRemovDiscordLink;
+  @Value("${kafka.ui.preferences.appName}")
+  private String kafkaUiAppName;
+  @Value("${kafka.ui.preferences.favicon}")
+  private String kafkaUiFavicon;
+  @Value("${kafka.ui.preferences.logo}")
+  private String kafkaUiIcon;
 
 
   @Override
   public Mono<ResponseEntity<ApplicationsPreferencesDTO>> getPreferences(ServerWebExchange exchange) {
-    return null;
+    ApplicationsPreferencesDTO res = new ApplicationsPreferencesDTO();
+    res.setAppName(kafkaUiAppName);
+    res.setRemoveDiscordLink(kafkaUiRemoveGitLink);
+    res.setRemoveGitLink(kafkaUiRemovDiscordLink);
+    res.setLogo(kafkaUiIcon);
+    res.setFavicon(kafkaUiFavicon);
+    return Mono.just(
+        ResponseEntity.ok(res
+        )
+    );
   }
 }
