778aae13b41cfef04be38a88776fa4953fa1abb1
gokhanimral <gimral@gmail.com>
2024-02-25 17:19:16 +0400

    Lint

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/emitter/OffsetsInfo.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/emitter/OffsetsInfo.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.mutable.MutableLong;
